02dfbe92fcbc016d176dca26c4d78706b9711099
Ali Saidi <saidi@eecs.umich.edu>
2004-07-08 00:28:02 -0400

    fixed a bad merge from linux<->tru64
    --HG--
    extra : convert_revision : d7a5acd89a2bdc013c8a2bd022cc7048a3920f01

--- arch/alpha/vtophys.cc
+++ arch/alpha/vtophys.cc
@@ -100,11 +100,11 @@ vtophys(ExecContext *xc, Addr vaddr)
     //so I put it back in. Perhaps something to do with gdb debugging?
     if (PC_PAL(vaddr)) {
         paddr = vaddr & ~ULL(1);
-    } else if (!ptbr) {
-        paddr = vaddr;
     } else {
         if (vaddr >= ALPHA_K0SEG_BASE && vaddr <= ALPHA_K0SEG_END) {
             paddr = ALPHA_K0SEG_TO_PHYS(vaddr);
+        } else if (!ptbr) {
+            paddr = vaddr;
         } else {
             Addr pte = kernel_pte_lookup(xc->physmem, ptbr, vaddr);
             uint64_t entry = xc->physmem->phys_read_qword(pte);
