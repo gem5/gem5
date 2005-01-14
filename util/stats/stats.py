21c6dd80d76c888e662bfd511cc7764d57b29d9f
Ali Saidi <saidi@eecs.umich.edu>
2005-01-13 23:59:39 -0500

    fix a display bug add option to limit results to a set of ticks fix ticks code to work
    util/stats/info.py:
        change samples -> ticks and pass all parameters
    util/stats/stats.py:
        add option to select a set of ticks and fix display bug
    
    --HG--
    extra : convert_revision : eca80a8c6bb75cf82bf1624f3d0170690b2928af

--- util/stats/stats.py
+++ util/stats/stats.py
@@ -202,6 +202,10 @@ def commands(options, command, args):
             if options.graph:
                 graphdata(runs, options, stat.name, stat.name, stat)
             else:
+                if options.ticks:
+                   print 'only displaying sample %s' % options.ticks
+                   stat.ticks = options.ticks
+
                 if options.binned:
                     print 'kernel ticks'
                     stat.bins = 'kernel'
@@ -216,7 +220,7 @@ def commands(options, command, args):
                     printdata(runs, stat)
 
                     print 'interrupt ticks'
-                    stat.bins = 'user'
+                    stat.bins = 'interrupt'
                     printdata(runs, stat)
 
                     print 'total ticks'
@@ -249,7 +253,7 @@ def commands(options, command, args):
                     printdata(runs, stat)
 
                     print 'interrupt ticks'
-                    stat.bins = 'user'
+                    stat.bins = 'interrupt'
                     printdata(runs, stat)
 
                     print 'total ticks'
@@ -578,8 +582,9 @@ if __name__ == '__main__':
     options.get = None
     options.binned = False
     options.graph = False
+    options.ticks = False
 
-    opts, args = getopts(sys.argv[1:], '-BEFGd:g:h:pr:s:u:')
+    opts, args = getopts(sys.argv[1:], '-BEFGd:g:h:pr:s:u:T:')
     for o,a in opts:
         if o == '-B':
             options.binned = True
@@ -603,6 +608,8 @@ if __name__ == '__main__':
             options.user = a
         if o == '-s':
             options.system = a
+        if o == '-T':
+            options.ticks = a
 
     if len(args) == 0:
         usage()
