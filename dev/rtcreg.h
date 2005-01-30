da269014dad5dce6136c53914d451ff9e6ef3e62
Ali Saidi <saidi@eecs.umich.edu>
2005-01-30 16:58:39 -0500

    removed all tsunami dependence on tlaserreg.h (RTC defines) and all but tlaser_node.cc dependence on tlaserreg.h
    dev/tsunami_io.cc:
    dev/tsunamireg.h:
        removed tlaserreg.h
    
    --HG--
    extra : convert_revision : 148a5d79530e5ed721a49279f684a48041deed2b

---
+++ dev/rtcreg.h
@@ -0,0 +1,44 @@
+
+/*
+ * Copyright (c) 2005 The Regents of The University of Michigan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define RTC_SEC                 0x00
+#define RTC_SEC_ALRM            0x01
+#define RTC_MIN                 0x02
+#define RTC_MIN_ALRM            0x03
+#define RTC_HR                  0x04
+#define RTC_HR_ALRM             0x05
+#define RTC_DOW                 0x06
+#define RTC_DOM                 0x07
+#define RTC_MON                 0x08
+#define RTC_YEAR                0x09
+#define RTC_CNTRL_REGA          0x0A
+#define RTC_CNTRL_REGB          0x0B
+#define RTC_CNTRL_REGC          0x0C
+#define RTC_CNTRL_REGD          0x0D
+
