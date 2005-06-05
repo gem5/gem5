5070f27c694571eb238bb2f98d69cf06d551994c
Ali Saidi <saidi@eecs.umich.edu>
2005-06-05 04:08:05 -0400

    added copyright
    kern/linux/sched.hh:
    kern/linux/thread_info.hh:
        got rid of everything but exactly what we needed
    util/categories.py:
        newest version from one of my repositories
    
    --HG--
    extra : convert_revision : c4328e5938d421d60493c0da07022bfa9e92c404

--- kern/linux/thread_info.hh
+++ kern/linux/thread_info.hh
@@ -6,22 +6,8 @@
 
 namespace Linux {
     struct thread_info {
-        struct pcb_struct       pcb;            /* palcode state */
-
-        Addr_a                  task;          /* main task structure */
-        uint32_t                flags;          /* low level flags */
-        uint32_t                ieee_state;     /* see fpu.h */
-
-        Addr_a                   exec_domain;   /* execution domain */
-        uint64_ta                addr_limit;     /* thread address space */
-        int64_ta                 cpu;            /* current CPU */
-        int32_t                  preempt_count;  /* 0 => preemptable, <0 => BUG */
-
-        int32_t                  bpt_nsaved;
-        uint64_ta                bpt_addr[2];    /* breakpoint handling  */
-        uint32_t                 bpt_insn[2];
-
-        /*restart_block;*/
+        struct pcb_struct       pcb;
+        Addr_a                  task;
     };
 }
 #endif /* __ALPHA_THREAD_INFO_H__ */
