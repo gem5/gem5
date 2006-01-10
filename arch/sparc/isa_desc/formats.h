79a46838767f82554fafe6dc3c811492bdb32f8a
Gabe Black <gblack@eecs.umich.edu>
2006-01-10 14:57:37 -0500

    Adding the sparc ISA files which were worked on locally
    arch/sparc/isa_desc/bitfields.h:
        This file defines the bit fields used by the isa description system
    arch/sparc/isa_desc/decoder.h:
        This file describes the decoder for the isa description system
    arch/sparc/isa_desc/formats.h:
        This file declares the instruction formats
    arch/sparc/isa_desc/formats/basic.format:
        This file implements the "basic" instruction format
    arch/sparc/isa_desc/formats/branch.format:
        This file implements the "branch" instruction format
    arch/sparc/isa_desc/formats/integerop.format:
        This file implements the "integerop" instruction format
    arch/sparc/isa_desc/formats/mem.format:
        This file implements the "mem" instruction format
    arch/sparc/isa_desc/formats/noop.format:
        This file implements the "noop" instruction format
    arch/sparc/isa_desc/formats/trap.format:
        This file implements the "trap" instruction format
    arch/sparc/isa_desc/includes.h:
        This file is all of the inclues that are used by the isa description system
    
    --HG--
    extra : convert_revision : 12a2ffe949317b8b57d83263a4261131b9432c2a

---
+++ arch/sparc/isa_desc/formats.h
@@ -0,0 +1,19 @@
+//Include the basic format
+//Templates from this format are used later
+##include "m5/arch/sparc/isa_desc/formats/basic.format"
+
+//Include the integerOp and integerOpCc format
+##include "m5/arch/sparc/isa_desc/formats/integerop.format"
+
+//Include the mem format
+##include "m5/arch/sparc/isa_desc/formats/mem.format"
+
+//Include the trap format
+##include "m5/arch/sparc/isa_desc/formats/trap.format"
+
+//Include the branch format
+##include "m5/arch/sparc/isa_desc/formats/branch.format"
+
+//Include the noop format
+##include "m5/arch/sparc/isa_desc/formats/noop.format"
+
