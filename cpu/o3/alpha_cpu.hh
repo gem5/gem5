802fd04f640b34d713f7ef75142e51d3d82559b9
Gabe Black <gblack@eecs.umich.edu>
2006-02-24 03:51:21 -0500

    Removed a stray ::.
    --HG--
    extra : convert_revision : f6114b78e30e8cba5af6276042b0f043d8773739

--- cpu/o3/alpha_cpu.hh
+++ cpu/o3/alpha_cpu.hh
@@ -280,7 +280,7 @@ class AlphaFullCPU : public FullO3CPU<Impl>
 
 #endif
 
-        return this->mem->write(req, (T)::htog(data));
+        return this->mem->write(req, (T)htog(data));
     }
 
     template <class T>
