ef8b7713ca1b6120ae4e851877f2a9ce71296219
Gabe Black <gblack@eecs.umich.edu>
2006-10-23 11:17:59 -0400

    Minor compile fix. Not sure why this is broken.
    --HG--
    extra : convert_revision : 6f181b15f37114ca0a3965cabcb2036bd2f97916

--- src/cpu/ozone/lw_lsq.hh
+++ src/cpu/ozone/lw_lsq.hh
@@ -260,7 +260,7 @@ class OzoneLWLSQ {
 
         virtual void getDeviceAddressRanges(AddrRangeList &resp,
                                             AddrRangeList &snoop)
-        { resp.clear(); snoop.clear(); snoop.push_back(RangeSize(0,-1); }
+        { resp.clear(); snoop.clear(); snoop.push_back(RangeSize(0,-1)); }
 
         virtual bool recvTiming(PacketPtr pkt);
 
