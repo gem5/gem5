628a3b1d01702d1e3888c40c4a02c38c636ae6d4
Gabe Black <gblack@eecs.umich.edu>
2006-10-29 04:04:50 -0500

    An attempt to serialize the state of the micro code mechanism in the simple cpu.
    src/cpu/simple/base.cc:
        Make a microcoded op start at the current micropc, rather than starting at 0.
    src/cpu/thread_state.cc:
        Serialize the microPC and nextMicroPC
    
    --HG--
    extra : convert_revision : 5302215f17312ecef3ff4c6548acb05297ee4ff6

--- src/cpu/thread_state.cc
+++ src/cpu/thread_state.cc
@@ -62,6 +62,8 @@ ThreadState::serialize(std::ostream &os)
     // thread_num and cpu_id are deterministic from the config
     SERIALIZE_SCALAR(funcExeInst);
     SERIALIZE_SCALAR(inst);
+    SERIALIZE_SCALAR(microPC);
+    SERIALIZE_SCALAR(nextMicroPC);
 
 #if FULL_SYSTEM
     Tick quiesceEndTick = 0;
@@ -81,6 +83,8 @@ ThreadState::unserialize(Checkpoint *cp, const std::string &section)
     // thread_num and cpu_id are deterministic from the config
     UNSERIALIZE_SCALAR(funcExeInst);
     UNSERIALIZE_SCALAR(inst);
+    UNSERIALIZE_SCALAR(microPC);
+    UNSERIALIZE_SCALAR(nextMicroPC);
 
 #if FULL_SYSTEM
     Tick quiesceEndTick;
