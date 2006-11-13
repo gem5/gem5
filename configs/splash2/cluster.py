023fccff0e7f6e2be144c56567b58845cc7383f5
Ron Dreslinski <rdreslin@umich.edu>
2006-11-13 16:09:47 -0500

    Update splash2 config files
    configs/splash2/run.py:
        Fix MaxTick for splash configs
    configs/splash2/cluster.py:
        Add a config that allows clusters of CPU's to be attached to a single L1
    
    --HG--
    extra : convert_revision : 1bb0a0c5f4889316940a9858be90ae2eaa849f1a

--- configs/splash2/cluster.py
+++ configs/splash2/cluster.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2005-2006 The Regents of The University of Michigan
+# Copyright (c) 2006 The Regents of The University of Michigan
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -26,8 +26,9 @@
 #
 # Authors: Ron Dreslinski
 
-# Splash2 Run Script
+# Simple test script
 #
+# "m5 test.py"
 
 import m5
 from m5.objects import *
@@ -43,6 +44,8 @@ parser = optparse.OptionParser()
 parser.add_option("-d", "--detailed", action="store_true")
 parser.add_option("-t", "--timing", action="store_true")
 parser.add_option("-m", "--maxtick", type="int")
+parser.add_option("-c", "--numclusters",
+                  help="Number of clusters", type="int")
 parser.add_option("-n", "--numcpus",
                   help="Number of cpus in total", type="int")
 parser.add_option("-f", "--frequency",
@@ -61,7 +64,7 @@ parser.add_option("--l2latency",
                   default = 10)
 parser.add_option("--rootdir",
                   help="ROot directory of Splash2",
-                  default="/dist/splash2/codes")
+                  default="/dist/splash2/codes/")
 parser.add_option("-b", "--benchmark",
                   help="Splash 2 benchmark to run")
 
@@ -69,10 +72,6 @@ parser.add_option("-b", "--benchmark",
 
 if args:
     print "Error: script doesn't take any positional arguments"
-    sys.exit(1)
-
-if not options.numcpus:
-    print "Specify the number of cpus with -n"
     sys.exit(1)
 
 # --------------------
@@ -84,53 +83,53 @@ class Cholesky(LiveProcess):
              + options.rootdir + '/kernels/cholesky/inputs/tk23.O'
 
 class FFT(LiveProcess):
-        executable = options.rootdir + '/kernels/fft/FFT'
+        executable = options.rootdir + 'kernels/fft/FFT'
         cmd = 'FFT -p' + str(options.numcpus) + ' -m18'
 
 class LU_contig(LiveProcess):
-        executable = options.rootdir + '/kernels/lu/contiguous_blocks/LU'
+        executable = options.rootdir + 'kernels/lu/contiguous_blocks/LU'
         cmd = 'LU -p' + str(options.numcpus)
 
 class LU_noncontig(LiveProcess):
-        executable = options.rootdir + '/kernels/lu/non_contiguous_blocks/LU'
+        executable = options.rootdir + 'kernels/lu/non_contiguous_blocks/LU'
         cmd = 'LU -p' + str(options.numcpus)
 
 class Radix(LiveProcess):
-        executable = options.rootdir + '/kernels/radix/RADIX'
+        executable = options.rootdir + 'kernels/radix/RADIX'
         cmd = 'RADIX -n524288 -p' + str(options.numcpus)
 
 class Barnes(LiveProcess):
-        executable = options.rootdir + '/apps/barnes/BARNES'
+        executable = options.rootdir + 'apps/barnes/BARNES'
         cmd = 'BARNES'
-        input = options.rootdir + '/apps/barnes/input.p' + str(options.numcpus)
+        input = options.rootdir + 'apps/barnes/input.p' + str(options.numcpus)
 
 class FMM(LiveProcess):
-        executable = options.rootdir + '/apps/fmm/FMM'
+        executable = options.rootdir + 'apps/fmm/FMM'
         cmd = 'FMM'
-        input = options.rootdir + '/apps/fmm/inputs/input.2048.p' + str(options.numcpus)
+        input = options.rootdir + 'apps/fmm/inputs/input.2048.p' + str(options.numcpus)
 
 class Ocean_contig(LiveProcess):
-        executable = options.rootdir + '/apps/ocean/contiguous_partitions/OCEAN'
+        executable = options.rootdir + 'apps/ocean/contiguous_partitions/OCEAN'
         cmd = 'OCEAN -p' + str(options.numcpus)
 
 class Ocean_noncontig(LiveProcess):
-        executable = options.rootdir + '/apps/ocean/non_contiguous_partitions/OCEAN'
+        executable = options.rootdir + 'apps/ocean/non_contiguous_partitions/OCEAN'
         cmd = 'OCEAN -p' + str(options.numcpus)
 
 class Raytrace(LiveProcess):
-        executable = options.rootdir + '/apps/raytrace/RAYTRACE'
+        executable = options.rootdir + 'apps/raytrace/RAYTRACE'
         cmd = 'RAYTRACE -p' + str(options.numcpus) + ' ' \
              + options.rootdir + 'apps/raytrace/inputs/teapot.env'
 
 class Water_nsquared(LiveProcess):
-        executable = options.rootdir + '/apps/water-nsquared/WATER-NSQUARED'
+        executable = options.rootdir + 'apps/water-nsquared/WATER-NSQUARED'
         cmd = 'WATER-NSQUARED'
-        input = options.rootdir + '/apps/water-nsquared/input.p' + str(options.numcpus)
+        input = options.rootdir + 'apps/water-nsquared/input.p' + str(options.numcpus)
 
 class Water_spatial(LiveProcess):
-        executable = options.rootdir + '/apps/water-spatial/WATER-SPATIAL'
+        executable = options.rootdir + 'apps/water-spatial/WATER-SPATIAL'
         cmd = 'WATER-SPATIAL'
-        input = options.rootdir + '/apps/water-spatial/input.p' + str(options.numcpus)
+        input = options.rootdir + 'apps/water-spatial/input.p' + str(options.numcpus)
 
 
 # --------------------
@@ -156,28 +155,63 @@ class L2(BaseCache):
     write_buffers = 8
 
 # ----------------------
-# Define the cpus
+# Define the clusters with their cpus
 # ----------------------
+class Cluster:
+    pass
+
+cpusPerCluster = options.numcpus/options.numclusters
 
 busFrequency = Frequency(options.frequency)
+busFrequency *= cpusPerCluster
 
+all_cpus = []
+all_l1s = []
+all_l1buses = []
 if options.timing:
-    cpus = [TimingSimpleCPU(cpu_id = i,
-                            clock=options.frequency)
-            for i in xrange(options.numcpus)]
+    clusters = [ Cluster() for i in xrange(options.numclusters)]
+    for j in xrange(options.numclusters):
+        clusters[j].id = j
+    for cluster in clusters:
+        cluster.clusterbus = Bus(clock=busFrequency)
+        all_l1buses += [cluster.clusterbus]
+        cluster.cpus = [TimingSimpleCPU(cpu_id = i + cluster.id,
+                                        clock=options.frequency)
+                        for i in xrange(cpusPerCluster)]
+        all_cpus += cluster.cpus
+        cluster.l1 = L1(size=options.l1size, assoc = 4)
+        all_l1s += [cluster.l1]
 elif options.detailed:
-    cpus = [DerivO3CPU(cpu_id = i,
-                       clock=options.frequency)
-            for i in xrange(options.numcpus)]
+    clusters = [ Cluster() for i in xrange(options.numclusters)]
+    for j in xrange(options.numclusters):
+        clusters[j].id = j
+    for cluster in clusters:
+        cluster.clusterbus = Bus(clock=busFrequency)
+        all_l1buses += [cluster.clusterbus]
+        cluster.cpus = [DerivO3CPU(cpu_id = i + cluster.id,
+                                   clock=options.frequency)
+                        for i in xrange(cpusPerCluster)]
+        all_cpus += cluster.cpus
+        cluster.l1 = L1(size=options.l1size, assoc = 4)
+        all_l1s += [cluster.l1]
 else:
-    cpus = [AtomicSimpleCPU(cpu_id = i,
-                            clock=options.frequency)
-            for i in xrange(options.numcpus)]
+    clusters = [ Cluster() for i in xrange(options.numclusters)]
+    for j in xrange(options.numclusters):
+        clusters[j].id = j
+    for cluster in clusters:
+        cluster.clusterbus = Bus(clock=busFrequency)
+        all_l1buses += [cluster.clusterbus]
+        cluster.cpus = [AtomicSimpleCPU(cpu_id = i + cluster.id,
+                                        clock=options.frequency)
+                        for i in xrange(cpusPerCluster)]
+        all_cpus += cluster.cpus
+        cluster.l1 = L1(size=options.l1size, assoc = 4)
+        all_l1s += [cluster.l1]
 
 # ----------------------
 # Create a system, and add system wide objects
 # ----------------------
-system = System(cpu = cpus, physmem = PhysicalMemory(),
+system = System(cpu = all_cpus, l1_ = all_l1s, l1bus_ = all_l1buses, physmem = PhysicalMemory(),
                 membus = Bus(clock = busFrequency))
 
 system.toL2bus = Bus(clock = busFrequency)
@@ -194,13 +228,13 @@ system.l2.mem_side = system.membus.port
 # ----------------------
 # Connect the L2 cache and clusters together
 # ----------------------
-for cpu in cpus:
-    cpu.addPrivateSplitL1Caches(L1(size = options.l1size, assoc = 1),
-                                L1(size = options.l1size, assoc = 4))
-    cpu.mem = cpu.dcache
-    # connect cpu level-1 caches to shared level-2 cache
-    cpu.connectMemPorts(system.toL2bus)
-
+for cluster in clusters:
+    cluster.l1.cpu_side = cluster.clusterbus.port
+    cluster.l1.mem_side = system.toL2bus.port
+    for cpu in cluster.cpus:
+        cpu.icache_port = cluster.clusterbus.port
+        cpu.dcache_port = cluster.clusterbus.port
+        cpu.mem = cluster.l1
 
 # ----------------------
 # Define the root
@@ -245,8 +279,9 @@ else:
 # Assign the workload to the cpus
 # ====================
 
-for cpu in cpus:
-    cpu.workload = root.workload
+for cluster in clusters:
+    for cpu in cluster.cpus:
+        cpu.workload = root.workload
 
 # ----------------------
 # Run the simulation
