ed50763135885c339009738ff44a8db434ddc93c
Lisa Hsu <hsul@eecs.umich.edu>
2006-12-01 13:51:49 -0500

    change this to be a quick one so that it's in the regressions every night - it's only maybe 15 min. long.
    tests/configs/twosys-tsunami-simple-atomic.py:
        don't need this import
    
    --HG--
    rename : tests/long/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/config.ini => tests/quick/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/config.ini
    rename : tests/long/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/config.out => tests/quick/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/config.out
    rename : tests/long/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/console.drivesys.sim_console => tests/quick/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/console.drivesys.sim_console
    rename : tests/long/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/console.testsys.sim_console => tests/quick/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/console.testsys.sim_console
    rename : tests/long/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/m5stats.txt => tests/quick/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/m5stats.txt
    rename : tests/long/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/stderr => tests/quick/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/stderr
    rename : tests/long/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/stdout => tests/quick/80.netperf-stream/ref/alpha/linux/twosys-tsunami-simple-atomic/stdout
    rename : tests/long/80.netperf-stream/test.py => tests/quick/80.netperf-stream/test.py
    extra : convert_revision : 68497b2ef8b21590cb6c636485703e46dc616513

--- tests/quick/80.netperf-stream/test.py
+++ tests/quick/80.netperf-stream/test.py
@@ -26,22 +26,3 @@
 #
 # Authors: Lisa Hsu
 
-import m5
-from m5.objects import *
-m5.AddToPath('../configs/common')
-from FSConfig import *
-from Benchmarks import *
-
-test_sys = makeLinuxAlphaSystem('atomic',
-                                 SysConfig('netperf-stream-client.rcS'))
-test_sys.cpu = AtomicSimpleCPU(cpu_id=0)
-test_sys.cpu.connectMemPorts(test_sys.membus)
-
-drive_sys = makeLinuxAlphaSystem('atomic',
-                                 SysConfig('netperf-server.rcS'))
-drive_sys.cpu = AtomicSimpleCPU(cpu_id=0)
-drive_sys.cpu.connectMemPorts(drive_sys.membus)
-
-root = makeDualRoot(test_sys, drive_sys, "ethertrace")
-
-maxtick = 199999999
