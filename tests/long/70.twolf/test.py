c5b49bdbad10a169b52181df7b37b168cc9800a5
Gabe Black <gblack@eecs.umich.edu>
2007-03-10 17:45:53 -0500

    If you can't get rid of the files you want to make sure are gone, assume they're already gone. Print a warning and move on.
    --HG--
    extra : convert_revision : 192740b1e1a7a30222c4e79c7e7a5770e682f014

--- tests/long/70.twolf/test.py
+++ tests/long/70.twolf/test.py
@@ -37,5 +37,11 @@ cwd = root.system.cpu.workload.cwd
 #Remove two files who's presence or absence affects execution
 sav_file = os.path.join(cwd, workload.input_set + '.sav')
 sv2_file = os.path.join(cwd, workload.input_set + '.sv2')
-os.unlink(sav_file)
-os.unlink(sv2_file)
+try:
+    os.unlink(sav_file)
+except:
+    print "Couldn't unlink ", sav_file
+try:
+    os.unlink(sv2_file)
+except:
+    print "Couldn't unlink ", sv2_file
