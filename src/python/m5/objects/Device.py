4a37c48e8e74405c39899e4914656930869f8bd4
Ali Saidi <saidi@eecs.umich.edu>
2007-05-10 00:08:22 -0400

    add/update parameters for bus bridge
    --HG--
    extra : convert_revision : 063f757fbfa2c613328ffa70e556f8926623fa91

--- src/python/m5/objects/Device.py
+++ src/python/m5/objects/Device.py
@@ -19,6 +19,12 @@ class DmaDevice(PioDevice):
     type = 'DmaDevice'
     abstract = True
     dma = Port(Self.pio.peerObj.port, "DMA port")
+    min_backoff_delay = Param.Latency('4ns',
+      "min time between a nack packet being received and the next request made by the device")
+    max_backoff_delay = Param.Latency('10us',
+      "max time between a nack packet being received and the next request made by the device")
+
+
 
 class IsaFake(BasicPioDevice):
     type = 'IsaFake'
