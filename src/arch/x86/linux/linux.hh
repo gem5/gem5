b9793c25060b445dc4fcfaaa1c76c934ee47733a
Gabe Black <gblack@eecs.umich.edu>
2007-08-04 20:18:20 -0700

    X86: Make the open flags correct.
    --HG--
    extra : convert_revision : 2dc81345176d1de247a567d1f748e2b2bd05f829

--- src/arch/x86/linux/linux.hh
+++ src/arch/x86/linux/linux.hh
@@ -87,16 +87,16 @@ class X86Linux64 : public Linux
 
     static OpenFlagTransTable openFlagTable[];
 
-    static const int TGT_O_RDONLY	= 0x00000000;	//!< O_RDONLY
-    static const int TGT_O_WRONLY	= 0x00000001;	//!< O_WRONLY
-    static const int TGT_O_RDWR	        = 0x00000002;	//!< O_RDWR
-    static const int TGT_O_NONBLOCK     = 0x00004000;	//!< O_NONBLOCK
-    static const int TGT_O_APPEND	= 0x00000008;	//!< O_APPEND
-    static const int TGT_O_CREAT	= 0x00000200;	//!< O_CREAT
-    static const int TGT_O_TRUNC	= 0x00000400;	//!< O_TRUNC
-    static const int TGT_O_EXCL	        = 0x00000800;	//!< O_EXCL
-    static const int TGT_O_NOCTTY	= 0x00008000;	//!< O_NOCTTY
-    static const int TGT_O_SYNC	        = 0x00002000;	//!< O_SYNC
+    static const int TGT_O_RDONLY	= 00000000;	//!< O_RDONLY
+    static const int TGT_O_WRONLY	= 00000001;	//!< O_WRONLY
+    static const int TGT_O_RDWR	        = 00000002;	//!< O_RDWR
+    static const int TGT_O_NONBLOCK     = 00004000;	//!< O_NONBLOCK
+    static const int TGT_O_APPEND	= 00002000;	//!< O_APPEND
+    static const int TGT_O_CREAT	= 00000100;	//!< O_CREAT
+    static const int TGT_O_TRUNC	= 00001000;	//!< O_TRUNC
+    static const int TGT_O_EXCL	        = 00000200;	//!< O_EXCL
+    static const int TGT_O_NOCTTY	= 00000400;	//!< O_NOCTTY
+    static const int TGT_O_SYNC	        = 00010000;	//!< O_SYNC
 //    static const int TGT_O_DRD	        = 0x00010000;	//!< O_DRD
 //    static const int TGT_O_DIRECTIO     = 0x00020000;	//!< O_DIRECTIO
 //    static const int TGT_O_CACHE	= 0x00002000;	//!< O_CACHE
