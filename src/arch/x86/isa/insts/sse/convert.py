8e3b199cb8fc0109b0bfe87905bb3253b4e7b8c7
Gabe Black <gblack@eecs.umich.edu>
2007-09-04 23:32:18 -0700

    X86: Add some SSE floating point/integer conversion microops.
    --HG--
    extra : convert_revision : 2a1aa16709db940f5f40bbd84ca082f26b03b9c5

--- src/arch/x86/isa/insts/sse/convert.py
+++ src/arch/x86/isa/insts/sse/convert.py
@@ -53,12 +53,34 @@
 #
 # Authors: Gabe Black
 
-categories = ["move",
-              "convert"]
-
 microcode = '''
-# SSE instructions
+def macroop CVTSI2SD_R_R {
+    cvtf_i2d xmml, regm
+};
+
+def macroop CVTSI2SD_R_M {
+    ld t1, seg, sib, disp
+    cvtf_i2d xmml, t1
+};
+
+def macroop CVTSI2SD_R_P {
+    rdip t7
+    ld t1, seg, riprel, disp
+    cvtf_i2d xmml, t1
+};
+
+def macroop CVTTSD2SI_R_R {
+    cvtf_d2i reg, xmmlm
+};
+
+def macroop CVTTSD2SI_R_M {
+    ldfp ufp1, seg, sib, disp
+    cvtf_d2i reg, ufp1
+};
+
+def macroop CVTTSD2SI_R_P {
+    rdip t7
+    ldfp ufp1, seg, riprel, disp
+    cvtf_d2i reg, ufp1
+};
 '''
-for category in categories:
-    exec "import %s as cat" % category
-    microcode += cat.microcode
