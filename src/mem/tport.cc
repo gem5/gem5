34ca72d16d50586763dbffa01ec7f16b555ac402
Gabe Black <gblack@eecs.umich.edu>
2008-10-12 23:50:22 -0700

    Get rid of some commented out code.

--- src/mem/tport.cc
+++ src/mem/tport.cc
@@ -65,7 +65,6 @@ SimpleTimingPort::recvTiming(PacketPtr pkt)
     // code to hanldle nacks here, but I'm pretty sure it didn't work
     // correctly with the drain code, so that would need to be fixed
     // if we ever added it back.
-    //assert(pkt->isRequest());
 
     if (pkt->memInhibitAsserted()) {
         // snooper will supply based on copy of packet
