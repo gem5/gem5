483c3e96b7e30d3bd788f32c5b421133e71ec476
Gabe Black <gblack@eecs.umich.edu>
2009-02-01 00:08:16 -0800

    X86: Calculate flags based on the actual result.

--- src/arch/x86/insts/microregop.cc
+++ src/arch/x86/insts/microregop.cc
@@ -67,6 +67,9 @@ namespace X86ISA
             bool subtract) const
     {
         DPRINTF(X86, "flagMask = %#x\n", flagMask);
+        if (_destRegIdx[0] & (1 << 6)) {
+            _dest >>= 8;
+        }
         uint64_t flags = oldFlags & ~flagMask;
         if(flagMask & (ECFBit | CFBit))
         {
