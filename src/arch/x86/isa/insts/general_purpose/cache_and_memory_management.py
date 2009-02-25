06ff83e1b9aa2a00af4f66fae7c9fce2ac36394a
Gabe Black <gblack@eecs.umich.edu>
2009-02-25 10:19:22 -0800

    X86: Implement a basic prefetch instruction.

--- src/arch/x86/isa/insts/general_purpose/cache_and_memory_management.py
+++ src/arch/x86/isa/insts/general_purpose/cache_and_memory_management.py
@@ -53,7 +53,31 @@
 #
 # Authors: Gabe Black
 
-microcode = ""
+microcode = '''
+def macroop PREFETCH_M
+{
+    ld t0, seg, sib, disp, dataSize=1, prefetch=True
+};
+
+def macroop PREFETCH_P
+{
+    rdip t7
+    ld t0, seg, riprel, disp, dataSize=1, prefetch=True
+};
+
+def macroop PREFETCH_T0_M
+{
+    ld t0, seg, sib, disp, dataSize=1, prefetch=True
+};
+
+def macroop PREFETCH_T0_P
+{
+    rdip t7
+    ld t0, seg, riprel, disp, dataSize=1, prefetch=True
+};
+
+'''
+
 #let {{
 #    class LFENCE(Inst):
 #       "GenFault ${new UnimpInstFault}"
@@ -62,8 +86,6 @@ microcode = ""
 #    class MFENCE(Inst):
 #       "GenFault ${new UnimpInstFault}"
 #    class PREFETCHlevel(Inst):
-#       "GenFault ${new UnimpInstFault}"
-#    class PREFETCH(Inst):
 #       "GenFault ${new UnimpInstFault}"
 #    class PREFETCHW(Inst):
 #       "GenFault ${new UnimpInstFault}"
