d4813236d6a4c4f982e7f4c00a4ccd8ceab12443
Korey Sewell <ksewell@umich.edu>
2009-07-25 22:22:13 -0400

    regress: edit 2t hello smt file to specify numThreads

--- tests/quick/01.hello-2T-smt/test.py
+++ tests/quick/01.hello-2T-smt/test.py
@@ -30,3 +30,4 @@ process1 = LiveProcess(cmd = 'hello', executable = binpath('hello'))
 process2 = LiveProcess(cmd = 'hello', executable = binpath('hello'))
 
 root.system.cpu.workload = [process1, process2]
+root.system.cpu.numThreads = 2
