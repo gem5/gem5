5a4ebd6d1293b51cadc33d5556daff81fd76799a
Derek Hower <drh5@cs.wisc.edu>
2010-01-25 11:51:16 -0600

    config: changed default ruby config file for regression

--- tests/configs/simple-atomic-ruby.py
+++ tests/configs/simple-atomic-ruby.py
@@ -30,7 +30,7 @@ import m5
 from m5.objects import *
 
 import ruby_config
-ruby_memory = ruby_config.generate("MI_example-homogeneous.rb", 1)
+ruby_memory = ruby_config.generate("TwoLevel_SplitL1UnifiedL2.rb", 1)
 
 system = System(cpu = AtomicSimpleCPU(cpu_id=0),
                 physmem = ruby_memory,
