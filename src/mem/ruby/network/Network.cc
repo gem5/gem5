a3b4b9b3e3f8a1462b34d758199312d33af4b0c7
Brad Beckmann <Brad.Beckmann@amd.com>
2010-08-20 11:46:12 -0700

    ruby: Added bcast msg profiling to hammer and token

--- src/mem/ruby/network/Network.cc
+++ src/mem/ruby/network/Network.cc
@@ -71,6 +71,7 @@ Network::MessageSizeType_to_int(MessageSizeType size_type)
       case MessageSizeType_Reissue_Control:
       case MessageSizeType_Response_Control:
       case MessageSizeType_Writeback_Control:
+      case MessageSizeType_Broadcast_Control:
       case MessageSizeType_Forwarded_Control:
       case MessageSizeType_Invalidate_Control:
       case MessageSizeType_Unblock_Control:
