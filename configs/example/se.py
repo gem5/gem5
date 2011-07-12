d919930c3c7f5d364f211513742a51f56e36eaab
Nilay Vaish <nilay@cs.wisc.edu>
2011-07-11 19:57:10 -0500

    se.py: Fixes the way ruby's options are added

--- configs/example/se.py
+++ configs/example/se.py
@@ -70,15 +70,16 @@ parser.add_option("-o", "--options", default="",
 parser.add_option("-i", "--input", default="", help="Read stdin from a file.")
 parser.add_option("--output", default="", help="Redirect stdout to a file.")
 parser.add_option("--errout", default="", help="Redirect stderr to a file.")
-parser.add_option("--ruby", action="store_true")
+
+if 'PROTOCOL' in buildEnv:
+    parser.add_option("--ruby", action="store_true")
 
 execfile(os.path.join(config_root, "common", "Options.py"))
 
-(options, args) = parser.parse_args()
-
-if options.ruby:
+if '--ruby' in sys.argv:
     Ruby.define_options(parser)
-    (options, args) = parser.parse_args()
+
+(options, args) = parser.parse_args()
 
 if args:
     print "Error: script doesn't take any positional arguments"
