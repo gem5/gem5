8870a5820a458ca22cbd4bec60f223a4fe4949e6
Wade Walker <wade.walker@arm.com>
2011-07-15 11:53:34 -0500

    ARM: Fix SWP/SWPB undefined instruction behavior
    SWP and SWPB now throw an undefined instruction exception if
    SCTLR.SW == 0. This also required the MIDR to be changed
    slightly so programs can correctly determine that gem5 supports
    the ARM v7 behavior of SWP/SWPB (in ARM v6, SWP/SWPB were
    deprecated, but not disabled at CPU startup).

--- src/arch/arm/ArmSystem.py
+++ src/arch/arm/ArmSystem.py
@@ -49,9 +49,9 @@ class ArmSystem(System):
     # 0x35 Implementor is '5' from "M5"
     # 0x0 Variant
     # 0xf Architecture from CPUID scheme
-    # 0xf00 Primary part number
+    # 0xc00 Primary part number ("c" or higher implies ARM v7)
     # 0x0 Revision
-    midr_regval = Param.UInt32(0x350ff000, "MIDR value")
+    midr_regval = Param.UInt32(0x350fc000, "MIDR value")
     boot_loader = Param.String("", "File that contains the boot loader code if any")
     boot_loader_mem = Param.PhysicalMemory(NULL,
                           "Memory object that boot loader is to be loaded into")
