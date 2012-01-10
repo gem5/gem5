bda1125e882eaf8c127ebdae0d847bf2148832ad
Dam Sunwoo <dam.sunwoo@arm.com>
2012-01-09 18:08:20 -0600

    stats: fix Vector2d to display stats correctly when y_subname is not specified.
    Vector2d stats with no y_subname were not displayed as the VectorPrint subname was not initialized correctly to reflect the empty field.

--- src/base/stats/text.cc
+++ src/base/stats/text.cc
@@ -513,7 +513,14 @@ Text::visit(const Vector2dInfo &info)
     bool havesub = false;
     VectorPrint print;
 
-    print.subnames = info.y_subnames;
+    if (!info.y_subnames.empty()) {
+        for (off_type i = 0; i < info.y; ++i) {
+            if (!info.y_subnames[i].empty()) {
+                print.subnames = info.y_subnames;
+            }
+            break;
+        }
+    }
     print.flags = info.flags;
     print.separatorString = info.separatorString;
     print.descriptions = descriptions;
