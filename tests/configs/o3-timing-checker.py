da0d67c3d6468bedae93d14a9b7461e2b9d7a645
Geoffrey Blake <geoffrey.blake@arm.com>
2012-03-09 09:59:28 -0500

    CheckerCPU: Make some basic regression tests for CheckerCPU
    Adds regression tests for the CheckerCPU. ARM ISA support
    only at this point.

--- tests/configs/o3-timing-checker.py
+++ tests/configs/o3-timing-checker.py
@@ -1,5 +1,14 @@
-# Copyright (c) 2006 The Regents of The University of Michigan
-# All rights reserved.
+# Copyright (c) 2011 ARM Limited
+# All rights reserved
+#
+# The license below extends only to copyright in the software and shall
+# not be construed as granting a license to any other intellectual
+# property including but not limited to intellectual property relating
+# to a hardware implementation of the functionality of the software
+# licensed hereunder.  You may use the software subject to the license
+# terms below provided that you ensure that this notice is replicated
+# unmodified and in its entirety in all distributions of the software,
+# modified or unmodified, in source code or in binary form.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -24,9 +33,36 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# Authors: Steve Reinhardt
+# Authors: Geoffrey Blake
 
-root.system.cpu.workload = LiveProcess(cmd = 'hello',
-                                       executable = binpath('hello'))
-if root.system.cpu.checker != NULL:
-    root.system.cpu.checker.workload = root.system.cpu.workload
+import m5
+from m5.objects import *
+m5.util.addToPath('../configs/common')
+
+class MyCache(BaseCache):
+    assoc = 2
+    block_size = 64
+    latency = '1ns'
+    mshrs = 10
+    tgts_per_mshr = 5
+
+class MyL1Cache(MyCache):
+    is_top_level = True
+    tgts_per_mshr = 20
+
+cpu = DerivO3CPU(cpu_id=0)
+cpu.createInterruptController()
+cpu.addCheckerCpu()
+cpu.addTwoLevelCacheHierarchy(MyL1Cache(size = '128kB'),
+                              MyL1Cache(size = '256kB'),
+                              MyCache(size = '2MB'))
+cpu.clock = '2GHz'
+
+system = System(cpu = cpu,
+                physmem = PhysicalMemory(),
+                membus = Bus())
+system.system_port = system.membus.slave
+system.physmem.port = system.membus.master
+cpu.connectAllPorts(system.membus)
+
+root = Root(full_system = False, system = system)
--- tests/configs/simple-atomic-dummychecker.py
+++ tests/configs/simple-atomic-dummychecker.py
@@ -1,5 +1,14 @@
-# Copyright (c) 2006 The Regents of The University of Michigan
-# All rights reserved.
+# Copyright (c) 2011 ARM Limited
+# All rights reserved
+#
+# The license below extends only to copyright in the software and shall
+# not be construed as granting a license to any other intellectual
+# property including but not limited to intellectual property relating
+# to a hardware implementation of the functionality of the software
+# licensed hereunder.  You may use the software subject to the license
+# terms below provided that you ensure that this notice is replicated
+# unmodified and in its entirety in all distributions of the software,
+# modified or unmodified, in source code or in binary form.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -24,9 +33,19 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# Authors: Steve Reinhardt
+# Authors: Geoffrey Blake
 
-root.system.cpu.workload = LiveProcess(cmd = 'hello',
-                                       executable = binpath('hello'))
-if root.system.cpu.checker != NULL:
-    root.system.cpu.checker.workload = root.system.cpu.workload
+import m5
+from m5.objects import *
+
+system = System(cpu = AtomicSimpleCPU(cpu_id=0),
+                physmem = PhysicalMemory(),
+                membus = Bus())
+system.system_port = system.membus.slave
+system.physmem.port = system.membus.master
+system.cpu.addCheckerCpu()
+system.cpu.createInterruptController()
+system.cpu.connectAllPorts(system.membus)
+system.cpu.clock = '2GHz'
+
+root = Root(full_system = False, system = system)
