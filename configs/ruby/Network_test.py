407b1a77c8ee4b3ed1a45bd9f29fad49f6ba90c4
Nilay Vaish <nilay@cs.wisc.edu>
2013-04-17 16:06:24 -0500

    config: ruby network test: remove piobus check

--- configs/ruby/Network_test.py
+++ configs/ruby/Network_test.py
@@ -93,11 +93,8 @@ def create_system(options, system, piobus, dma_ports, ruby_system):
                                 ruby_system = ruby_system)
 
         l1_cntrl.sequencer = cpu_seq
-
-        if piobus != None:
-            cpu_seq.pio_port = piobus.slave
-
         exec("ruby_system.l1_cntrl%d = l1_cntrl" % i)
+
         #
         # Add controllers and sequencers to the appropriate lists
         #
