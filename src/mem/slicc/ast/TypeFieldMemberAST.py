64af621cc639d5b43567d3503ec17a0ab3c7ab06
Malek Musleh <malek.musleh@gmail.com>
2013-05-21 11:57:14 -0500

    ruby: slicc: fix error msg in TypeFieldMemberAST.py

--- src/mem/slicc/ast/TypeFieldMemberAST.py
+++ src/mem/slicc/ast/TypeFieldMemberAST.py
@@ -54,4 +54,4 @@ class TypeFieldMemberAST(TypeFieldAST):
         if not type.addDataMember(self.field_id, field_type, self.pairs,
                                   init_code):
 
-            self.error("Duplicate data member: %s:%s" % (type_ptr, field_id))
+            self.error("Duplicate data member: %s:%s" % (field_type, self.field_id))
