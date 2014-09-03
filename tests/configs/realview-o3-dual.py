ee68c2b30232a43c5494ce183022e9fe09a004d2
Andreas Hansson <andreas.hansson@arm.com>
2014-09-03 07:42:41 -0400

    tests: Use O3_ARM_v7a config for full-system ARM regressions
    This patch changes the CPU configuration used for the full-system ARM
    regressions to increase the test coverage. Note that it is only the
    core configuration, and not the caches etc.

--- tests/configs/realview-o3-dual.py
+++ tests/configs/realview-o3-dual.py
@@ -37,8 +37,9 @@
 
 from m5.objects import *
 from arm_generic import *
+from O3_ARM_v7a import O3_ARM_v7a_3
 
 root = LinuxArmFSSystem(mem_mode='timing',
                         mem_class=DDR3_1600_x64,
-                        cpu_class=DerivO3CPU,
+                        cpu_class=O3_ARM_v7a_3,
                         num_cpus=2).create_root()
--- tests/configs/realview-o3.py
+++ tests/configs/realview-o3.py
@@ -37,7 +37,8 @@
 
 from m5.objects import *
 from arm_generic import *
+from O3_ARM_v7a import O3_ARM_v7a_3
 
 root = LinuxArmFSSystemUniprocessor(mem_mode='timing',
                                     mem_class=DDR3_1600_x64,
-                                    cpu_class=DerivO3CPU).create_root()
+                                    cpu_class=O3_ARM_v7a_3).create_root()
