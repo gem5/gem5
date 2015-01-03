5a5416d575f68e416ebe713ad76dc468a59ac23c
Maxime Martinasso <maxime.cscs@gmail.com>
2015-01-03 17:51:48 -0600

    x86: implements the simd128 ADDSUBPD instruction
    This patch implements the simd128 ADDSUBPD instruction for the x86 architecture.
    
    Tested with a simple program in assembly language which executes the
    instruction.  Checked that different versions of the instruction are executed
    by using the execution tracing option.
    
    Committed by: Nilay Vaish <nilay@cs.wisc.edu

--- src/arch/x86/isa/insts/simd128/floating_point/arithmetic/simultaneous_addition_and_subtraction.py
+++ src/arch/x86/isa/insts/simd128/floating_point/arithmetic/simultaneous_addition_and_subtraction.py
@@ -37,5 +37,24 @@
 
 microcode = '''
 # ADDSUBPS
-# ADDSUBPD
+
+def macroop ADDSUBPD_XMM_XMM {
+    msubf xmml, xmml, xmmlm, size=8, ext=Scalar
+    maddf xmmh, xmmh, xmmhm, size=8, ext=Scalar
+};
+
+def macroop ADDSUBPD_XMM_M {
+    ldfp ufp1, seg, sib, disp, dataSize=8
+    ldfp ufp2, seg, sib, "DISPLACEMENT+8", dataSize=8
+    msubf xmmlm, xmml, ufp1, size=8, ext=0
+    maddf xmmhm, xmmh, ufp2, size=8, ext=0
+};
+
+def macroop ADDSUBPD_XMM_P {
+    rdip t7
+    ldfp ufp1, seg, sib, disp, dataSize=8
+    ldfp ufp2, seg, sib, "DISPLACEMENT+8", dataSize=8
+    msubf xmmlm, xmml, ufp1, size=8, ext=0
+    maddf xmmhm, xmmh, ufp2, size=8, ext=0
+};
 '''
