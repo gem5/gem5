5d555df359868ade54ea0a23b027839bd40ae613
Nilay Vaish <nilay@cs.wisc.edu>
2015-09-01 15:50:32 -0500

    ruby: directory memory: drop unused variable.

--- src/mem/ruby/structures/DirectoryMemory.hh
+++ src/mem/ruby/structures/DirectoryMemory.hh
@@ -76,7 +76,6 @@ class DirectoryMemory : public SimObject
 
     static int m_num_directories;
     static int m_num_directories_bits;
-    static uint64_t m_total_size_bytes;
     static int m_numa_high_bit;
 };
 
