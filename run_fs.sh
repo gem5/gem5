5e3bf2370ee477331e7dfb365613dc1c5a51f2e7
Min Cai <min.cai.china@gmail.com>
2015-09-19 12:24:02 +0800

    Remove unused run_se.sh; update run_fs.sh.

--- run_fs.sh
+++ run_fs.sh
@@ -1,3 +1,2 @@
 #! /bin/sh
-#build/X86_MESI_Two_Level/gem5.opt configs/example/fs.py --num-cpus=4 --num-dirs=4 --topology=Mesh --mesh-rows=1 --garnet-network=fixed --cpu-type=timing --script=run_script.rcS
-build/X86_MESI_Two_Level/gem5.opt configs/example/fs.py --num-cpus=4 --num-dirs=4 --cpu-type=timing --script=run_script.rcS
+build/X86_MESI_Two_Level/gem5.opt configs/example/fs.py --cpu-type=timing --num-cpus=4 --num-dirs=4 --caches --ruby --num-l2caches=4 --l1d_size=8kB --l1i_size=8kB --l2_size=128kB --topology=Mesh --mesh-rows=2 --garnet-network=fixed --script=run_script.rcS
