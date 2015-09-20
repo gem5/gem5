0443c37124ae7213c6ed0c67543ed6242d0d3c6c
Min Cai <min.cai.china@gmail.com>
2015-09-20 09:11:15 +0800

    Update all.sh to include the call to mount.sh.

--- all.sh
+++ all.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 ./compile.sh
 # Go to http://www.gem5.org/Download/, and download the following two full-system stuffs.
-# Unzip x86Dist.tar.gz into ~/Tools/GEM5/ before executing the following commands.
-# Copy linux-bigswap2.img from m5_system_2.0b3.tar.bz2 to ~/Tools/GEM5/x86Dist/disks/.
+# Extract x86Dist.tar.gz into ~/Tools/GEM5/ before executing the following commands.
+# Extract linux-bigswap2.img from m5_system_2.0b3.tar.bz2 into ~/Tools/GEM5/x86Dist/disks/.
+./mount.sh
 ./run_fs.sh
 ./run_mcpat.sh
