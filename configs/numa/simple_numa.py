206c461559750cb8530b3176a7a0a5c73e8335b7
Min Cai <min.cai.china@gmail.com>
2015-09-24 01:28:04 +0800

    Second commit of Simple NUMA python scripts.

--- configs/numa/simple_numa.py
+++ configs/numa/simple_numa.py
@@ -28,6 +28,11 @@
 
 import m5
 from m5.objects import *
+from m5.util import addToPath, fatal
+
+addToPath('../common')
+
+from Caches import *
 
 num_domains = 2
 
@@ -37,38 +42,39 @@ isa = str(m5.defines.buildEnv['TARGET_ISA']).lower()
 
 binary = 'tests/test-progs/hello/bin/' + isa + '/linux/hello'
 
-class DomainXBar(SystemXBar):
-    pass
-
-class SystemXBar(SystemXBar):
-    pass
-
-class NUMACache(Cache):
-    assoc = 8
-    hit_latency = 50
-    response_latency = 50
-    mshrs = 20
-    tgts_per_mshr = 12
-
 class Domain:
     def __init__(self, id = -1):
         self.id = id
 
         self.mem_ranges = AddrRange(Addr(str(self.id * 512) + 'MB'), size = '512MB')
 
-        self.mem_bus = DomainXBar()
+        self.membus = SystemXBar()
 
-        self.mem_ctrl = DDR3_1600_x64(range = self.mem_ranges, port = self.mem_bus.master)
+        self.mem_ctrl = DDR3_1600_x64(range = self.mem_ranges, port = self.membus.master)
 
-        self.cpus = [TimingSimpleCPU(cpu_id = num_cpus_per_domain * self.id + i, icache_port = self.mem_bus.slave, dcache_port = self.mem_bus.slave) for i in range(num_cpus_per_domain)]
+        self.l2bus = L2XBar()
 
-        for cpu in self.cpus:
+        self.l2cache = L2Cache(size = '256kB')
+        self.l2cache.cpu_side = self.l2bus.master
+        self.l2cache.mem_side = self.membus.slave
+
+        self.cpu = [TimingSimpleCPU(cpu_id = num_cpus_per_domain * self.id + i,
+                                     icache = L1_ICache(size = '16kB'), dcache = L1_DCache(size = '64kB'))
+                     for i in range(num_cpus_per_domain)]
+
+        for cpu in self.cpu:
+            cpu.icache.cpu_side = cpu.icache_port
+            cpu.icache.mem_side = self.l2bus.slave
+
+            cpu.dcache.cpu_side = cpu.dcache_port
+            cpu.dcache.mem_side = self.l2bus.slave
+
             cpu.createInterruptController()
 
             if isa == 'x86':
-                cpu.interrupts.pio = self.mem_bus.master
-                cpu.interrupts.int_master = self.mem_bus.slave
-                cpu.interrupts.int_slave = self.mem_bus.master
+                cpu.interrupts.pio = self.membus.master
+                cpu.interrupts.int_master = self.membus.slave
+                cpu.interrupts.int_slave = self.membus.master
 
             cpu.workload = LiveProcess(cmd = [binary])
             cpu.createThreads()
@@ -81,25 +87,47 @@ system.mem_mode = 'timing'
 
 domains = [Domain(id = i) for i in range(num_domains)]
 
-mem_ctrls = []
-mem_buses = []
-cpus = []
+system.systembus = SystemXBar()
+
+numa_cache = []
+mem_ctrl = []
+membus = []
+l2bus = []
+l2cache = []
+cpu = []
 
 for domain in domains:
-    for mem_ctrl in domain.mem_ctrl:
-        mem_ctrls.append(mem_ctrl)
+    domain.numa_cache = IOCache()
 
-    for mem_bus in domain.mem_bus:
-        mem_buses.append(mem_bus)
+    domain.numa_cache.addr_ranges = []
+    for r in range(num_domains):
+        if r != domain.id:
+            domain.numa_cache.addr_ranges.append(AddrRange(Addr(str(r * 512) + 'MB'), size = '512MB'))
 
-    for cpu in domain.cpus:
-        cpus.append(cpu)
+    domain.numa_cache.cpu_side = domain.membus.master
+    domain.numa_cache.mem_side = system.systembus.slave
+    numa_cache.append(domain.numa_cache)
 
-system.mem_ctrls = mem_ctrls
-system.mem_buses = mem_buses
-system.cpus = cpus
+    system.mem_ranges.append(domain.mem_ranges)
 
-system.system_port = domains[0].mem_bus.slave
+    mem_ctrl.append(domain.mem_ctrl)
+
+    membus.append(domain.membus)
+
+    l2bus.append(domain.l2bus)
+
+    l2cache.append(domain.l2cache)
+
+    cpu += domain.cpu
+
+system.numa_cache = numa_cache
+system.mem_ctrl = mem_ctrl
+system.membus = membus
+system.l2bus = l2bus
+system.l2cache = l2cache
+system.cpu = cpu
+
+system.system_port = system.systembus.slave
 
 root = Root(full_system = False, system = system)
 
