ed5f674a4e042bb543f6403d76554f61492bea08
Min Cai <min.cai.china@gmail.com>
2015-09-30 15:42:12 +0800

    Update ALPHA_RUBY_GARNET/4c/simsmall experiment results.

--- run_all_experiments_ALPHA_RUBY_GARNET.py
+++ run_all_experiments_ALPHA_RUBY_GARNET.py
@@ -11,7 +11,7 @@ import os
 # benches = ['blackscholes', 'bodytrack', 'canneal', 'dedup', 'facesim',
 #            'ferret', 'fluidanimate', 'freqmine', 'streamcluster',
 #            'swaptions', 'vips', 'x264', 'rtview']
-benches = ['vips', 'x264']
+benches = ['canneal']
 
 # num_threads = [1, 4, 8, 16]
 # num_threads = [1, 2, 4, 8, 16, 32]
