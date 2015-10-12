a8c6e1e20dc220b299334d2ac4b328098a0aa9d3
Min Cai <min.cai.china@gmail.com>
2015-10-12 17:51:43 +0800

    First commit of ccnuma_fs stuffs.

---
+++ configs/numa/ccnuma_fs.py
@@ -0,0 +1,165 @@
+# Copyright (c) 2015 Min Cai
+# Copyright (c) 2010-2013 ARM Limited
+# All rights reserved.
+#
+# The license below extends only to copyright in the software and shall
+# not be construed as granting a license to any other intellectual
+# property including but not limited to intellectual property relating
+# to a hardware implementation of the functionality of the software
+# licensed hereunder.  You may use the software subject to the license
+# terms below provided that you ensure that this notice is replicated
+# unmodified and in its entirety in all distributions of the software,
+# modified or unmodified, in source code or in binary form.
+#
+# Copyright (c) 2012-2014 Mark D. Hill and David A. Wood
+# Copyright (c) 2009-2011 Advanced Micro Devices, Inc.
+# Copyright (c) 2006-2007 The Regents of The University of Michigan
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: Ali Saidi
+#          Brad Beckmann
+#          Min Cai
+
+import optparse
+import sys
+
+import m5
+from m5.defines import buildEnv
+from m5.objects import *
+from m5.util import addToPath, fatal
+
+addToPath('../common')
+
+from FSConfig import *
+from SysPaths import *
+from Benchmarks import *
+import Simulation
+import CacheConfig
+import MemConfig
+from Caches import *
+import Options
+
+def cmd_line_template():
+    if options.command_line and options.command_line_file:
+        print "Error: --command-line and --command-line-file are " \
+              "mutually exclusive"
+        sys.exit(1)
+    if options.command_line:
+        return options.command_line
+    if options.command_line_file:
+        return open(options.command_line_file).read().strip()
+    return None
+
+def build_test_system(np):
+    cmdline = cmd_line_template()
+    if buildEnv['TARGET_ISA'] == "alpha":
+        test_sys = makeLinuxAlphaSystem(test_mem_mode, bm[0], options.ruby,
+                                        cmdline=cmdline)
+    else:
+        fatal("Incapable of building %s full system!", buildEnv['TARGET_ISA'])
+
+    # Set the cache line size for the entire system
+    test_sys.cache_line_size = options.cacheline_size
+
+    # Create a top-level voltage domain
+    test_sys.voltage_domain = VoltageDomain(voltage = options.sys_voltage)
+
+    # Create a source clock for the system and set the clock period
+    test_sys.clk_domain = SrcClockDomain(clock =  options.sys_clock,
+            voltage_domain = test_sys.voltage_domain)
+
+    # Create a CPU voltage domain
+    test_sys.cpu_voltage_domain = VoltageDomain()
+
+    # Create a source clock for the CPUs and set the clock period
+    test_sys.cpu_clk_domain = SrcClockDomain(clock = options.cpu_clock,
+                                             voltage_domain =
+                                             test_sys.cpu_voltage_domain)
+
+    if options.kernel is not None:
+        test_sys.kernel = binary(options.kernel)
+
+    if options.script is not None:
+        test_sys.readfile = options.script
+
+    if options.lpae:
+        test_sys.have_lpae = True
+
+    if options.virtualisation:
+        test_sys.have_virtualization = True
+
+    test_sys.init_param = options.init_param
+
+    # For now, assign all the CPUs to the same clock domain
+    test_sys.cpu = [TestCPUClass(clk_domain=test_sys.cpu_clk_domain, cpu_id=i)
+                    for i in xrange(np)]
+
+    # By default the IOCache runs at the system clock
+    test_sys.iocache = IOCache(addr_ranges = test_sys.mem_ranges)
+    test_sys.iocache.cpu_side = test_sys.iobus.master
+    test_sys.iocache.mem_side = test_sys.membus.slave
+
+    for i in xrange(np):
+        test_sys.cpu[i].createThreads()
+
+    CacheConfig.config_cache(options, test_sys)
+    MemConfig.config_mem(options, test_sys)
+
+    return test_sys
+
+# Add options
+parser = optparse.OptionParser()
+Options.addCommonOptions(parser)
+Options.addFSOptions(parser)
+
+(options, args) = parser.parse_args()
+
+if args:
+    print "Error: script doesn't take any positional arguments"
+    sys.exit(1)
+
+# system under test can be any CPU
+(TestCPUClass, test_mem_mode, FutureClass) = Simulation.setCPUClass(options)
+
+# Match the memories with the CPUs, based on the options for the test system
+TestMemClass = Simulation.setMemClass(options)
+
+bm = [SysConfig(disk=options.disk_image, rootdev=options.root_device,
+                mem=options.mem_size, os_type=options.os_type)]
+
+np = options.num_cpus
+
+test_sys = build_test_system(np)
+root = Root(full_system=True, system=test_sys)
+
+if options.timesync:
+    root.time_sync_enable = True
+
+if options.frame_capture:
+    VncServer.frame_capture = True
+
+Simulation.setWorkCountOptions(test_sys, options)
+Simulation.run(options, root, test_sys, FutureClass)
