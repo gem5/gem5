455f815b1281835213efaa01579f1065cf52893f
Min Cai <min.cai.china@gmail.com>
2015-10-26 17:59:09 +0800

    Use gem5.opt in place of gem5.fast; update configure.README.

--- run_fs.sh
+++ run_fs.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-build/X86_MESI_Two_Level/gem5.fast configs/example/fs.py --cpu-type=detailed --num-cpus=4 --num-dirs=4 --caches --ruby --num-l2caches=4 --l1d_size=8kB --l1i_size=8kB --l2_size=128kB --topology=Mesh --mesh-rows=2 --garnet-network=fixed --script=run_script.rcS
+build/X86_MESI_Two_Level/gem5.opt configs/example/fs.py --cpu-type=detailed --num-cpus=4 --num-dirs=4 --caches --ruby --num-l2caches=4 --l1d_size=8kB --l1i_size=8kB --l2_size=128kB --topology=Mesh --mesh-rows=2 --garnet-network=fixed --script=run_script.rcS
--- run_learning_gem5_simple.sh
+++ run_learning_gem5_simple.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-build/X86_MESI_Two_Level/gem5.fast configs/learning_gem5/part1/simple.py
+build/X86_MESI_Two_Level/gem5.opt configs/learning_gem5/part1/simple.py
