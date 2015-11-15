e081187a489d57295eaae160c584b336960cfaad
Min Cai <min.cai.china@gmail.com>
2015-11-15 12:50:45 +0800

    Update ccnuma_fs stuffs.

--- compile_ALPHA_CCNUMA.sh
+++ compile_ALPHA_CCNUMA.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #scons build/ALPHA/gem5.fast
 #scons build/ALPHA_MESI_Two_Level/gem5.opt
-scons build/ALPHA_MESI_Two_Level/gem5.debug TRACING_ON=1 -j8
+#scons build/ALPHA_MESI_Two_Level/gem5.debug TRACING_ON=1 -j8
+scons build/ALPHA_MESI_Two_Level/gem5.opt -j8
 #~ scons build/ARM/gem5.fast -j2
 #scons build/ALPHA_MESI_CMP_directory/gem5.fast -j2
