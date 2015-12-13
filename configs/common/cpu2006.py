042d49847921aae26338614687f5ea45f230498c
Min Cai <min.cai.china@gmail.com>
2015-12-13 20:48:59 +0800

    Fix bugs in cpu2006.py.

--- configs/common/cpu2006.py
+++ configs/common/cpu2006.py
@@ -8,8 +8,8 @@ data_dir = binary_dir
 
 # 437.leslie3d
 leslie3d = LiveProcess()
-leslie3d.executable = binary_dir + '437.leslie3d/run/run_base_ref_amd64-m64-gcc32-nn.0000/leslie3d_base.amd64-m64-gcc42-nn'
-stdin = data_dir + '437.leslie3d/run/run_base_ref_amd64-m64-gcc32-nn.0000/leslie3d.in'
+leslie3d.executable = binary_dir + '437.leslie3d/run/run_base_ref_amd64-m64-gcc42-nn.0000/leslie3d_base.amd64-m64-gcc42-nn'
+stdin = data_dir + '437.leslie3d/run/run_base_ref_amd64-m64-gcc42-nn.0000/leslie3d.in'
 leslie3d.cmd = [leslie3d.executable]
 leslie3d.input = stdin
 leslie3d.output = 'leslie3d.stdout'
