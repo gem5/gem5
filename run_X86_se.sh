f2f171be9063e19defe76663fa2e48e7dd9b705d
Min Cai <min.cai.china@gmail.com>
2015-12-13 21:18:01 +0800

    Update run_X86_se.sh.

--- run_X86_se.sh
+++ run_X86_se.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-build/X86_MESI_Two_Level/gem5.opt configs/example/se.py --bench='437.leslie3d' --cpu-type=atomic -n 1 --mem-size=2048MB
+build/X86_MESI_Two_Level/gem5.opt configs/example/se.py --bench='400.perlbench-437.leslie3d' --cpu-type=atomic -n 2 --mem-size=2048MB
