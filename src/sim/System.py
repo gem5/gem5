b5a54eb64ebce9c217c1d44cc93aebb7cb508c6d
Andreas Sandberg <andreas.sandberg@arm.com>
2015-12-18 10:14:17 +0000

    sim: Use the old work item behavior by default
    When adding an option to forward work items to the Python environment,
    the new behavior was accidentally enabled by default. Set the value of
    exit_on_work_items to False by default to revert to the old behavior
    unless the simulation scripts explicitly requests work item
    forwarding.

--- src/sim/System.py
+++ src/sim/System.py
@@ -73,7 +73,7 @@ class System(MemObject):
 
     cache_line_size = Param.Unsigned(64, "Cache line size in bytes")
 
-    exit_on_work_items = Param.Bool(True, "Exit from the simulation loop when "
+    exit_on_work_items = Param.Bool(False, "Exit from the simulation loop when "
                                     "encountering work item annotations.")
     work_item_id = Param.Int(-1, "specific work item id")
     num_work_ids = Param.Int(16, "Number of distinct work item types")
