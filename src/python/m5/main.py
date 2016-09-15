18461d1522821495813b342b50ca3c69e8db2083
Curtis Dunham <Curtis.Dunham@arm.com>
2016-09-15 18:21:38 +0100

    base: eliminate ipython warning
    Change-Id: I3e282baeb969b6bb9534813a2f433d68246c0669
    Reviewed-by: Andreas Sandberg <andreas.sandberg@arm.com>

--- src/python/m5/main.py
+++ src/python/m5/main.py
@@ -151,7 +151,7 @@ def interact(scope):
         try:
             import IPython
             from IPython.config.loader import Config
-            from IPython.frontend.terminal.embed import InteractiveShellEmbed
+            from IPython.terminal.embed import InteractiveShellEmbed
 
             cfg = Config()
             cfg.PromptManager.in_template = prompt_in1
