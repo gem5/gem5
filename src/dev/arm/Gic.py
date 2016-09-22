109cc2caa6fbdfe6231336e56b9ae2158a13804f
Curtis Dunham <Curtis.Dunham@arm.com>
2016-09-22 14:46:37 +0100

    arm: disable GIC extensions
    Change-Id: If19b9c593b48ded1ea848f2d3710d4369ec8a221
    Reviewed-by: Andreas Sandberg <andreas.sandberg@arm.com>

--- src/dev/arm/Gic.py
+++ src/dev/arm/Gic.py
@@ -59,7 +59,7 @@ class Pl390(BaseGic):
     cpu_pio_delay = Param.Latency('10ns', "Delay for PIO r/w to cpu interface")
     int_latency = Param.Latency('10ns', "Delay for interrupt to get to CPU")
     it_lines = Param.UInt32(128, "Number of interrupt lines supported (max = 1020)")
-    gem5_extensions = Param.Bool(True, "Enable gem5 extensions")
+    gem5_extensions = Param.Bool(False, "Enable gem5 extensions")
 
 class Gicv2mFrame(SimObject):
     type = 'Gicv2mFrame'
