912b20d02a4ec12b076e675ff150e3f1e89febaa
Andreas Hansson <andreas.hansson@arm.com>
2017-02-19 05:30:31 -0500

    mem: Ensure deferred snoops are cache-line aligned
    This patch fixes a bug where a deferred snoop ended up being to a
    partial cache line, and not cache-line aligned, all due to how we copy
    the packet.

--- src/mem/cache/mshr.cc
+++ src/mem/cache/mshr.cc
@@ -415,7 +415,7 @@ MSHR::handleSnoop(PacketPtr pkt, Counter _order)
         // the packet and the request as part of handling the deferred
         // snoop.
         PacketPtr cp_pkt = will_respond ? new Packet(pkt, true, true) :
-            new Packet(new Request(*pkt->req), pkt->cmd);
+            new Packet(new Request(*pkt->req), pkt->cmd, blkSize);
 
         if (will_respond) {
             // we are the ordering point, and will consequently
