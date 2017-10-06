25daea0e6fd12b8ce3c282b49b3fd334d1871975
Jason Lowe-Power <jason@lowepower.com>
2017-10-06 14:27:37 -0700

    learning_gem5: Add code for hello-goodbye example
    Adding more code from Learning gem5 Part II
    
    See http://learning.gem5.org/book/part2/parameters.html
    
    Change-Id: I9fe5655239e011c718c5cf5fd62bebcda66ea966
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>
    Reviewed-on: https://gem5-review.googlesource.com/5021
    Reviewed-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

--- configs/learning_gem5/part2/hello_goodbye.py
+++ configs/learning_gem5/part2/hello_goodbye.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2016 Jason Lowe-Power
+# Copyright (c) 2017 Jason Lowe-Power
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -27,13 +27,30 @@
 #
 # Authors: Jason Lowe-Power
 
-Import('*')
+"""
+A simple run file that creates two SimObjects: HelloObject and GoodbyeObject
+and then runs the simulation. Using the debug "Hello" is informative.
 
-SimObject('SimpleObject.py')
-SimObject('HelloObject.py')
+IMPORTANT: If you modify this file, it's likely that the Learning gem5 book
+           also needs to be updated. For now, email Jason <power.jg@gmail.com>
 
-Source('simple_object.cc')
-Source('hello_object.cc')
-Source('goodbye_object.cc')
+"""
 
-DebugFlag('HelloExample', "For Learning gem5 Part 2. Simple example debug flag")
+# import the m5 (gem5) library created when gem5 is built
+import m5
+# import all of the SimObjects
+from m5.objects import *
+
+# set up the root SimObject and start the simulation
+root = Root(full_system = False)
+
+# Create an instantiation of the simobject you created
+root.hello = HelloObject(time_to_wait = '2us', number_of_fires = 5)
+root.hello.goodbye_object = GoodbyeObject(buffer_size='100B')
+
+# instantiate all of the objects we've created above
+m5.instantiate()
+
+print "Beginning simulation!"
+exit_event = m5.simulate()
+print 'Exiting @ tick %i because %s' % (m5.curTick(), exit_event.getCause())
--- src/learning_gem5/part2/HelloObject.py
+++ src/learning_gem5/part2/HelloObject.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2016 Jason Lowe-Power
+# Copyright (c) 2017 Jason Lowe-Power
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -27,13 +27,24 @@
 #
 # Authors: Jason Lowe-Power
 
-Import('*')
+from m5.params import *
+from m5.SimObject import SimObject
 
-SimObject('SimpleObject.py')
-SimObject('HelloObject.py')
+class HelloObject(SimObject):
+    type = 'HelloObject'
+    cxx_header = "learning_gem5/part2/hello_object.hh"
 
-Source('simple_object.cc')
-Source('hello_object.cc')
-Source('goodbye_object.cc')
+    time_to_wait = Param.Latency("Time before firing the event")
+    number_of_fires = Param.Int(1, "Number of times to fire the event before "
+                                   "goodbye")
 
-DebugFlag('HelloExample', "For Learning gem5 Part 2. Simple example debug flag")
+    goodbye_object = Param.GoodbyeObject("A goodbye object")
+
+class GoodbyeObject(SimObject):
+    type = 'GoodbyeObject'
+    cxx_header = "learning_gem5/part2/goodbye_object.hh"
+
+    buffer_size = Param.MemorySize('1kB',
+                                   "Size of buffer to fill with goodbye")
+    write_bandwidth = Param.MemoryBandwidth('100MB/s', "Bandwidth to fill "
+                                            "the buffer")
