ba7a202ffac695dd2c6c9c7254d34d89dea68eaf
Gabe Black <gabeblack@google.com>
2018-01-29 14:38:37 -0800

    base: Delete commented out versions of the format_integer function.
    If they're needed, they'd be fairly easy to recreate and are also
    available in the revision history.
    
    Change-Id: I5cf5e4b1271ce488016464048de69bc643dee4d9
    Reviewed-on: https://gem5-review.googlesource.com/7641
    Reviewed-by: Jason Lowe-Power <jason@lowepower.com>
    Maintainer: Gabe Black <gabeblack@google.com>

--- src/base/cprintf_formats.hh
+++ src/base/cprintf_formats.hh
@@ -312,32 +312,6 @@ format_integer(std::ostream &out, unsigned char data, Format &fmt)
 inline void
 format_integer(std::ostream &out, signed char data, Format &fmt)
 { _format_integer(out, (int)data, fmt); }
-#if 0
-inline void
-format_integer(std::ostream &out, short data, Format &fmt)
-{ _format_integer(out, data, fmt); }
-inline void
-format_integer(std::ostream &out, unsigned short data, Format &fmt)
-{ _format_integer(out, data, fmt); }
-inline void
-format_integer(std::ostream &out, int data, Format &fmt)
-{ _format_integer(out, data, fmt); }
-inline void
-format_integer(std::ostream &out, unsigned int data, Format &fmt)
-{ _format_integer(out, data, fmt); }
-inline void
-format_integer(std::ostream &out, long data, Format &fmt)
-{ _format_integer(out, data, fmt); }
-inline void
-format_integer(std::ostream &out, unsigned long data, Format &fmt)
-{ _format_integer(out, data, fmt); }
-inline void
-format_integer(std::ostream &out, long long data, Format &fmt)
-{ _format_integer(out, data, fmt); }
-inline void
-format_integer(std::ostream &out, unsigned long long data, Format &fmt)
-{ _format_integer(out, data, fmt); }
-#endif
 
 //
 // floating point formats
