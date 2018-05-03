e1e908fceb3236435e4d8ec4637488ebd89e6c77
Tony Gutierrez <anthony.gutierrez@amd.com>
2018-05-03 14:00:00 -0400

    arch-x86: Enable fstatfs for x86_64
    Change-Id: Ic871f852c4892f2228f0d9bb3cc5cb66887d9736
    Reviewed-on: https://gem5-review.googlesource.com/10201
    Reviewed-by: Gabe Black <gabeblack@google.com>
    Maintainer: Gabe Black <gabeblack@google.com>

--- src/arch/x86/linux/process.cc
+++ src/arch/x86/linux/process.cc
@@ -358,7 +358,7 @@ static SyscallDesc syscallDescs64[] = {
     /* 135 */ SyscallDesc("personality", unimplementedFunc),
     /* 136 */ SyscallDesc("ustat", unimplementedFunc),
     /* 137 */ SyscallDesc("statfs", statfsFunc<X86Linux64>),
-    /* 138 */ SyscallDesc("fstatfs", unimplementedFunc),
+    /* 138 */ SyscallDesc("fstatfs", fstatfsFunc<X86Linux64>),
     /* 139 */ SyscallDesc("sysfs", unimplementedFunc),
     /* 140 */ SyscallDesc("getpriority", unimplementedFunc),
     /* 141 */ SyscallDesc("setpriority", ignoreFunc),
