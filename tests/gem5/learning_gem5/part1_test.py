b2b9285809c47dc7e757c2747a8a5497087b11ec
Rutuja Oza <roza@ucdavis.edu>
2019-03-11 15:26:52 -0700

    tests: Add tests for learning_gem5 configs
    Change-Id: Ia9cbb77115c741051c871526517bb7d8124a8051
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/17873
    Reviewed-by: Jason Lowe-Power <jason@lowepower.com>
    Maintainer: Jason Lowe-Power <jason@lowepower.com>

---
+++ tests/gem5/learning_gem5/part1_test.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2019 The Regents of the University of California.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: Jason Lowe-Power
+
+from testlib import *
+
+config_path = joinpath(config.base_dir, 'configs', 'learning_gem5', 'part1')
+
+gem5_verify_config(
+    name='simple_test',
+    verifiers = (),
+    config=joinpath(config_path, 'simple.py'),
+    config_args = [],
+    valid_isas=('X86', 'RISCV', 'ARM'),
+)
+
+gem5_verify_config(
+    name='two_level_test',
+    verifiers = (),
+    config=joinpath(config_path, 'two_level.py'),
+    config_args = [],
+    valid_isas=('X86', 'RISCV', 'ARM'),
+)
+
---
+++ tests/gem5/learning_gem5/part3_test.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2019 The Regents of the University of California.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors: Jason Lowe-Power
+
+from testlib import *
+
+# Note: This isn't going to work because MSI caches won't be built. Need to
+# think more about this. Maybe we should have another parameter to
+# gem5_verify_config...
+
+config_path = joinpath(config.base_dir, 'configs', 'learning_gem5', 'part3')
+ref_path = joinpath(getcwd(), 'ref')
+
+gem5_verify_config(
+    name='simple_ruby_test',
+    verifiers = (verifier.MatchStdoutNoPerf(joinpath(ref_path, 'threads')),),
+    config=joinpath(config_path, 'simple_ruby.py'),
+    config_args = [],
+    protocol = 'MSI',
+    valid_isas=("X86",), # Currently only x86 has the threads test
+)
+
+gem5_verify_config(
+    name='ruby_test_test',
+    verifiers = (verifier.MatchStdout(joinpath(ref_path, 'test')),),
+    config=joinpath(config_path, 'ruby_test.py'),
+    config_args = [],
+    protocol = 'MSI',
+    valid_isas=("X86",), # Currently only x86 has the threads test
+)
