f4b4ff8d442b2cc73f6575391d15f6bc8aaba277
Tiago Muck <tiago.muck@arm.com>
2019-04-17 15:02:29 -0500

    mem-ruby: Check on PerfectCacheMemory deallocate
    Allowing deallocate to be called for non-existing blocks may hide
    potential bugs.
    
    Change-Id: Ida77e2db1da59d7cdb21d58968e1f17e75eaa6e0
    Signed-off-by: Tiago Mück <tiago.muck@arm.com>
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/21921
    Reviewed-by: Pouya Fotouhi <pfotouhi@ucdavis.edu>
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>
    Maintainer: Jason Lowe-Power <power.jg@gmail.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- src/mem/ruby/structures/PerfectCacheMemory.hh
+++ src/mem/ruby/structures/PerfectCacheMemory.hh
@@ -1,4 +1,16 @@
 /*
+ * Copyright (c) 2019 ARM Limited
+ * All rights reserved.
+ *
+ * The license below extends only to copyright in the software and shall
+ * not be construed as granting a license to any other intellectual
+ * property including but not limited to intellectual property relating
+ * to a hardware implementation of the functionality of the software
+ * licensed hereunder.  You may use the software subject to the license
+ * terms below provided that you ensure that this notice is replicated
+ * unmodified and in its entirety in all distributions of the software,
+ * modified or unmodified, in source code or in binary form.
+ *
  * Copyright (c) 1999-2008 Mark D. Hill and David A. Wood
  * All rights reserved.
  *
@@ -138,7 +150,8 @@ template<class ENTRY>
 inline void
 PerfectCacheMemory<ENTRY>::deallocate(Addr address)
 {
-    m_map.erase(makeLineAddress(address));
+    auto num_erased M5_VAR_USED = m_map.erase(makeLineAddress(address));
+    assert(num_erased == 1);
 }
 
 // Returns with the physical address of the conflicting cache line
