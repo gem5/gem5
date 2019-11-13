cbdf361f3f10ddfc351c24a1baab85a648d5e16d
Bobby R. Bruce <bbruce@ucdavis.edu>
2019-11-12 18:57:36 -0800

    test,base: Added GTest for base/loader/image_file_data.cc
    image_file_data.cc reads an image file, or an image file compressed with
    gzip. Mock image file data, and that data in a gzipped state, has been
    included in base/loader/small_image_file.test.hh to aid testing.
    
    Change-Id: I69691b93ca03c34d6bd736cbc5c6503115bd7b3f
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/22743
    Reviewed-by: Daniel Carvalho <odanrc@yahoo.com.br>
    Reviewed-by: Jason Lowe-Power <jason@lowepower.com>
    Maintainer: Jason Lowe-Power <jason@lowepower.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

---
+++ src/base/loader/image_file_data.test.cc
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2019 The Regents of the University of California
+ * All rights reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: Bobby R. Bruce
+ */
+
+#include <gtest/gtest.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cstdlib>
+
+#include "base/loader/image_file_data.hh"
+#include "base/loader/small_image_file.test.hh"
+
+TEST(ImageFileDataTest, SimpleImage)
+{
+    /*
+     * Create a temporary file from our data blob.
+     */
+    char filename[] = "image-XXXXXX";
+    int fd = mkstemp(filename);
+    ASSERT_NE(-1, fd);
+    ssize_t size = write(fd, image_file, sizeof(image_file));
+
+    /*
+     * In this basic test, the image file is simply loaded to the
+     * ImageFileData object (i.e., no decompression).
+     */
+    ImageFileData idf(filename);
+
+    EXPECT_EQ(idf.filename(), filename);
+    EXPECT_EQ(size, idf.len());
+
+    for (size_t i = 0; i < sizeof(image_file); i++) {
+        EXPECT_EQ(image_file[i], idf.data()[i]);
+    }
+
+    /*
+     * Close and delete the temporary file.
+     */
+    close(fd);
+    unlink(filename);
+}
+
+
+TEST(ImageFileDataTest, GZipImage)
+{
+    /*
+     * Create temporary files from our data blobs.
+     */
+    char filename_gz[] = "image-XXXXXX";
+    int fd_gz = mkstemp(filename_gz);
+    ASSERT_NE(-1, fd_gz);
+    ssize_t size_gz = write(fd_gz, image_file_gzipped,
+                            sizeof(image_file_gzipped));
+
+    char filename[] = "image-XXXXXX";
+    int fd = mkstemp(filename);
+    ASSERT_NE(-1, fd);
+    ssize_t size = write(fd, image_file, sizeof(image_file));
+
+    /*
+     * ImageFileData decompresses a gzipped file. image_file_gzipped is just
+     * image_file gzipped. Therefore ifd_gz.len() should equal ifd.len() and
+     * ifd.data() should equal ifd_gz.data().
+     */
+    ImageFileData ifd_gz(filename_gz);
+    ImageFileData ifd(filename);
+
+    EXPECT_EQ(ifd.len(), ifd_gz.len());
+    EXPECT_EQ(size, ifd.len());
+    EXPECT_NE(size_gz, ifd_gz.len());
+
+    for (size_t index = 0; index < ifd.len(); index++) {
+        EXPECT_EQ(ifd.data()[index], ifd_gz.data()[index]);
+    }
+
+    /*
+     * Close and delete the temporary files.
+     */
+    close(fd);
+    unlink(filename);
+    close(fd_gz);
+    unlink(filename_gz);
+}
