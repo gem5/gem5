1fd1d2ed267757e4ea50faeaf9c6d784aa1f0049
Giacomo Travaglini <giacomo.travaglini@arm.com>
2019-11-13 13:47:19 +0000

    tests: Specify a non-default root folder for regressions
    The new testlib library is looking for regressions walking from
    a root folder. This by default points to the tests dir.
    Since all regressions are supposed to live in the tests/gem5 subdir,
    the patch is assigning the gem5 subdir as a root directory.
    
    This will prevent the example garbage to be printed in the ci framework:
    
    Exception thrown while loading
    "/tmpfs/src/git/jenkins-gem5-prod/tests/long/fs/10.linux-boot/test.py"
    Ignoring all tests in this file.
    Exception thrown while loading
    "/tmpfs/src/git/jenkins-gem5-prod/tests/long/fs/80.solaris-boot/test.py"
    Ignoring all tests in this file.
    [...]
    
    Change-Id: Ia12c6bbeda4ceac71ccd38156ab1e3bb98b05c89
    Signed-off-by: Giacomo Travaglini <giacomo.travaglini@arm.com>
    Reviewed-by: Nikos Nikoleris <nikos.nikoleris@arm.com>
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/22726
    Reviewed-by: Jason Lowe-Power <jason@lowepower.com>
    Maintainer: Jason Lowe-Power <jason@lowepower.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- tests/jenkins/presubmit.sh
+++ tests/jenkins/presubmit.sh
@@ -46,4 +46,5 @@ cd git/jenkins-gem5-prod/tests
 
 # Build with 4 threads (i.e., pass -j4 to scons)
 # Run 4 tests in parallel
-./main.py run -j4 -t4
+# Look for tests in the gem5 subdirectory
+./main.py run -j4 -t4 gem5
