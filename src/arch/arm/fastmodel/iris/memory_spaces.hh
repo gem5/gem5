bc65d49ce3e46485711567bab697f98db271adc5
Gabe Black <gabeblack@google.com>
2019-12-13 00:24:43 -0800

    fastmodel: Add a header for IRIS MSN constants.
    Change-Id: I06a7d7db95ec1ce65945c9e09f812f0b69aaa8e6
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/23643
    Reviewed-by: Gabe Black <gabeblack@google.com>
    Reviewed-by: Giacomo Travaglini <giacomo.travaglini@arm.com>
    Maintainer: Gabe Black <gabeblack@google.com>
    Maintainer: Giacomo Travaglini <giacomo.travaglini@arm.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

---
+++ src/arch/arm/fastmodel/iris/memory_spaces.hh
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019 Google, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: Gabe Black
+ */
+
+#ifndef __ARCH_ARM_FASTMODEL_IRIS_MEMORY_SPACES_HH__
+#define __ARCH_ARM_FASTMODEL_IRIS_MEMORY_SPACES_HH__
+
+namespace Iris
+{
+
+enum CanonicalMsn
+{
+    SecureMonitorMsn = 0x1000,
+    GuestMsn = 0x1001,
+    NsHypMsn = 0x1002,
+    MemoryMsn = 0x1003,
+    HypAppMsn = 0x1004,
+    HostMsn = 0x1005,
+    CurrentMsn = 0x10ff,
+    IpaMsn = 0x1100,
+    PhysicalMemorySecureMsn = 0x1200,
+    PhysicalMemoryNonSecureMsn = 0x1201,
+    PhysicalMemoryMsn = 0x1202
+};
+
+} // namespace Iris
+
+#endif // __ARCH_ARM_FASTMODEL_IRIS_MEMORY_SPACES_HH__
