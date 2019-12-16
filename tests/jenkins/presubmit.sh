41a8800950f706536111dd170dc487a8453574a1
Bobby R. Bruce <bbruce@ucdavis.edu>
2019-12-16 14:07:22 -0800

    tests: Setup Kokoro to run the GTest suite.
    Change-Id: If700eed24b2902d04a9b0ee72b72e9e6a3472ef5
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/23724
    Reviewed-by: Daniel Carvalho <odanrc@yahoo.com.br>
    Reviewed-by: Jason Lowe-Power <jason@lowepower.com>
    Maintainer: Jason Lowe-Power <jason@lowepower.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- tests/jenkins/presubmit.sh
+++ tests/jenkins/presubmit.sh
@@ -47,4 +47,5 @@ cd git/jenkins-gem5-prod/tests
 # Build with 4 threads (i.e., pass -j4 to scons)
 # Run 4 tests in parallel
 # Look for tests in the gem5 subdirectory
-./main.py run -j4 -t4 gem5
+# Once complete, run the Google Tests
+./main.py run -j4 -t4 gem5 && scons -C .. build/NULL/unittests.opt
