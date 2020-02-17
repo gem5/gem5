766e19efce701d24a8c048c3c67816a034d8fc75
Gabe Black <gabeblack@google.com>
2020-02-16 23:57:13 -0800

    mips: Delete authors lists from mips files.
    Change-Id: I56c054c64fe3d1e39ed5d315b8ac78de2e993dc5
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/25449
    Reviewed-by: Daniel Carvalho <odanrc@yahoo.com.br>
    Maintainer: Gabe Black <gabeblack@google.com>
    Maintainer: Bobby R. Bruce <bbruce@ucdavis.edu>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- src/arch/mips/faults.hh
+++ src/arch/mips/faults.hh
@@ -25,12 +25,6 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Authors: Gabe Black
- *          Korey Sewell
- *          Jaidev Patwardhan
- *          Zhengxing Li
- *          Deyuan Guo
  */
 
 #ifndef __MIPS_FAULTS_HH__
