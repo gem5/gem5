648b7571208deff91dabe8d72f33367b62ec7d3e
Gabe Black <gabeblack@google.com>
2020-02-16 23:11:01 -0800

    arch: Delete authors lists from generic arch files.
    Change-Id: I831a0f1876845f37ab12a2448e898719e74a0b55
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/25445
    Reviewed-by: Daniel Carvalho <odanrc@yahoo.com.br>
    Maintainer: Bobby R. Bruce <bbruce@ucdavis.edu>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- src/arch/generic/vec_reg.hh
+++ src/arch/generic/vec_reg.hh
@@ -33,10 +33,6 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Authors: Giacomo Gabrielli
- *          Nathanael Premillieu
- *          Rekai Gonzalez
  */
 
 /** \file arch/generic/vec_reg.hh
