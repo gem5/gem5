6362e8b2d2f1bfd3f6686fda8c43469b65b768c9
Nils Asmussen <nils.asmussen@barkhauseninstitut.org>
2020-02-18 08:54:41 +0100

    arch-riscv: added support for pseudo instructions.
    Change-Id: I4f73f8fcf62def8815e82555fc2a67f89efc09d1
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/25645
    Tested-by: Gem5 Cloud Project GCB service account <345032938727@cloudbuild.gserviceaccount.com>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Gabe Black <gabeblack@google.com>
    Maintainer: Gabe Black <gabeblack@google.com>

--- src/arch/riscv/utility.hh
+++ src/arch/riscv/utility.hh
@@ -2,6 +2,7 @@
  * Copyright (c) 2013 ARM Limited
  * Copyright (c) 2014-2015 Sven Karlsson
  * Copyright (c) 2018 TU Dresden
+ * Copyright (c) 2020 Barkhausen Institut
  * All rights reserved
  *
  * The license below extends only to copyright in the software and shall
@@ -109,7 +110,14 @@ buildRetPC(const PCState &curPC, const PCState &callPC)
 inline uint64_t
 getArgument(ThreadContext *tc, int &number, uint16_t size, bool fp)
 {
-    return 0;
+    panic_if(fp, "getArgument(): Floating point arguments not implemented");
+    panic_if(size != 8, "getArgument(): Can only handle 64-bit arguments.");
+    panic_if(number >= ArgumentRegs.size(),
+             "getArgument(): Don't know how to handle stack arguments");
+
+    // The first 8 integer arguments are passed in registers, the rest
+    // are passed on the stack.
+    return tc->readIntReg(ArgumentRegs[number]);
 }
 
 inline void
