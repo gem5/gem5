9e4209cd3eaa889ae48328983ee067c22ece4778
Bobby R. Bruce <bbruce@ucdavis.edu>
2020-02-25 17:38:15 -0800

    tests: Removed old scon-based 40.m5threads-test-atomic tests
    These have been migrated to be run via testlib.
    
    Change-Id: I186e4048096f718c0de378033924cd23328168d7
    Jira: https://gem5.atlassian.net/browse/GEM5-109
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/25843
    Reviewed-by: Bobby R. Bruce <bbruce@ucdavis.edu>
    Maintainer: Bobby R. Bruce <bbruce@ucdavis.edu>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- tests/quick/se/40.m5threads-test-atomic/test.py
+++
@@ -1,5 +0,0 @@
-process = Process(executable = binpath('m5threads', 'test_atomic'),
-                  cmd = ['test_atomic', str(nb_cores)])
-
-for i in range(nb_cores):
-    root.system.cpu[i].workload = process
