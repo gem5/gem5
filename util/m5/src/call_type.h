26454e8072e607d54ac67c42b33355d7c94d6d60
Gabe Black <gabeblack@google.com>
2020-03-27 01:52:27 -0700

    util: Add an abstraction layer for call types in the m5 utility.
    These make the calling code in m5.c a little bit more generic. Each call
    type will have a function to check the arguments and see if that type is
    being requested and/or has any additional options set in the arguments.
    If so, those are processed, and argc and argv are adjusted.
    
    Then another function returns the appropriate dispatch table to use for
    that invocation scheme. This is behind a function instead of, for
    instance, a global variable because it gives the call type a little bit
    more control over what's happening, for instance if it would use
    different implementations in slightly different circumstances.
    
    Change-Id: I661cf202ec657466496767cbdf331fe27995ab26
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/27241
    Maintainer: Gabe Black <gabeblack@google.com>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Bobby R. Bruce <bbruce@ucdavis.edu>

---
+++ util/m5/src/call_type.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __CALL_TYPE_H__
+#define __CALL_TYPE_H__
+
+#include "dispatch_table.h"
+
+#if ENABLE_CT_addr
+#include "addr_call_type.h"
+#endif
+#if ENABLE_CT_inst
+#include "inst_call_type.h"
+#endif
+#if ENABLE_CT_semi
+#include "semi_call_type.h"
+#endif
+
+#define default_call_type_init() \
+    M5OP_MERGE_TOKENS(DEFAULT_CALL_TYPE, _call_type_init())
+
+#endif // __CALL_TYPE_H__
