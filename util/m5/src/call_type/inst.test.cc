45beec4a59f6ec6c71b6632665a628703bf45dd7
Gabe Black <gabeblack@google.com>
2020-04-09 02:33:56 -0700

    util: Add a unit test for the "inst" call type in the m5 util.
    This test does two things. First, it makes sure that the "inst" call
    type detects that it's being requested in the command line arguments
    correctly.
    
    Second, it detects whether it's running in gem5 or not, really just
    detecting an environment variable which tells it whether it is. If it
    is, then it attempts to run the "sum" op which it expects to succeed and
    give the right answer.
    
    If not, it expects to get a SIGILL signal from the OS when it tries to
    execute the otherwise illegal instruction. It sets up a signal handler
    to catch it, and in that handler saves off information about what
    happened. It then uses siglongjmp to return to sanity (before the
    signal) and to examine what happened to see if the right instruction was
    attempted.
    
    It looks like, depending on the architecture, Linux will either set
    si_code to ILL_ILLOPC (illegal opcode) or ILL_ILLOPN (illegal operand).
    The later doesn't seem right since the entire instruction is illegal,
    not just some operand, but it is what it is and we need to handle
    either.
    
    The test then calls a small function, abi_verify, which takes the
    siginfo_t and does any abi specific verification. That includes
    extracting fields from the instruction if the instruction trigger the
    signal, or checking for architecture specific constants, etc.
    
    Also, to centralize setting the macro which lets a call type know that
    it's the default, the call types are now also responsible for setting up
    their own tweaks to the environment.
    
    Change-Id: I8710e39e20bd9c03b1375a2dccefb27bd6fe0c10
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/27689
    Reviewed-by: Giacomo Travaglini <giacomo.travaglini@arm.com>
    Maintainer: Gabe Black <gabeblack@google.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- util/m5/src/call_type/inst.test.cc
+++ util/m5/src/call_type/inst.test.cc
@@ -27,8 +27,137 @@
 
 #include <gtest/gtest.h>
 
+#include <csetjmp>
+#include <csignal>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+#include <gem5/asm/generic/m5ops.h>
+
+#include "args.hh"
 #include "call_type.hh"
+#include "call_type/verify_inst.hh"
+#include "dispatch_table.hh"
+
+class DefaultCallType : public CallType
+{
+  private:
+    DispatchTable dt;
+
+  public:
+    DefaultCallType() : CallType("default") {}
+
+    bool init_called = false;
+    void init() override { init_called = true; }
+
+    bool isDefault() const override { return true; }
+    void printDesc(std::ostream &os) const override {}
+    const DispatchTable &getDispatch() const override { return dt; }
+};
+
+DefaultCallType defaultCallType;
+
+TEST(InstCallType, Detect)
+{
+    CallType *ct;
+
+    // Inst should not be selected if there are no arguments.
+    Args empty({});
+    defaultCallType.init_called = false;
+    ct = CallType::detect(empty);
+    EXPECT_EQ(ct, &defaultCallType);
+    EXPECT_TRUE(defaultCallType.init_called);
+
+    // Inst should not be selected if --inst isn't the first argument.
+    Args one_arg({"one"});
+    defaultCallType.init_called = false;
+    ct = CallType::detect(one_arg);
+    EXPECT_EQ(ct, &defaultCallType);
+    EXPECT_TRUE(defaultCallType.init_called);
+
+    // Inst should be selected if --inst is the first argument.
+    Args selected({"--inst"});
+    defaultCallType.init_called = false;
+    ct = CallType::detect(selected);
+    EXPECT_NE(ct, &defaultCallType);
+    EXPECT_NE(ct, nullptr);
+    EXPECT_FALSE(defaultCallType.init_called);
+
+    Args extra({"--inst", "foo"});
+    defaultCallType.init_called = false;
+    ct = CallType::detect(extra);
+    EXPECT_NE(ct, &defaultCallType);
+    EXPECT_NE(ct, nullptr);
+    EXPECT_FALSE(defaultCallType.init_called);
+
+    // Inst should not be selected if --inst isn't first.
+    Args not_first({"foo", "--inst"});
+    defaultCallType.init_called = false;
+    ct = CallType::detect(not_first);
+    EXPECT_EQ(ct, &defaultCallType);
+    EXPECT_TRUE(defaultCallType.init_called);
+}
+
+sigjmp_buf interceptEnv;
+siginfo_t interceptSiginfo;
+
+void
+sigill_handler(int sig, siginfo_t *info, void *ucontext)
+{
+    std::memcpy(&interceptSiginfo, info, sizeof(interceptSiginfo));
+    siglongjmp(interceptEnv, 1);
+}
 
 TEST(InstCallType, Sum)
 {
+    // Get the inst call type, which is in an anonymous namespace.
+    Args args({"--inst"});
+    CallType *inst_call_type = CallType::detect(args);
+    EXPECT_NE(inst_call_type, nullptr);
+
+    // Get the dispatch table associated with it.
+    const auto &dt = inst_call_type->getDispatch();
+
+    // Determine if we're running within gem5 by checking whether a flag is
+    // set in the environment.
+    bool in_gem5 = (std::getenv("RUNNING_IN_GEM5") != nullptr);
+    if (in_gem5)
+        std::cout << "In gem5, m5 ops should work." << std::endl;
+    else
+        std::cout << "Not in gem5, m5 ops won't work." << std::endl;
+
+    // If it is, then we should be able to run the "sum" command.
+    if (in_gem5) {
+        EXPECT_EQ((*dt.m5_sum)(2, 2, 0, 0, 0, 0), 4);
+        return;
+    }
+
+    // If not, then we'll need to try to catch the fall out from trying to run
+    // an m5 op and verify that what we were trying looks correct.
+
+    struct sigaction sigill_action;
+    std::memset(&sigill_action, 0, sizeof(sigill_action));
+    sigill_action.sa_sigaction = &sigill_handler;
+    sigill_action.sa_flags = SA_SIGINFO | SA_RESETHAND;
+
+    struct sigaction old_sigill_action;
+
+    sigaction(SIGILL, &sigill_action, &old_sigill_action);
+
+    if (!sigsetjmp(interceptEnv, 1)) {
+        (*dt.m5_sum)(2, 2, 0, 0, 0, 0);
+        sigaction(SIGILL, &old_sigill_action, nullptr);
+        ADD_FAILURE() << "Didn't die when attempting to run \"sum\".";
+        return;
+    }
+
+    // Back from siglongjump.
+    auto &info = interceptSiginfo;
+
+    EXPECT_EQ(info.si_signo, SIGILL);
+    EXPECT_TRUE(info.si_code == ILL_ILLOPC || info.si_code == ILL_ILLOPN);
+
+    // Do abi specific verification.
+    abi_verify_inst(info, M5OP_SUM);
 }
