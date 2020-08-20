7c82d1b8d5db9081306628a9d2024b7aaf3ec63c
Bobby R. Bruce <bbruce@ucdavis.edu>
2020-08-20 12:42:02 -0700

    tests: Removed the ignoring of tests
    This commit reverts
    https://gem5-review.googlesource.com/c/public/gem5/+/23023.
    
    It has proven to be an unpopular piece of functionality which makes it
    too easy to silently ignore failing tests. The new policy will be to
    remove/comment-out failing tests in the testing source and tag with Jira
    issues as to why.
    
    Change-Id: I17d69fc57a9171ce3702e019615390a9aa3da250
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/33139
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>
    Maintainer: Jason Lowe-Power <power.jg@gmail.com>

--- tests/gem5/suite.py
+++ tests/gem5/suite.py
@@ -88,15 +88,6 @@ def gem5_verify_config(name,
     fixtures = list(fixtures)
     testsuites = []
 
-    # Obtain the set of tests to ignore. This is found in the
-    # ".testignore" file.
-    __location__ = os.path.realpath(
-        os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    _test_ignore_file_loc = os.path.join(__location__,".testignore")
-    ignore = set()
-    if os.path.exists(_test_ignore_file_loc):
-        ignore.update(open(_test_ignore_file_loc).read().splitlines())
-
     for host in valid_hosts:
         for opt in valid_variants:
             for isa in valid_isas:
@@ -114,11 +105,6 @@ def gem5_verify_config(name,
                         opt=opt)
                 if protocol:
                     _name += '-'+protocol
-
-                # We check to see if this test suite is to be ignored. If so,
-                # we skip it.
-                if _name in ignore:
-                    continue
 
                 # Create the running of gem5 subtest.  NOTE: We specifically
                 # create this test before our verifiers so this is listed
