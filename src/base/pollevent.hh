1c0976534644be5c2aadd115886413270a7ed0c0
Hoa Nguyen <hoanguyen@ucdavis.edu>
2020-08-20 16:40:59 -0700

    base: Tag API methods in pollevent.hh
    Change-Id: Ic2eb596c873fc4917cf650c2e240310b1e025fa5
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/33114
    Reviewed-by: Bobby R. Bruce <bbruce@ucdavis.edu>
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>
    Maintainer: Bobby R. Bruce <bbruce@ucdavis.edu>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- src/base/pollevent.hh
+++ src/base/pollevent.hh
@@ -49,13 +49,24 @@ class PollEvent : public Serializable
     bool enabled;
 
   public:
+    /**
+     * @ingroup api_poll_event
+     */
     PollEvent(int fd, int event);
     virtual ~PollEvent();
 
+    /**
+     * @ingroup api_poll_event
+     * @{
+     */
     void disable();
     void enable();
     virtual void process(int revent) = 0;
+    /** @} */ // end of api_poll_event
 
+    /**
+     * @ingroup api_poll_event
+     */
     bool queued() { return queue != 0; }
 
     void serialize(CheckpointOut &cp) const override;
@@ -73,18 +84,30 @@ class PollQueue
     int num_fds;
 
   public:
+    /**
+     * @ingroup api_poll_queue
+     */
     PollQueue();
     ~PollQueue();
 
+    /**
+     * @ingroup api_poll_queue
+     * @{
+     */
     void copy();
     void remove(PollEvent *event);
     void schedule(PollEvent *event);
     void service();
+    /** @} */ // end of api_poll_queue
+
 
   public:
     static void setupAsyncIO(int fd, bool set);
 };
 
+/**
+ * @ingroup api_poll_queue
+ */
 extern PollQueue pollQueue;
 
 #endif // __POLLEVENT_H__
