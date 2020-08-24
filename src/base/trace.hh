c314c1fa9eac8452c296f9096aa3e54d315fdf1b
Hoa Nguyen <hoanguyen@ucdavis.edu>
2020-08-23 21:56:06 -0700

    base: Tag API methods to trace.hh
    Change-Id: Ia2ad759b9bee4fd3413abd62cfa6b120616a14f9
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/33255
    Reviewed-by: Bobby R. Bruce <bbruce@ucdavis.edu>
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>
    Maintainer: Bobby R. Bruce <bbruce@ucdavis.edu>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- src/base/trace.hh
+++ src/base/trace.hh
@@ -156,15 +156,27 @@ class Named
     const std::string &name() const { return _name; }
 };
 
-//
-// DPRINTF is a debugging trace facility that allows one to
-// selectively enable tracing statements.  To use DPRINTF, there must
-// be a function or functor called name() that returns a const
-// std::string & in the current scope.
-//
-// If you desire that the automatic printing not occur, use DPRINTFR
-// (R for raw)
-//
+/**
+ * DPRINTF is a debugging trace facility that allows one to
+ * selectively enable tracing statements.  To use DPRINTF, there must
+ * be a function or functor called name() that returns a const
+ * std::string & in the current scope.
+ *
+ * If you desire that the automatic printing not occur, use DPRINTFR
+ * (R for raw)
+ *
+ * \def DDUMP(x, data, count)
+ * \def DPRINTF(x, ...)
+ * \def DPRINTFS(x, s, ...)
+ * \def DPRINTFR(x, ...)
+ * \def DDUMPN(data, count)
+ * \def DPRINTFN(...)
+ * \def DPRINTFNR(...)
+ * \def DPRINTF_UNCONDITIONAL(x, ...)
+ *
+ * @ingroup api_trace
+ * @{
+ */
 
 #if TRACING_ON
 
@@ -228,5 +240,7 @@ class Named
 #define DPRINTF_UNCONDITIONAL(x, ...) do {} while (0)
 
 #endif  // TRACING_ON
+
+/** @} */ // end of api_trace
 
 #endif // __BASE_TRACE_HH__
