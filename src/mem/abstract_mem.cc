b1c70250c4fc111b78b30266cf290a1d3c5bfb0a
Gabe Black <gabeblack@google.com>
2020-09-14 20:15:37 -0700

    mem: Remove #if THE_ISA in the AbstractMemory class.
    This used to guard the extraction of the endianness when tracing memory
    accesses. Since that's now always possible even in NULL_ISA, we don't
    need conditional compilation.
    
    Change-Id: Ie5ec76f5b0f27dd4123bc0f0a4c02438bed629ad
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/34499
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>
    Reviewed-by: Nikos Nikoleris <nikos.nikoleris@arm.com>
    Maintainer: Gabe Black <gabeblack@google.com>

--- src/mem/abstract_mem.cc
+++ src/mem/abstract_mem.cc
@@ -345,17 +345,15 @@ static inline void
 tracePacket(System *sys, const char *label, PacketPtr pkt)
 {
     int size = pkt->getSize();
-#if THE_ISA != NULL_ISA
     if (size == 1 || size == 2 || size == 4 || size == 8) {
         ByteOrder byte_order = sys->getGuestByteOrder();
-        DPRINTF(MemoryAccess,"%s from %s of size %i on address %#x data "
+        DPRINTF(MemoryAccess, "%s from %s of size %i on address %#x data "
                 "%#x %c\n", label, sys->getRequestorName(pkt->req->
                 requestorId()), size, pkt->getAddr(),
                 size, pkt->getAddr(), pkt->getUintX(byte_order),
                 pkt->req->isUncacheable() ? 'U' : 'C');
         return;
     }
-#endif
     DPRINTF(MemoryAccess, "%s from %s of size %i on address %#x %c\n",
             label, sys->getRequestorName(pkt->req->requestorId()),
             size, pkt->getAddr(), pkt->req->isUncacheable() ? 'U' : 'C');
