ab65f6acc5ccb64299d34ddf5e1ca731d55201f5
Gabe Black <gabeblack@google.com>
2020-10-13 04:01:00 -0700

    systemc: Use the new M5_WEAK macro to hide [[gnu::weak]].
    Other compilers may need to support that macro in other ways.
    
    Change-Id: If6ee05ce69c1bfd24108cf0785fac2d7bc259f0a
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/35940
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Nikos Nikoleris <nikos.nikoleris@arm.com>
    Maintainer: Gabe Black <gabeblack@google.com>

--- src/systemc/core/sc_main_fiber.cc
+++ src/systemc/core/sc_main_fiber.cc
@@ -38,7 +38,7 @@
 #include "systemc/utils/report.hh"
 
 // A weak symbol to detect if sc_main has been defined, and if so where it is.
-[[gnu::weak]] int sc_main(int argc, char *argv[]);
+M5_WEAK int sc_main(int argc, char *argv[]);
 
 namespace sc_gem5
 {
