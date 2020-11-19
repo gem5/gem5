adbd2d0138f3480d7f4ebec96ca2a1f6b4c3a1b9
Giacomo Travaglini <giacomo.travaglini@arm.com>
2020-11-19 18:15:13 +0000

    python: Remove SortedDict from python utilities
    The SortedDict isn't actually used. A developer willing to
    use a sorted dictionary should resort to the collections.OrderedDict
    instead
    
    Change-Id: Ia2cc664eb01e59b197218ccf40ff9c680a410fb2
    Signed-off-by: Giacomo Travaglini <giacomo.travaglini@arm.com>
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/37796
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>
    Reviewed-by: Hoa Nguyen <hoanguyen@ucdavis.edu>
    Maintainer: Jason Lowe-Power <power.jg@gmail.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- src/python/m5/util/__init__.py
+++ src/python/m5/util/__init__.py
@@ -52,7 +52,6 @@ from . import jobfile
 from .attrdict import attrdict, multiattrdict, optiondict
 from .code_formatter import code_formatter
 from .multidict import multidict
-from .sorteddict import SortedDict
 
 # panic() should be called when something happens that should never
 # ever happen regardless of what the user does (i.e., an acutal m5
