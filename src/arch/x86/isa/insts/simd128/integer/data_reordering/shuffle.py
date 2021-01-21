3d257c287397ae5370209bd05ca55d8e6a6dd3eb
Tong Shen <endlessroad@google.com>
2021-01-20 16:11:01 -0800

    arch-x86: implement PSHUFB SSE instruction.
    Change-Id: I9398f9ecb26b6aabf4015e0e285fdc2f4c2487dd
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/39495
    Reviewed-by: Tong Shen <endlessroad@google.com>
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>
    Maintainer: Jason Lowe-Power <power.jg@gmail.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- src/arch/x86/isa/insts/simd128/integer/data_reordering/shuffle.py
+++ src/arch/x86/isa/insts/simd128/integer/data_reordering/shuffle.py
@@ -84,4 +84,32 @@ def macroop PSHUFLW_XMM_P_I {
     ldfp ufp1, seg, riprel, "DISPLACEMENT", dataSize=8
     shuffle xmml, ufp1, ufp1, size=2, ext=imm
 };
+
+def macroop PSHUFB_XMM_XMM {
+    movfp ufp1, xmmlm, dataSize=8
+    movfp ufp2, xmmhm, dataSize=8
+    shuffle ufp1, xmml, xmmh, size=1, ext=0
+    shuffle ufp2, xmml, xmmh, size=1, ext=0
+    movfp xmml, ufp1, dataSize=8
+    movfp xmmh, ufp2, dataSize=8
+};
+
+def macroop PSHUFB_XMM_M {
+    ldfp ufp1, seg, sib, "DISPLACEMENT", dataSize=8
+    ldfp ufp2, seg, sib, "DISPLACEMENT + 8", dataSize=8
+    shuffle ufp1, xmml, xmmh, size=1, ext=0
+    shuffle ufp2, xmml, xmmh, size=1, ext=0
+    movfp xmml, ufp1, dataSize=8
+    movfp xmmh, ufp2, dataSize=8
+};
+
+def macroop PSHUFB_XMM_P {
+    rdip t7
+    ldfp ufp1, seg, riprel, "DISPLACEMENT", dataSize=8
+    ldfp ufp2, seg, riprel, "DISPLACEMENT + 8", dataSize=8
+    shuffle ufp1, xmml, xmmh, size=1, ext=0
+    shuffle ufp2, xmml, xmmh, size=1, ext=0
+    movfp xmml, ufp1, dataSize=8
+    movfp xmmh, ufp2, dataSize=8
+};
 '''
