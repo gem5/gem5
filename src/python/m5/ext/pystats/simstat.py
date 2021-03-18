447ba531333030dbe16de535affaf9aeb63c3b77
Kelly Nguyen <klynguyen@ucdavis.edu>
2021-03-18 13:52:46 -0700

    python: Fix optional parameters with default None
    Change-Id: I40c80c8fdd1384b3405e6b1c0b0b9f310580568d
    Signed-off-by: Kelly Nguyen <klynguyen@ucdavis.edu>
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/43605
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>
    Maintainer: Jason Lowe-Power <power.jg@gmail.com>

--- src/python/m5/ext/pystats/simstat.py
+++ src/python/m5/ext/pystats/simstat.py
@@ -42,10 +42,10 @@ class SimStat(JsonSerializable):
     simulated_begin_time: Optional[Union[int, float]]
     simulated_end_time: Optional[Union[int, float]]
 
-    def __init__(self, creation_time: Optional[datetime],
-                 time_conversion: Optional[TimeConversion],
-                 simulated_begin_time: Optional[Union[int, float]],
-                 simulated_end_time: Optional[Union[int, float]],
+    def __init__(self, creation_time: Optional[datetime] = None,
+                 time_conversion: Optional[TimeConversion] = None,
+                 simulated_begin_time: Optional[Union[int, float]] = None,
+                 simulated_end_time: Optional[Union[int, float]] = None,
                  **kwargs: Dict[str, Union[Group,Statistic,List[Group]]]):
         self.creation_time = creation_time
         self.time_conversion = time_conversion
