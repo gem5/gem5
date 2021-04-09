eff396359fe7caedeb51bf96e6b3a92453c80d09
Giacomo Travaglini <giacomo.travaglini@arm.com>
2021-04-09 14:26:00 +0100

    tests: Arm FS regressions using new guest binaries
    The aarch-system-20210904.tar.bz2 tarball has been added to reflect
    changes in the bootloader
    
    Change-Id: I5c93b92d15a0d2c9ef53ab9e25d44e6154af8fd6
    Signed-off-by: Giacomo Travaglini <giacomo.travaglini@arm.com>
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/44345
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Richard Cooper <richard.cooper@arm.com>

--- tests/gem5/fs/linux/arm/test.py
+++ tests/gem5/fs/linux/arm/test.py
@@ -93,7 +93,7 @@ arm_fs_long_tests = [
     #'realview64-o3-checker',
 ]
 
-tarball = 'aarch-system-20200611.tar.bz2'
+tarball = 'aarch-system-20210904.tar.bz2'
 url = config.resource_url + "/arm/" + tarball
 filepath = os.path.dirname(os.path.abspath(__file__))
 path = joinpath(config.bin_path, 'arm')
