59879e3794c4da69c272a58f11e01d9c510cd21d
Iru Cai <mytbk920423@gmail.com>
2021-06-03 18:33:28 +0800

    systemc: include <typeinfo>
    GCC 11.1 complains about not finding std::type_info, which is in
    header <typeinfo>.
    
    Change-Id: I3b4352c8caeac73815f569911f47a7dfbddf23f3
    Signed-off-by: Iru Cai <mytbk920423@gmail.com>
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/46399
    Reviewed-by: Gabe Black <gabe.black@gmail.com>
    Maintainer: Gabe Black <gabe.black@gmail.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- src/systemc/ext/tlm_utils/instance_specific_extensions_int.h
+++ src/systemc/ext/tlm_utils/instance_specific_extensions_int.h
@@ -19,6 +19,7 @@
 #ifndef __SYSTEMC_EXT_TLM_UTILS_INSTANCE_SPECIFIC_EXTENSIONS_INT_H__
 #define __SYSTEMC_EXT_TLM_UTILS_INSTANCE_SPECIFIC_EXTENSIONS_INT_H__
 
+#include <typeinfo>
 #include <vector>
 
 #include "../tlm_core/2/generic_payload/array.hh"
