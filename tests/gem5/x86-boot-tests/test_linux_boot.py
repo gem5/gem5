2d82333afe7056c629cbcb8f6bd5ba4b75898e3c
Bobby R. Bruce <bbruce@ucdavis.edu>
2021-08-17 17:11:37 -0700

    tests: Update the x86-boot-tests to use boot_exit_disk_run.py
    This change makes three improvements:
    
    1. It uses the gem5 components library
    `config/example/components-library/boot_exit_disk_run.py` example. This
    test therefore doubles-up as a test for this example and the gem5
    components library, as well as full-system x86 booting of Linux.
    
    2. It includes 'very-long' tests to be run weekly. These are a full
    cross-product of x86 boot tests and will help identify gem5 failures
    more quickly.
    
    3. Quick tests are added to boot the linux OS to a certain tick.
    
    Change-Id: Ie52c267f3382e37912b8ae41dc92b1ee8d06f9f9
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/49325
    Reviewed-by: Bobby R. Bruce <bbruce@ucdavis.edu>
    Maintainer: Bobby R. Bruce <bbruce@ucdavis.edu>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- tests/gem5/x86-boot-tests/test_linux_boot.py
+++ tests/gem5/x86-boot-tests/test_linux_boot.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2020 The Regents of the University of California
-# All Rights Reserved.
+# Copyright (c) 2021 The Regents of the University of California
+# All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -24,56 +24,240 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import os
+from typing import Optional
+import re
+
 from testlib import *
 
-
 if config.bin_path:
-    base_path = config.bin_path
+    resource_path = config.bin_path
 else:
-    base_path = joinpath(absdirpath(__file__), '..', 'resources',
-            'ubuntu-boot')
+    resource_path = joinpath(absdirpath(__file__), "..", "resources")
 
-image_url = config.resource_url + '/images/x86/ubuntu-18-04/base.img.gz'
-kernel_url = config.resource_url + '/kernels/x86/static/vmlinux-4.19.83'
 
-image_name = 'ubuntu-18-04-base.img'
-kernel_name = 'vmlinux-4.19.83' # 4.19 is LTS (Projected EOL: Dec, 2020)
+def test_boot(
+    cpu: str,
+    num_cpus: int,
+    mem_system: str,
+    length: str,
+    boot_type: Optional[str] = "init",
+    to_tick: Optional[int] = None,
+):
 
-image = DownloadedProgram(image_url, base_path, image_name, True)
-kernel = DownloadedProgram(kernel_url, base_path, kernel_name)
+    name = "{}-cpu_{}-cores_{}_{}_x86-boot-test".format(
+        cpu, str(num_cpus), mem_system, boot_type
+    )
+    verifiers = []
+    additional_config_args = []
 
-def support_kvm():
-    return os.access("/dev/kvm", os.R_OK | os.W_OK)
+    if to_tick != None:
+        name += "_to-tick"
+        exit_regex = re.compile(
+            "Exiting @ tick {} because simulate\(\) limit reached".format(
+                str(to_tick)
+            )
+        )
+        verifiers.append(verifier.MatchRegex(exit_regex))
+        additional_config_args.append("--tick-exit")
+        additional_config_args.append(str(to_tick))
 
-def test_boot(cpu_type, num_cpus, boot_type, host):
+    if mem_system == "mesi_two_level":
+        protocol_to_use="MESI_Two_Level"
+        isa_to_use=constants.x86_tag
+    else:
+        protocol_to_use=None
+        isa_to_use=constants.gcn3_x86_tag
+
     gem5_verify_config(
-        name = 'test-ubuntu_boot-' + cpu_type + '_cpu-' + num_cpus + '_cpus-'
-               + boot_type + '_boot',
-        verifiers = (),
-        fixtures = (image, kernel,),
-        config = joinpath(joinpath(absdirpath(__file__), 'run_exit.py')),
-        config_args = [
-            '--kernel', joinpath(base_path, kernel_name),
-            '--disk', joinpath(base_path, image_name),
-            '--cpu-type', cpu_type,
-            '--num-cpus', num_cpus,
-            '--boot-type', boot_type,
-        ],
-        valid_isas = (constants.x86_tag,),
-        valid_hosts = host,
-        length = constants.long_tag,
+        name=name,
+        verifiers=verifiers,
+        fixtures=(),
+        config=joinpath(
+            config.base_dir,
+            "configs",
+            "example",
+            "components-library",
+            "boot_exit_disk_run.py",
+        ),
+        config_args=[
+            "--cpu",
+            cpu,
+            "--num-cpus",
+            str(num_cpus),
+            "--mem-system",
+            mem_system,
+            "--boot-type",
+            boot_type,
+            "--override-download",
+            "--resource-directory",
+            resource_path,
+        ]
+        + additional_config_args,
+        valid_isas=(isa_to_use,),
+        valid_hosts=constants.supported_hosts,
+        protocol=protocol_to_use,
+        length=length,
     )
 
-# Test every CPU type
-cpu_types = ('atomic', 'simple',)
-for cpu_type in cpu_types:
-    test_boot(cpu_type, '1', 'init', constants.supported_hosts)
 
-# Test a multicore system
-test_boot('atomic', '4', 'systemd', constants.supported_hosts)
+#### The quick (pre-submit/Kokoro) tests ####
 
-#KVM
-if(support_kvm()):
-    test_boot('kvm', '1', 'init', (constants.host_x86_64_tag,))
-    test_boot('kvm', '4', 'systemd', (constants.host_x86_64_tag,))
+test_boot(
+    cpu="atomic",
+    num_cpus=1,
+    mem_system="classic",
+    to_tick=10000000000, #Simulates 1/100th of a second.
+    length=constants.quick_tag,
+)
+
+test_boot(
+    cpu="timing",
+    num_cpus=1,
+    mem_system="classic",
+    to_tick=10000000000,
+    length=constants.quick_tag,
+)
+
+test_boot(
+    cpu="atomic",
+    num_cpus=4,
+    mem_system="classic",
+    to_tick=10000000000,
+    length=constants.quick_tag,
+)
+
+test_boot(
+    cpu="o3",
+    num_cpus=1,
+    mem_system="classic",
+    to_tick=10000000000,
+    length=constants.quick_tag,
+)
+
+#### The long (Nightly) tests ####
+
+test_boot(
+    cpu="atomic",
+    num_cpus=1,
+    mem_system="classic",
+    boot_type="init",
+    length=constants.long_tag,
+)
+
+test_boot(
+    cpu="timing",
+    num_cpus=1,
+    mem_system="mesi_two_level",
+    boot_type="init",
+    length=constants.long_tag,
+)
+
+test_boot(
+    cpu="timing",
+    num_cpus=1,
+    mem_system="mi_example",
+    boot_type="init",
+    length=constants.long_tag,
+)
+
+test_boot(
+    cpu="atomic",
+    num_cpus=4,
+    mem_system="classic",
+    boot_type="systemd",
+    length=constants.long_tag,
+)
+
+test_boot(
+    cpu="o3",
+    num_cpus=2,
+    mem_system="mesi_two_level",
+    boot_type="init",
+    length=constants.long_tag,
+)
+
+#### The very-long (Weekly) tests ####
+
+# This maps the cross product of the test to run. As 'init' is a subset
+# of the 'systemd' boot-type, we only run 'systemd'. A test with a value of
+# 'False' will not be run. This is either due to 'Timeout' (this test cannot
+# complete within 12 hours), this setup is not supported at present, or a
+# simulation crash.
+run_map = {
+    # The Memory System.
+    "classic": {
+        # The CPU Type.
+        "atomic": {
+            # The number of cores.
+            1: True,
+            2: True,
+            4: False,  # We already run this in the long (Nightly) tests.
+            8: True,
+        },
+        "timing": {
+            1: True,
+            2: False,  # Timeout
+            4: False,  # Timeout
+            8: False,  # Timeout
+        },
+        "o3": {
+            1: False,  # Timeout
+            2: False,  # Not Supported
+            4: False,  # Not Supported
+            8: False,  # Not Supported
+        },
+    },
+    "mi_example": {
+        "atomic": {
+            1: False,  # Not Supported
+            2: False,  # Not Supported
+            4: False,  # Not Supported
+            8: False,  # Not Supported
+        },
+        "timing": {
+            1: True,
+            2: True,
+            4: True,
+            8: True,
+        },
+        "o3": {
+            1: False,  # Timeout
+            2: False,  # Timeout
+            4: False,  # Timeout
+            8: False,  # Timeout
+        },
+    },
+    "mesi_two_level": {
+        "atomic": {
+            1: False,  # Not Supported
+            2: False,  # Not Supported
+            4: False,  # Not Supported
+            8: False,  # Not Supported
+        },
+        "timing": {
+            1: True,
+            2: True,
+            4: True,
+            8: True,
+        },
+        "o3": {
+            1: False,  # Timeout
+            2: False,  # Timeout
+            4: False,  # Simulation Crash
+            8: False,  # Simulation Crash
+        },
+    },
+}
+
+for mem_system in run_map:
+    for cpu in run_map[mem_system]:
+        for num_cpus in run_map[mem_system][cpu]:
+            if run_map[mem_system][cpu][num_cpus]:
+
+                test_boot(
+                    cpu=cpu,
+                    num_cpus=num_cpus,
+                    mem_system=mem_system,
+                    boot_type="systemd",
+                    length=constants.very_long_tag,
+                )
