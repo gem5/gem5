06401fa80ff0a5d581417a9d3d6ed569dc94ceae
Bobby R. Bruce <bbruce@ucdavis.edu>
2021-09-23 12:12:23 -0700

    configs,tests: Add "Hello world" example for the gem5 library
    Change-Id: Ifeb33f5425952aac113a04f8e27306476a6fc381
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/50887
    Reviewed-by: Bobby R. Bruce <bbruce@ucdavis.edu>
    Maintainer: Bobby R. Bruce <bbruce@ucdavis.edu>
    Tested-by: kokoro <noreply+kokoro@google.com>

---
+++ configs/example/gem5_library/arm-hello.py
@@ -0,0 +1,127 @@
+# Copyright (c) 2021 The Regents of the University of California
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+This gem5 configuation script creates a simple board to run an ARM
+"hello world" binary.
+
+This is setup is the close to the simplest setup possible using the gem5
+library. It does not contain any kind of caching, IO, or any non-essential
+components.
+
+Usage
+-----
+
+```
+scons build/ARM/gem5.opt
+./build/ARM/gem5.opt configs/gem5_library/arm-hello.py
+```
+"""
+
+import m5
+from m5.objects import Root
+
+from gem5.isas import ISA
+from gem5.utils.requires import requires
+from gem5.resources.resource import Resource
+from gem5.components.boards.simple_board import SimpleBoard
+from gem5.components.cachehierarchies.classic.no_cache import NoCache
+from gem5.components.memory.single_channel import SingleChannelDDR3_1600
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.components.processors.cpu_types import CPUTypes
+
+import os
+
+# This check ensures the gem5 binary is compiled to the ARM ISA target. If not,
+# an exception will be thrown.
+requires(isa_required=ISA.ARM)
+
+# In this setup we don't have a cache. `NoCache` can be used for such setups.
+cache_hierarchy = NoCache()
+
+# We use a single channel DDR3_1600 memory system
+memory = SingleChannelDDR3_1600(size="32MB")
+
+# We use a simple Timing processor with one core.
+processor = SimpleProcessor(cpu_type=CPUTypes.TIMING, num_cores=1)
+
+# The gem5 library simble board which can be used to run simple SE-mode
+# simulations.
+board = SimpleBoard(
+    clk_freq="3GHz",
+    processor=processor,
+    memory=memory,
+    cache_hierarchy=cache_hierarchy,
+)
+
+# This method must be called to connect all the components specified during
+# the board's construction.
+board.connect_things()
+
+# Here we set the workload. In this case we want to run a simple "Hello World!"
+# program compiled to the ARM ISA. The `Resource` class will automatically
+# download the binary from the gem5 Resources cloud bucket if it's not already
+# present.
+board.set_workload(
+    Resource(
+        # The `Resource` class reads the `resources.json` file from the gem5
+        # resources repository:
+        # https://gem5.googlesource.com/public/gem5-resource.
+        # Any resource specified in this file will be automatically retrieved.
+        # At the time of writing, this file is a WIP and does not contain all
+        # resources. Jira ticket: https://gem5.atlassian.net/browse/GEM5-1096
+        "arm-hello64-static",
+        # `override=True` means the if the binary already exists locally within
+        # the resource directory, and the hash's differ to that in the cloud
+        # bucket, the local copy will be overwritten. If this were set to false
+        # an exception would be thrown in this case.
+        override=True,
+        # We hardcode the resource directory to `tests/gem5/resources`. The
+        # `arm-hello64-static` binary will be downloaded and stored there.
+        resource_directory=os.path.join(
+            os.path.dirname(
+                os.path.dirname(
+                    os.path.dirname(
+                        os.path.dirname(os.path.realpath(__file__))
+                    )
+                )
+            ),
+            "tests",
+            "gem5",
+            "resources",
+        ),
+    )
+)
+
+# Lastly we setup the root, instantiate the design, and run the simulation.
+root = Root(full_system=False, system=board)
+
+m5.instantiate()
+
+exit_event = m5.simulate()
+print(
+    "Exiting @ tick {} because {}.".format(m5.curTick(), exit_event.getCause())
+)
