3e32fd3b330e59ba4e64205948559811a41bf958
Jason Lowe-Power <jason@lowepower.com>
2021-10-11 16:30:09 -0700

    mem-ruby: Add RISC-V atomic support to Ruby
    RISC-V atomics carry a atomic functor that needs to be executed in the
    cache hierarchy. To implement this in Ruby, we execute the functor in
    the hitCallback function. Note that these functions are slightly
    different than the atomic functions used in the GPU model and the GPU
    coalescer even though they have similar semantics.
    
    This change was tested with RISC-V Linux boot which has a few atomics
    and linux boot finishes successfully. Previously, the boot got stuck
    after the incorrect atomic operation.
    
    Change-Id: I47a69c05ad9f4267d0220023289116e62b5231be
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/51447
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Bobby R. Bruce <bbruce@ucdavis.edu>
    Reviewed-by: Matt Sinclair <mattdsinclair@gmail.com>
    Reviewed-by: Matthew Poremba <matthew.poremba@amd.com>
    Maintainer: Matt Sinclair <mattdsinclair@gmail.com>

--- src/mem/ruby/system/Sequencer.cc
+++ src/mem/ruby/system/Sequencer.cc
@@ -613,6 +613,15 @@ Sequencer::hitCallback(SequencerRequest* srequest, DataBlock& data,
             data.setData(&overwrite_val[0],
                          getOffset(request_address), pkt->getSize());
             DPRINTF(RubySequencer, "swap data %s\n", data);
+        } else if (pkt->isAtomicOp()) {
+            // Set the data in the packet to the old value in the cache
+            pkt->setData(
+                data.getData(getOffset(request_address), pkt->getSize()));
+            DPRINTF(RubySequencer, "AMO original data %s\n", data);
+            // execute AMO operation
+            (*(pkt->getAtomicOp()))(
+                data.getDataMod(getOffset(request_address)));
+            DPRINTF(RubySequencer, "AMO new data %s\n", data);
         } else if (type != RubyRequestType_Store_Conditional || llscSuccess) {
             // Types of stores set the actual data here, apart from
             // failed Store Conditional requests
