52661838a4ed50532ebd805e894cfa7c6157b04b
Yu-hsin Wang <yuhsingw@google.com>
2022-01-11 17:25:02 +0800

    ext: upgrade to googletest 1.11.x
    Upgrade googletest to 1.11.x
    upstream commit: 8306020a3e9eceafec65508868d7ab5c63bb41f7
    
    sha1sum df8cdd26ee7cdf2a3d9c05a92d3630a96f406422 generated by command:
    find . -type f ! -name SConscript ! -path "./.*" -print0 \
    | sort -z | xargs -0 sha1sum | sha1sum
    
    This upgrade is mainly for providing ConditionalMatcher support.
    
    Change-Id: I27d971c02c59a3ad42c3002f1b4e1a8b18269c56
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/55384
    Maintainer: Gabe Black <gabe.black@gmail.com>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>
    Reviewed-by: Giacomo Travaglini <giacomo.travaglini@arm.com>
    Reviewed-by: Bobby Bruce <bbruce@ucdavis.edu>
    Maintainer: Bobby Bruce <bbruce@ucdavis.edu>

--- ext/googletest/googletest/include/gtest/internal/gtest-death-test-internal.h
+++ ext/googletest/googletest/include/gtest/internal/gtest-death-test-internal.h
@@ -33,8 +33,8 @@
 // death tests.  They are subject to change without notice.
 // GOOGLETEST_CM0001 DO NOT DELETE
 
-#ifndef GTEST_INCLUDE_GTEST_INTERNAL_GTEST_DEATH_TEST_INTERNAL_H_
-#define GTEST_INCLUDE_GTEST_INTERNAL_GTEST_DEATH_TEST_INTERNAL_H_
+#ifndef GOOGLETEST_INCLUDE_GTEST_INTERNAL_GTEST_DEATH_TEST_INTERNAL_H_
+#define GOOGLETEST_INCLUDE_GTEST_INTERNAL_GTEST_DEATH_TEST_INTERNAL_H_
 
 #include "gtest/gtest-matchers.h"
 #include "gtest/internal/gtest-internal.h"
@@ -42,10 +42,10 @@
 #include <stdio.h>
 #include <memory>
 
+GTEST_DECLARE_string_(internal_run_death_test);
+
 namespace testing {
 namespace internal {
-
-GTEST_DECLARE_string_(internal_run_death_test);
 
 // Names of the flags (needed for parsing Google Test flags).
 const char kDeathTestStyleFlag[] = "death_test_style";
@@ -236,8 +236,6 @@ inline Matcher<const ::std::string&> MakeDeathTestMatcher(
           gtest_dt->Abort(::testing::internal::DeathTest::TEST_DID_NOT_DIE);   \
           break;                                                               \
         }                                                                      \
-        default:                                                               \
-          break;                                                               \
       }                                                                        \
     }                                                                          \
   } else                                                                       \
@@ -301,4 +299,4 @@ InternalRunDeathTestFlag* ParseInternalRunDeathTestFlag();
 }  // namespace internal
 }  // namespace testing
 
-#endif  // GTEST_INCLUDE_GTEST_INTERNAL_GTEST_DEATH_TEST_INTERNAL_H_
+#endif  // GOOGLETEST_INCLUDE_GTEST_INTERNAL_GTEST_DEATH_TEST_INTERNAL_H_
