f161c0b6bce28646d3f2e7a191ad9998bdbf2c9e
Gabe Black <gabe.black@gmail.com>
2022-01-18 21:12:03 -0800

    dev: Add a base QEMU firmware config device.
    This artificial device is provided by QEMU inside their emulated
    machines to feed extra configuration information to the system firmware,
    or even to the operating system if it chose to use it. The behavior of
    this device is explained in the docs/specs/fw_cfg.txt file in the QEMU
    source.
    
    This implementation currently supports the traditional interface, and
    does not support the DMA based interface, although it probably wouldn't
    be that hard to expand it to in the future.
    
    The interface exposes individual entries which can optionally (and
    usually) have paths associated with them that you can look up in a
    directory type entry which has a fixed index. There are some entries
    which are built into the device itself, which are the ID, signature and
    directory entries, but the rest can be set up in the config scripts.
    
    To make it easier to add new entries which are not from config scripts,
    aka the ones that are hard coded in C++ and built into the device, the
    actual entries themselves are not SimObjects, but it's easy to create a
    SimObject wrapper which will spit them out for the device to consume.
    Other items can be added to the device manually without generating them
    with SimObjects.
    
    Entries can have fixed or automatically generated indices. All entries
    have a "path" in the sense that they have a name, but as a minor
    deviation from what the QEMU documentation says, a "path" which begins
    with a "." is not exported in the directory. This is purposefully
    reminiscent of the unix style hidden file mechanism, where files or
    directories who's names begin with "." are not normally shown by ls.
    
    There are two different styles of this device, one which is IO port
    based, and one which is MMIO based. Which to use depends on the
    architecture, where x86 currently uses the IO scheme and ARM uses the
    MMIO scheme. The documentation doesn't say what other ISAs use, if any
    other ones support this interface, but I'd assume the MMIO version.
    These are split out because the rules for how they work are subtly
    different, but they share a lot of common machinery under the hood.
    
    In most cases where somebody tries to talk to the device in an unusual
    way, for instance accessing a register with an incomplete width or at an
    offset, the device will just report all zeroes. The behavior in those
    cases isn't specified, in many cases doesn't make sense based on the
    design of the device, and doesn't seem to be depended on in the limited
    use case I looked at.
    
    Change-Id: Ib81ace406f877b298b9b98883d417e7d673916b5
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/55627
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Gabe Black <gabe.black@gmail.com>
    Maintainer: Gabe Black <gabe.black@gmail.com>

---
+++ src/dev/qemu/QemuFwCfg.py
@@ -0,0 +1,87 @@
+# Copyright 2022 Google, Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from m5.params import *
+from m5.objects.SimObject import SimObject
+from m5.objects.Device import PioDevice
+
+class QemuFwCfgItem(SimObject):
+    type = 'QemuFwCfgItem'
+    cxx_class = 'gem5::qemu::FwCfgItemFactoryBase'
+    cxx_header = 'dev/qemu/fw_cfg.hh'
+    abstract = True
+
+    # The path this item will be listed under in the firmware config directory.
+    arch_specific = Param.Bool(False, 'if this item is archiecture specific')
+    index = Param.Unsigned(0, 'Fixed index, or 0 for automatic')
+    path = Param.String('Path to item in the firmware config directory')
+
+class QemuFwCfgItemFile(QemuFwCfgItem):
+    type = 'QemuFwCfgItemFile'
+    cxx_class = 'gem5::qemu::FwCfgItemFactory<gem5::qemu::FwCfgItemFile>'
+    cxx_template_params = ['class ItemType']
+    cxx_header = 'dev/qemu/fw_cfg.hh'
+
+    # The path to the file that will be used to populate this item.
+    file = Param.String('Path to file to export')
+
+class QemuFwCfgItemString(QemuFwCfgItem):
+    type = 'QemuFwCfgItemString'
+    cxx_class = 'gem5::qemu::FwCfgItemFactory<gem5::qemu::FwCfgItemString>'
+    cxx_template_params = ['class ItemType']
+    cxx_header = 'dev/qemu/fw_cfg.hh'
+
+    # The string which directly populates this item.
+    string = Param.String('String to export')
+
+class QemuFwCfg(PioDevice):
+    type = 'QemuFwCfg'
+    cxx_class = 'gem5::qemu::FwCfg'
+    cxx_header = 'dev/qemu/fw_cfg.hh'
+    abstract = True
+
+    items = VectorParam.QemuFwCfgItem([],
+            'Items exported by the firmware config device')
+
+class QemuFwCfgIo(QemuFwCfg):
+    type = 'QemuFwCfgIo'
+    cxx_class = 'gem5::qemu::FwCfgIo'
+    cxx_header = 'dev/qemu/fw_cfg.hh'
+
+    # The selector register is 16 bits wide, and little endian. The data
+    # register must be one port ahead of the selector.
+    selector_addr = Param.Addr('IO port for the selector register')
+
+class QemuFwCfgMmio(QemuFwCfg):
+    type = 'QemuFwCfgMmio'
+    cxx_class = 'gem5::qemu::FwCfgMmio'
+    cxx_header = 'dev/qemu/fw_cfg.hh'
+
+    # The selector register is 16 bits wide, and big endian.
+    selector_addr = Param.Addr('Memory address for the selector register')
+
+    # The data register is 8, 16, 32 or 64 bits wide.
+    data_addr_range = \
+            Param.AddrRange('Memory address range for the data register')
