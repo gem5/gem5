305427e094dccdc46e39c03122d314ad516cd32f
Gabe Black <gabe.black@gmail.com>
2022-02-18 00:52:43 -0800

    scons: Hook up the kconfig guiconfig program.
    Change-Id: I0563a2fb2d79cea5974aeaf65a400be5ee51dc63

--- site_scons/gem5_scons/kconfig.py
+++ site_scons/gem5_scons/kconfig.py
@@ -30,6 +30,7 @@ import kconfiglib
 
 _kconfig_helpers = {
     "DEFCONFIG_PY": "defconfig.py",
+    "GUICONFIG_PY": "guiconfig.py",
     "LISTNEWCONFIG_PY": "listnewconfig.py",
     "MENUCONFIG_PY": "menuconfig.py",
     "SETCONFIG_PY": "setconfig.py",
@@ -75,6 +76,13 @@ def defconfig(env, base_kconfig, config_in, config_out):
         != 0
     ):
         error("Failed to run defconfig")
+
+
+def guiconfig(env, base_kconfig, config_path, main_menu_text):
+    kconfig_env = _prep_env(env, base_kconfig, config_path)
+    kconfig_env["ENV"]["MAIN_MENU_TEXT"] = main_menu_text
+    if kconfig_env.Execute('"${GUICONFIG_PY}" "${BASE_KCONFIG}"') != 0:
+        error("Failed to run guiconfig")
 
 
 def listnewconfig(env, base_kconfig, config_path):
