2ef82c4fa59ef8bb16f65b2973d2ddfe470556d3
Bobby R. Bruce <bbruce@ucdavis.edu>
2022-06-18 11:47:35 -0400

    misc: Revert "tests: Add v22-0 tag for compiler-tests..."
    This reverts commit 78bf9b19605a42206bc63e715989d62934fece4d.
    https://gem5-review.googlesource.com/c/public/gem5/+/60569
    
    Change-Id: I7d5e8e99039d27f58589197d9f908f615187ee9b
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/60630
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Bobby Bruce <bbruce@ucdavis.edu>
    Maintainer: Bobby Bruce <bbruce@ucdavis.edu>

--- tests/compiler-tests.sh
+++ tests/compiler-tests.sh
@@ -103,7 +103,7 @@ for compiler in ${images[@]}; do
     # targets for this test
     build_indices=(${build_permutation[@]:0:$builds_count})
 
-    repo_name="${base_url}/${compiler}:v22-0"
+    repo_name="${base_url}/${compiler}:latest"
 
     # Grab compiler image
     docker pull $repo_name >/dev/null
