f9b57ee4ed4666301e7cfc0eb3a27504536b2a84
Richard Cooper <richard.cooper@arm.com>
2022-06-23 16:14:42 +0100

    mem-ruby: Allow SLICC symbols to have no description.
    Updated the SLICC `Symbol` class to return an empty string when its
    `desc` property is read.
    
    The SLICC language does not require a symbol to have a `desc` for
    protocol generation, but the generation of SLICC HTML documentation
    expects a `desc` and will fail if it is not present.
    
    Change-Id: I07cc0ab805520eb74f86c6ea8036abb7354b10a9
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/60870
    Maintainer: Bobby Bruce <bbruce@ucdavis.edu>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jason Lowe-Power <power.jg@gmail.com>

--- src/mem/slicc/symbols/Symbol.py
+++ src/mem/slicc/symbols/Symbol.py
@@ -1,3 +1,15 @@
+# Copyright (c) 2022 Arm Limited
+# All rights reserved.
+#
+# The license below extends only to copyright in the software and shall
+# not be construed as granting a license to any other intellectual
+# property including but not limited to intellectual property relating
+# to a hardware implementation of the functionality of the software
+# licensed hereunder.  You may use the software subject to the license
+# terms below provided that you ensure that this notice is replicated
+# unmodified and in its entirety in all distributions of the software,
+# modified or unmodified, in source code or in binary form.
+#
 # Copyright (c) 1999-2008 Mark D. Hill and David A. Wood
 # Copyright (c) 2009 The Hewlett-Packard Development Company
 # All rights reserved.
@@ -64,7 +76,11 @@ class Symbol(PairContainer):
 
     @property
     def desc(self):
-        return self["desc"]
+        # Allow Symbols with no description: return an empty string.
+        if "desc" not in self:
+            return ""
+        else :
+            return self["desc"]
 
     def error(self, message, *args):
         self.location.error(message, *args)
