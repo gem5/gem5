a49cba948049b7b8a3a30a586160c8198292ff51
Bobby R. Bruce <bbruce@ucdavis.edu>
2022-11-10 17:07:11 -0800

    arch-x86: X86ISA default vector_string to HygonGenuine
    This commit,
    https://gem5-review.googlesource.com/c/public/gem5/+/64831, changed the
    default 'vendor_string' for the 'X86ISA' SimObject from 'M5 Simulator'
    and 'AuthenticAMD'. Unforunately due to an issue highlighted here:
    https://gem5.atlassian.net/browse/GEM5-1300 we cannot use the
    'AuthenticAMD'. Therefore, this change updates the default vector_string
    to HygonGenuine.
    
    The HygonGenuine is simple but works.
    
    Change-Id: I21421da8ae73e76d9daaf2fdd0b3238d5b309172
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/65492
    Tested-by: kokoro <noreply+kokoro@google.com>
    Maintainer: Bobby Bruce <bbruce@ucdavis.edu>
    Reviewed-by: Bobby Bruce <bbruce@ucdavis.edu>

--- src/arch/x86/X86ISA.py
+++ src/arch/x86/X86ISA.py
@@ -42,6 +42,15 @@ class X86ISA(BaseISA):
     cxx_class = "gem5::X86ISA::ISA"
     cxx_header = "arch/x86/isa.hh"
 
+    # Here we set the default vector string to "HygonGenuine". Previously this
+    # "M5 Simulator" but due to stricter checks in newer versions of GLIBC,
+    # the CPUID is checked for the required features. As "M5 Simulator" is not
+    # genuine CPUID, an error is returned. This change
+    # https://gem5-review.googlesource.com/c/public/gem5/+/64831 changed this
+    # to "GenuineAMD" but due to issues with booting the Linux Kernel using
+    # this vector string (highlighted here:
+    # https://gem5.atlassian.net/browse/GEM5-1300) we opted to use
+    # "HygonGenuine" instead.
     vendor_string = Param.String(
-        "AuthenticAMD", "Vendor string for CPUID instruction"
+        "HygonGenuine", "Vendor string for CPUID instruction"
     )
