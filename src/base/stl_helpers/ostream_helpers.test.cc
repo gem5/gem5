73afee1e0d25646bb8b42b610f2ffdd43b6aaed4
Gabriel Busnot <gabriel.busnot@arteris.com>
2023-02-27 15:48:23 +0000

    base: Provide stl_helpers::operator<< for more types
    This operator can be safely brought in scope when needed with "using
    stl_helpers::operator<<".
    
    In order to provide a specialization for operator<< with
    stl_helpers-enabled types without loosing the hability to use it with
    other types, a dual-dispatch mechanism is used. The only entry point
    in the system is through a primary dispatch function that won't
    resolve for non-helped types. Then, recursive calls go through the
    secondary dispatch interface that sort between helped and non-helped
    types. Helped typed will enter the system back through the primary
    dispatch interface while other types will look for operator<< through
    regular lookup, especially ADL.
    
    Change-Id: I1609dd6e85e25764f393458d736ec228e025da32
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/67666
    Reviewed-by: Daniel Carvalho <odanrc@yahoo.com.br>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Maintainer: Bobby Bruce <bbruce@ucdavis.edu>

---
+++ src/base/stl_helpers/ostream_helpers.test.cc
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2023 Arteris, Inc. and its applicable licensors and
+ * affiliates.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer; redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials
+ * provided with the distribution; neither the name of the copyright holders
+ * nor the names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <gtest/gtest.h>
+
+#include <map>
+#include <sstream>
+#include <string_view>
+#include <vector>
+
+#include "base/stl_helpers/ostream_helpers.hh"
+
+using gem5::stl_helpers::operator<<;
+
+TEST(OstreamHelpers, pair) {
+    auto p = std::make_pair(1, 2);
+    std::ostringstream os;
+    os << p;
+    EXPECT_EQ(os.str(), "(1, 2)");
+}
+
+TEST(OstreamHelpers, tuple) {
+    auto t = std::make_tuple(true,
+        std::make_pair("Hello", std::string_view("World")), '!');
+    std::ostringstream os;
+    os << t;
+    EXPECT_EQ(os.str(), "(1, (Hello, World), !)");
+}
+
+TEST(OstreamHelpers, vector) {
+    auto v = std::vector<const char*>{"abc", "defg", "hijklm", "\n"};
+    std::ostringstream os;
+    os << v;
+    EXPECT_EQ(os.str(), "[ abc, defg, hijklm, \n, ]");
+}
+
+TEST(OstreamHelpers, map) {
+    auto m = std::map<char, int>{{'a', 0}, {'b', 1}, {'c', 2}, {'d', 3}};
+    std::ostringstream os;
+    os << m;
+    EXPECT_EQ(os.str(), "[ (a, 0), (b, 1), (c, 2), (d, 3), ]");
+}
