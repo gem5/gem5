ee2be9cffe168a6d8fec9232189d11a8ee2358bd
Melissa Jost <melissakjost@gmail.com>
2023-03-28 13:37:29 -0700

    tests: Update nightly SystemC test
    The SystemC test requires gem5 to be built with the minimum
    dependencies image, and it was also updated to properly use
    se.py since it has now been deprecated.
    
    Change-Id: I1223afd70f105073d2cd4a2a78135f1f935c06ec
    Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/69337
    Reviewed-by: Bobby Bruce <bbruce@ucdavis.edu>
    Maintainer: Bobby Bruce <bbruce@ucdavis.edu>
    Tested-by: kokoro <noreply+kokoro@google.com>

--- tests/nightly.sh
+++ tests/nightly.sh
@@ -172,7 +172,7 @@ build_and_run_systemc () {
     rm -rf "${gem5_root}/build/ARM"
     docker run -u $UID:$GID --volume "${gem5_root}":"${gem5_root}" -w \
         "${gem5_root}" --memory="${docker_mem_limit}" --rm \
-        gcr.io/gem5-test/ubuntu-22.04_all-dependencies:${tag} bash -c "\
+        gcr.io/gem5-test/ubuntu-22.04_min-dependencies:${tag} bash -c "\
 scons -j${compile_threads} --ignore-style build/ARM/gem5.opt && \
 scons --with-cxx-config --without-python --without-tcmalloc USE_SYSTEMC=0 \
     -j${compile_threads} build/ARM/libgem5_opt.so \
@@ -183,7 +183,7 @@ scons --with-cxx-config --without-python --without-tcmalloc USE_SYSTEMC=0 \
         gcr.io/gem5-test/systemc-env:${tag} bash -c "\
 cd util/systemc/gem5_within_systemc && \
 make -j${compile_threads} && \
-../../../build/ARM/gem5.opt ../../../configs/example/se.py -c \
+../../../build/ARM/gem5.opt ../../../configs/deprecated/example/se.py -c \
     ../../../tests/test-progs/hello/bin/arm/linux/hello && \
 LD_LIBRARY_PATH=../../../build/ARM/:/opt/systemc/lib-linux64/ \
     ./gem5.opt.sc m5out/config.ini && \
