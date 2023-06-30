e21d78e53fef6de7b31eb462a49c719421232434
Daniel Kouchekinia <kouchekinia@wisc.edu>
2023-06-30 14:04:13 -0500

    mem-ruby,configs: Add GLC Atomic Latency VIPER Parameter
    Added a GLC atomic latency parameter (glc-atomic-latency) used when
    enqueueing response messages regarding atomics directly performed in
    the TCC. This latency is added in addition to the L2 response latency
    (TCC_latency). This represents the latency of performing an atomic
    within the L2.
    
    With this change, the TCC response queue will receive enqueues with
    varying latencies as GLC atomic responses will have this added GLC
    atomic latency while data responses will not. To accommodate this in
    light of the queue having strict FIFO ordering (which would be violated
    here), this change also adds an optional parameter bypassStrictFIFO to
    the SLICC enqueue function which allows overriding strict FIFO
    requirements for individual messages on a case-by-case basis. This
    parameter is only being used in the TCC's atomic response enqueue call.
    
    Change-Id: Iabd52cbd2c0cc385c1fb3fe7bcd0cc64bdb40aac

--- src/mem/slicc/parser.py
+++ src/mem/slicc/parser.py
@@ -633,11 +633,15 @@ class SLICC(Grammar):
 
     def p_statement__enqueue(self, p):
         "statement : ENQUEUE '(' var ',' type ')' statements"
-        p[0] = ast.EnqueueStatementAST(self, p[3], p[5], None, p[7])
+        p[0] = ast.EnqueueStatementAST(self, p[3], p[5], None, None, p[7])
 
     def p_statement__enqueue_latency(self, p):
         "statement : ENQUEUE '(' var ',' type ',' expr ')' statements"
-        p[0] = ast.EnqueueStatementAST(self, p[3], p[5], p[7], p[9])
+        p[0] = ast.EnqueueStatementAST(self, p[3], p[5], p[7], None, p[9])
+
+    def p_statement__enqueue_latency_bypass_strict_fifo(self, p):
+        "statement : ENQUEUE '(' var ',' type ',' expr ',' expr ')' statements"
+        p[0] = ast.EnqueueStatementAST(self, p[3], p[5], p[7], p[9], p[11])
 
     def p_statement__defer_enqueueing(self, p):
         "statement : DEFER_ENQUEUEING '(' var ',' type ')' statements"
