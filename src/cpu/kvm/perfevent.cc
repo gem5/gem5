2d8a98cddcd67023baf02be172dfd0d3f51aedb5
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-07-18 22:59:49 -0700

    misc: Add an example to the panic
    Change-Id: Ic1fdfb955e5d8b9ad1d4f0a2bf30fa8050deba70
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- src/cpu/kvm/perfevent.cc
+++ src/cpu/kvm/perfevent.cc
@@ -173,18 +173,20 @@ PerfKvmCounter::attach(PerfKvmCounterConfig &config,
     {
         if (errno == EACCES)
         {
-            panic("PerfKvmCounter::attach received error EACCESS.\n"
-            " This error may be caused by a too restrictive setting"
-            " in the file '/proc/sys/kernel/perf_event_paranoid'."
-            " The default value was changed to 2 in kernel 4.6."
-            " A value greater than 1 prevents gem5 from making"
-            " the syscall to perf_event_open.\n"
-            " Alternatively, you can set the usePerf flag of the KVM CPU to"
-            " False. Setting this flag to False will limit some"
-            " functionalities of KVM CPU, such as counting the number of"
-            " cycles and the number of instructions, as well as the ability"
-            " to exit to gem5 after a certain amount of cycles of"
-            " instructions when using KVM CPU.");
+            panic("    PerfKvmCounter::attach received error EACCESS.\n"
+            "  This error may be caused by a too restrictive setting\n"
+            "  in the file '/proc/sys/kernel/perf_event_paranoid'.\n"
+            "  The default value was changed to 2 in kernel 4.6.\n"
+            "  A value greater than 1 prevents gem5 from making\n"
+            "  the syscall to perf_event_open.\n"
+            "    Alternatively, you can set the usePerf flag of the KVM\n"
+            "  CPU to False. Setting this flag to False will limit some\n"
+            "  functionalities of KVM CPU, such as counting the number of\n"
+            "  cycles and the number of instructions, as well as the\n"
+            "  ability of exiting to gem5 after a certain amount of cycles\n"
+            "  or instructions when using KVM CPU. An example can be found\n"
+            "  here, configs/example/gem5_library/"
+            "x86-ubuntu-run-with-kvm-no-perf.py");
         }
         panic("PerfKvmCounter::attach failed (%i)\n", errno);
     }
