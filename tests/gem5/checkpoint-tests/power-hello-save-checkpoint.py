a903ff43f2cfd983d95e0ac21a3025928d8f6a52
Harshil Patel <91860903+Harshil2107@users.noreply.github.com>
2023-08-08 09:40:07 -0700

    tests: Add checkpoint tests for all ISAs
    Added save and restore checkpoint tests for arm-hello, x86-hello, x86-fs, power-hello
    Added mips and sparc test but mips does not support checkpoint and there is a bug in sparc.
    Added test file to run the tests.
    
    Change-Id: I2d3b96f95ee08aae921de9a885ac5be77d49f326

---
+++ tests/gem5/checkpoint-tests/power-hello-save-checkpoint.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2022 The Regents of the University of California
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+This gem5 test script creates a simple board to run the first
+10^6 ticks of "power-hello" binary simulation and saves a checkpoint.
+This configuration serves as a test to ensure that checkpoints work
+with POWER ISA.
+"""
+
+import argparse
+from gem5.isas import ISA
+from gem5.utils.requires import requires
+from gem5.resources.resource import obtain_resource
+from gem5.components.cachehierarchies.classic.no_cache import NoCache
+from gem5.components.boards.simple_board import SimpleBoard
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.simulate.simulator import Simulator
+from gem5.components.memory import SingleChannelDDR3_1600
+from gem5.components.processors.cpu_types import CPUTypes
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--checkpoint-path",
+    type=str,
+    required=False,
+    default="power-hello-test-checkpoint/",
+    help="The directory to store the checkpoint.",
+)
+
+args = parser.parse_args()
+requires(isa_required=ISA.POWER)
+
+cache_hierarchy = NoCache()
+
+memory = SingleChannelDDR3_1600(size="32MB")
+processor = SimpleProcessor(
+    cpu_type=CPUTypes.TIMING, isa=ISA.POWER, num_cores=2
+)
+
+board = SimpleBoard(
+    clk_freq="3GHz",
+    processor=processor,
+    memory=memory,
+    cache_hierarchy=cache_hierarchy,
+)
+board.set_se_binary_workload(obtain_resource("power-hello"))
+
+sim = Simulator(board=board, full_system=False)
+max_ticks = 10**6
+sim.run(max_ticks=max_ticks)
+print(
+    "Exiting @ tick {} because {}.".format(
+        sim.get_current_tick(), sim.get_last_exit_event_cause()
+    )
+)
+print("Taking checkpoint at", args.checkpoint_path)
+sim.save_checkpoint(args.checkpoint_path)
+print("Done taking checkpoint")
