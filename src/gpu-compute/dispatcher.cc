57b3d2897c77d0907016ee972655c7bbcdfe23e7
Matthew Poremba <matthew.poremba@amd.com>
2023-08-25 09:35:55 -0500

    gpu-compute: Use timing DMAs for GPUFS HSA signals
    The functional HSA signal read was a hack left in the gpu-compute code.
    In full system, this functional read is causing problems occasionally
    with the translation not yet being in the page table. The error message
    output by gem5 was a fatal message on the readBlob method in port proxy.
    Changing this to a timing DMA fixes this problem.
    
    This commit adds the various timing DMA functions to send and receive
    response and clean up. A helper method "sendCompletionSignal" is added
    to the GPUCommandProcessor because the indentation level was getting too
    deep. This change applies only to FS mode. Code for SE mode is
    equivalent to what it was before this commit.
    
    Change-Id: I1bfcaa0a52731cdf9532a7fd0eb06ab2f0e09d48

--- src/gpu-compute/dispatcher.cc
+++ src/gpu-compute/dispatcher.cc
@@ -310,20 +310,10 @@ GPUDispatcher::notifyWgCompl(Wavefront *wf)
         gpuCmdProc->hsaPacketProc()
             .finishPkt(task->dispPktPtr(), task->queueId());
         if (task->completionSignal()) {
-            /**
-            * HACK: The semantics of the HSA signal is to decrement
-            * the current signal value. We cheat here and read out
-            * he value from main memory using functional access and
-            * then just DMA the decremented value.
-            */
-            uint64_t signal_value =
-                gpuCmdProc->functionalReadHsaSignal(task->completionSignal());
-
             DPRINTF(GPUDisp, "HSA AQL Kernel Complete with completion "
                     "signal! Addr: %d\n", task->completionSignal());
 
-            gpuCmdProc->updateHsaSignal(task->completionSignal(),
-                                        signal_value - 1);
+            gpuCmdProc->sendCompletionSignal(task->completionSignal());
         } else {
             DPRINTF(GPUDisp, "HSA AQL Kernel Complete! No completion "
                 "signal\n");
