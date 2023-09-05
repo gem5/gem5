c46e1cf39e3b3aea8bfa74ea5cf02dbdb3ccbce8
Bobby R. Bruce <bbruce@ucdavis.edu>
2023-09-05 13:33:16 -0700

    misc: Another try at this
    Change-Id: I426ee87420fbfe17203b794ea93fee5cbd0b863f

---
+++ .github/actions/runner-cleanup/entrypoint.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env sh -l
+
+# Copyright (c) 2023 The Regents of the University of California
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Parse the command line options.
+# This is really basic, but it works for our purposes.
+
+WORKSPACE=$1
+HOME=$2
+PRUNE_DOCKER=$3
+CLEAN_WORKSPACE=$4
+CLEAR_CACHE=$5
+
+if [[ $PRUNE_DOCKER == "true" ]]; then
+    docker system prune -f
+fi
+
+if [[ $CLEAN_WORKSPACE == "true" ]]; then
+    rm -rf "${WORKSPACE}/{*,.*}"
+fi
+
+if [[ $CLEAR_CACHE == "true" ]]; then
+    rm -rf "${HOME}/.cache"
+fi
