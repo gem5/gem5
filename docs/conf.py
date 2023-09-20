c3eeceb2201c088e03c20ec9c02de22c471cd7e5
Melissa Jost <melissakjost@gmail.com>
2023-09-20 09:44:20 -0700

    misc: Add sphinx stdlib documentation
    This adds documentation to the stdlib using sphinx. There are
    details in https://gem5.atlassian.net/browse/GEM5-1314 on
    how this documentation was generated, however, I also built
    the documentation using ALL_CHI to include some of the missing
    documentation there. Currently, there are a couple modules
    that show up blank, such as dramsys and mesi three level that
    I'm working on either figuring out how to build, or will
    exclude in the configuration file.  To view the current state
    of the documentation, run cd docs/_build/html;
    python -m http.server 8000
    
    Change-Id: Ic7cfab4da32ea29bdda958c48ea3c6d26cfd6765

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'gem5'
+copyright = ''
+author = ''
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+]
+
+templates_path = ['_templates']
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+    'build/lib64/python3.8/site-packages',
+    'build/lib/python3.8/site-packages'
+]
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# -- Options for todo extension ----------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#configuration
+
+todo_include_todos = True
