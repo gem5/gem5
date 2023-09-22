2d85707a754122ab940cc1b7c0dd07c3c3f28df7
Giacomo Travaglini <giacomo.travaglini@arm.com>
2023-09-22 08:59:43 +0100

    sim: Define an InstructionDisassembler SimObject
    We want to be able to configure from python the disassembler
    used by an instruction tracer. The default/base version will
    reuse existing instruction logic and it will simply
    call the StaticInst::disassemble method.
    
    Change-Id: Ieb16f059a436757c5892dcc82882f6d42090927f
    Signed-off-by: Giacomo Travaglini <giacomo.travaglini@arm.com>

--- src/sim/InstTracer.py
+++ src/sim/InstTracer.py
@@ -1,3 +1,15 @@
+# Copyright (c) 2023 Arm Limited
+# All rights reserved.
+#
+# The license below extends only to copyright in the software and shall
+# not be construed as granting a license to any other intellectual
+# property including but not limited to intellectual property relating
+# to a hardware implementation of the functionality of the software
+# licensed hereunder.  You may use the software subject to the license
+# terms below provided that you ensure that this notice is replicated
+# unmodified and in its entirety in all distributions of the software,
+# modified or unmodified, in source code or in binary form.
+#
 # Copyright (c) 2007 The Regents of The University of Michigan
 # All rights reserved.
 #
@@ -28,8 +40,18 @@ from m5.SimObject import SimObject
 from m5.params import *
 
 
+class InstDisassembler(SimObject):
+    type = "InstDisassembler"
+    cxx_header = "sim/insttracer.hh"
+    cxx_class = "gem5::trace::InstDisassembler"
+
+
 class InstTracer(SimObject):
     type = "InstTracer"
     cxx_header = "sim/insttracer.hh"
     cxx_class = "gem5::trace::InstTracer"
     abstract = True
+
+    disassembler = Param.InstDisassembler(
+        InstDisassembler(), "Instruction Disassembler"
+    )
