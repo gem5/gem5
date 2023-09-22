952c4f5eead8af33bfbff7c63cb0ebe934ebbc5d
Giacomo Travaglini <giacomo.travaglini@arm.com>
2023-09-22 09:01:09 +0100

    cpu: Pass a reference of the parent tracer to the ExeTracerRecord
    Change-Id: I3576df2b7bee1289db60bb6072bd9c90038ca8ce
    Signed-off-by: Giacomo Travaglini <giacomo.travaglini@arm.com>

--- src/cpu/nativetrace.cc
+++ src/cpu/nativetrace.cc
@@ -1,4 +1,16 @@
 /*
+ * Copyright (c) 2023 Arm Limited
+ * All rights reserved
+ *
+ * The license below extends only to copyright in the software and shall
+ * not be construed as granting a license to any other intellectual
+ * property including but not limited to intellectual property relating
+ * to a hardware implementation of the functionality of the software
+ * licensed hereunder.  You may use the software subject to the license
+ * terms below provided that you ensure that this notice is replicated
+ * unmodified and in its entirety in all distributions of the software,
+ * modified or unmodified, in source code or in binary form.
+ *
  * Copyright (c) 2006-2009 The Regents of The University of Michigan
  * All rights reserved.
  *
@@ -47,6 +59,17 @@ NativeTrace::NativeTrace(const Params &p)
     native_listener->listen();
 
     fd = native_listener->accept();
+}
+
+NativeTraceRecord::NativeTraceRecord(
+        NativeTrace *_parent,
+        Tick _when, ThreadContext *_thread,
+        const StaticInstPtr _staticInst, const PCStateBase &_pc,
+        const StaticInstPtr _macroStaticInst)
+  : ExeTracerRecord(_when, _thread, _staticInst, _pc,
+                    *_parent, _macroStaticInst),
+    parent(_parent)
+{
 }
 
 void
