237bbf0e42ab3b7f43d582566e021604794e5462
Giacomo Travaglini <giacomo.travaglini@arm.com>
2023-09-22 09:05:45 +0100

    cpu: Disassemble through the InstDisassembler in the ExeTracer
    Change-Id: I4a0c585b9b8824a0694066bef0ee004f68407111
    Signed-off-by: Giacomo Travaglini <giacomo.travaglini@arm.com>

--- src/cpu/exetrace.cc
+++ src/cpu/exetrace.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2019 ARM Limited
+ * Copyright (c) 2017, 2019, 2023 Arm Limited
  * All rights reserved
  *
  * The license below extends only to copyright in the software and shall
@@ -101,7 +101,7 @@ ExeTracerRecord::traceInst(const StaticInstPtr &inst, bool ran)
     //
 
     outs << std::setw(26) << std::left;
-    outs << inst->disassemble(cur_pc, &loader::debugSymbolTable);
+    outs << tracer.disassemble(inst, *pc, &loader::debugSymbolTable);
 
     if (ran) {
         outs << " : ";
