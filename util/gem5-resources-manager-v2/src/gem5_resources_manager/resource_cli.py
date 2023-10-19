5c8dc905c05c264cd2c8773bc24e43e6ac7a95ce
Bobby R. Bruce <bbruce@ucdavis.edu>
2023-10-19 11:21:12 -0700

    util: Fix argparsing for subtool
    Change-Id: I16d018481691b6de0975f605e32b4ee9621bf339

--- util/gem5-resources-manager-v2/src/gem5_resources_manager/resource_cli.py
+++ util/gem5-resources-manager-v2/src/gem5_resources_manager/resource_cli.py
@@ -15,14 +15,15 @@ def cli():
 
     subparserer = parser.add_subparsers()
 
+    parser.add_argument("obj")
     for subtool in subtools:
-        subtool_parser = subtool.get_arg_parser()
-        subtool_parser.set_defaults(subtool=subtool)
-        subparserer.add_parser(subtool.get_arg_parser())
+        subtool_parser = subtool.__class__.get_arg_parser()
+        subtool_parser.set_defaults(obj=subtool)
+        subparserer.add_parser(subtool.__class__.get_arg_parser())
 
     args = parser.parse_args()
 
-    subtool = args.subtool
+    subtool = args.obj
 
     with DummyDatabase() as data_source:
         subtool.execute(args, data_source)
