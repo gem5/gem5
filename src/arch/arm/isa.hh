65cf6b0a1c28644513f7bbb4192f732d84a0757c
Giacomo Travaglini <giacomo.travaglini@arm.com>
2024-01-30 10:33:39 +0000

    arch-arm: Cache the highestEL in the ISA object
    This is for fast retrieval of the highest implemented
    exception level
    
    Change-Id: Id631c2b999d46a8b79570e4043ae04bc2b2e7531
    Signed-off-by: Giacomo Travaglini <giacomo.travaglini@arm.com>
    Reviewed-by: Richard Cooper <richard.cooper@arm.com>

--- src/arch/arm/isa.hh
+++ src/arch/arm/isa.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2012-2023 Arm Limited
+ * Copyright (c) 2010, 2012-2024 Arm Limited
  * All rights reserved
  *
  * The license below extends only to copyright in the software and shall
@@ -90,6 +90,7 @@ namespace ArmISA
 
         // Cached copies of system-level properties
         bool highestELIs64;
+        ExceptionLevel highestEL;
         bool haveLargeAsid64;
         uint8_t physAddrRange;
 
