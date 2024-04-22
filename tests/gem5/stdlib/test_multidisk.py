6c637a7571fb1dc728c3e0bc69d21701c9e2abce
Bobby R. Bruce <bbruce@ucdavis.edu>
2024-04-22 07:49:31 -0700

    tests,stdlib,arch-riscv: Add tests for RISCVMatched Multidisk
    Adds tests to verify the Multidisk features is working for the
    RISCVMatched board.
    
    Change-Id: I1d3851644b33de68565bd79282a7454855b02852

---
+++ tests/gem5/stdlib/test_multidisk.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2024 The Regents of the University of California
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from testlib import *
+
+if config.bin_path:
+    resource_path = config.bin_path
+else:
+    resource_path = joinpath(absdirpath(__file__), "..", "resources")
+
+gem5_verify_config(
+    name=f"test-multidisk-riscvmatched",
+    verifiers=(
+        verifier.MatchStdout(
+            joinpath(getcwd(), "ref", "riscvmatched_multidisk_out.txt")
+        ),
+    ),
+    fixtures=(),
+    config=joinpath(
+        config.base_dir,
+        "tests",
+        "gem5",
+        "stdlib",
+        "configs",
+        "riscvmatched_multidisk.py",
+    ),
+    config_args=["--resource-directtory", resource_path],
+    valid_isas=(constants.riscv_tag,),
+    # Run as part of the weekly tests (booting the RISCVMatchedBoard takes
+    # hours).
+    length=constants.very_long_tag,
+)
