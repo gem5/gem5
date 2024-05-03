781c8cb876585a05b14ffda8a68570fefaba3983
Matthew Poremba <matthew.poremba@amd.com>
2024-05-03 10:06:34 -0700

    dev-amdgpu: Correct missing GART warning
    SDMA ptePde packets are generating a warning that a GART address is
    missing, causing a wrong address to be clobbered by the operation.
    
    This commit fixes this by converting the GART address when the queue is
    running in privledged mode, which is the only mode allowed to use GART
    addresses. This removes the warnings and writes to the correct memory
    region.
    
    Change-Id: I64acac308db2431c5996b876bf4cda704f51cf25

--- src/dev/amdgpu/sdma_engine.cc
+++ src/dev/amdgpu/sdma_engine.cc
@@ -1000,6 +1000,9 @@ SDMAEngine::ptePde(SDMAQueue *q, sdmaPtePde *pkt)
                                              sizeof(uint64_t) * pkt->count, 0,
                                              cb);
     } else {
+        if (q->priv()) {
+            pkt->dest = getGARTAddr(pkt->dest);
+        }
         auto cb = new DmaVirtCallback<uint64_t>(
             [ = ] (const uint64_t &) { ptePdeDone(q, pkt, dmaBuffer); });
         dmaWriteVirt(pkt->dest, sizeof(uint64_t) * pkt->count, cb,
