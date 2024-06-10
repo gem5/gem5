8993f12304e8d184fde276edc4b7fc4028cb9a78
Jarvis Jia <jia44@wisc.edu>
2024-06-10 13:58:24 -0500

    Adding functions to choose replacement policies for GPU
    Adding RP_choose functions to change replacement policies among
    TreePLRU, LRU, FIFO, LFU, LIP, MRU, NRU, RRIP, SecondChance AND ShiPMem replacement
    policies  for TCC, TCP and SQC caches for GPU

--- configs/example/apu_se.py
+++ configs/example/apu_se.py
@@ -380,6 +380,16 @@ parser.add_argument(
     help="Gfx version for gpuNote: gfx902 is not fully supported by ROCm",
 )
 
+parser.add_argument("--tcp-rp", type=str, default="TreePLRURP", help="cache replacement policy"
+                    "policy for tcp")
+
+parser.add_argument("--tcc-rp", type=str, default="TreePLRURP", help="cache replacement policy"
+                    "policy for tcc")
+
+# sqc rp both changes sqc rp and scalar cache rp
+parser.add_argument("--sqc-rp", type=str, default="TreePLRURP", help="cache replacement policy"
+                    "policy for sqc")
+
 Ruby.define_options(parser)
 
 # add TLB options to the parser
