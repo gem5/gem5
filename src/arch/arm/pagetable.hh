0a4086cb36a9cbf9ad01a49f8be2c4df6f0f08b1
Giacomo Travaglini <giacomo.travaglini@arm.com>
2024-07-15 18:57:33 +0100

    arch-arm: Add invalidate method to the TlbEntry
    This is needed for compliance with the AssociativeCache
    container. It will call the invalidate method when
    invalidating the TLB entry
    
    Change-Id: Idb1bc40b5aea8c475146700c81ab79d9980f745d
    Signed-off-by: Giacomo Travaglini <giacomo.travaglini@arm.com>
    Reviewed-by: Andreas Sandberg <andreas.sandberg@arm.com>

--- src/arch/arm/pagetable.hh
+++ src/arch/arm/pagetable.hh
@@ -359,6 +359,13 @@ struct TlbEntry : public ReplaceableEntry, Serializable
         std::swap(xs, rhs.xs);
     }
 
+    /** Need for compliance with the AssociativeCache interface */
+    void
+    invalidate()
+    {
+        valid = false;
+    }
+
     void
     updateVaddr(Addr new_vaddr)
     {
