fe18012dd36300c023f986dc99ac766dea71827b
studyztp <studyztp@gmail.com>
2024-08-06 13:47:34 -0700

    cpu: add the python class
    Add the python classes for the LooppointAnalysis and the
    LooppointAnalysis Manager.
    
    Change-Id: I0a882bc1a9ef03b7b482e871a7160e7c33f9ac08

--- src/cpu/simple/probes/LooppointAnalysis.py
+++ src/cpu/simple/probes/LooppointAnalysis.py
@@ -23,3 +23,68 @@
 # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from m5.objects import SimObject
+from m5.objects.Probe import ProbeListenerObject
+from m5.params import *
+from m5.util.pybind import *
+
+
+class LooppointAnalysis(ProbeListenerObject):
+    """This ProbeListenerObject is meant to attach to the ATOMIC CPU and use
+    the ATOMIC CPU's ppCommit probe point to collect information needed to
+    perform the LoopPoint analysis.
+    It is notified when the ATOMIC CPU commits an instruction.
+    Each LooppointAnalysis object only attaches to one core and listen to one
+    probe point.
+    """
+
+    type = "LooppointAnalysis"
+    cxx_header = "cpu/simple/probes/LooppointAnalysis.hh"
+    cxx_class = "gem5::LooppointAnalysis"
+
+    cxx_exports = [
+        PyBindMethod("startListening"),
+        PyBindMethod("stopListening"),
+        PyBindMethod("getLocalBBV"),
+        PyBindMethod("clearLocalBBV"),
+    ]
+
+    looppoint_analysis_manager = Param.LooppointAnalysisManager(
+        "the LooppointAnalysis manager"
+    )
+    bb_valid_addr_range = Param.AddrRange(
+        AddrRange(start=0, end=0), "the valid address range for basic blocks"
+    )
+    marker_valid_addr_range = Param.AddrRange(
+        AddrRange(start=0, end=0), "the valid address range for markers"
+    )
+    bb_excluded_addr_ranges = VectorParam.AddrRange(
+        [], "the excluded address ranges for basic blocks"
+    )
+    if_listening = Param.Bool(
+        True, "if the LooppointAnalysis is listening to " "the probe point"
+    )
+
+
+class LooppointAnalysisManager(SimObject):
+    """This SimObject is meant to manage the LooppointAnalysis objects and
+    collect the global information needed to perform the LoopPoint analysis
+    across all cores.
+    """
+
+    type = "LooppointAnalysisManager"
+    cxx_header = "cpu/simple/probes/LooppointAnalysis.hh"
+    cxx_class = "gem5::LooppointAnalysisManager"
+
+    cxx_exports = [
+        PyBindMethod("getGlobalBBV"),
+        PyBindMethod("clearGlobalBBV"),
+        PyBindMethod("getGlobalInstCounter"),
+        PyBindMethod("clearGlobalInstCounter"),
+        PyBindMethod("getBackwardBranchCounter"),
+        PyBindMethod("getMostRecentBackwardBranchPC"),
+        PyBindMethod("getMostRecentBackwardBranchCount"),
+    ]
+
+    region_length = Param.Int(100000000, "the length of the region")
