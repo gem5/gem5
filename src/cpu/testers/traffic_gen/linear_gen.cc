33e3bc4ff15e9b1c40cf287c41b87dd3ced661f6
MMysore2 <61029417+MMysore2@users.noreply.github.com>
2024-08-08 12:46:10 -0700

    Updating Traffic Generators (#1416)
    Added documentation for `strided_generator.py` and
    `strided_generator_core.py.`
    
    Updated clarity of documentation for `linear_generator.py`,
    `linear_generator_core.py`, `random_generator.py`, and
    `random_generator_core.py`.
    
    Made `max_addr` exclusive instead of inclusive for strided and linear
    traffic generation in `strided_gen.cc` and `linear_gen.cc`.

--- src/cpu/testers/traffic_gen/linear_gen.cc
+++ src/cpu/testers/traffic_gen/linear_gen.cc
@@ -78,7 +78,7 @@ LinearGen::getNextPacket()
 
     // If we have reached the end of the address space, reset the
     // address to the start of the range
-    if (nextAddr > endAddr) {
+    if (nextAddr >= endAddr) {
         DPRINTF(TrafficGen, "Wrapping address to the start of "
                 "the range\n");
         nextAddr = startAddr;
