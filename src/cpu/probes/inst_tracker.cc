7239d1480fa28a216e3eedd12dc9c8ddaa0ef11c
studyztp <studyztp@gmail.com>
2024-08-07 21:43:09 -0700

    cpu: add GlobalInstTracker and LocalInstTracker
    The GlobalInstTracker manages the global instruction counter and
    responsible for triggering an exit event when the global instruction
    counter reaches the defined threshold.
    
    The LocalInstTracker listens to one core's retiredInsts probe point
    and updates the GlobalInstTracker every time there is an instruction
    committed.
    
    The purpose of this instruction tracker is to raise an instruction
    executed exit event with multi-core simulation.
    
    Related discussion can be found:
    https://github.com/gem5/gem5/issues/1087
    
    Change-Id: Iab6fec57f14f28e590b035506282130ba8662706

---
+++ src/cpu/probes/inst_tracker.cc
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2024 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "cpu/probes/inst_tracker.hh"
+
+namespace gem5
+{
+
+LocalInstTracker::LocalInstTracker(const LocalInstTrackerParams &p)
+    : ProbeListenerObject(p),
+      globalInstTracker(p.global_inst_tracker),
+      ifListening(p.start_listening)
+{
+    DPRINTF(InstTracker, "ifListening = %s\n", ifListening ? "true" : "false");
+}
+
+void
+LocalInstTracker::regProbeListeners()
+{
+    if (ifListening) {
+        if (listeners.empty())
+        {
+            listeners.push_back(new LocalInstTrackerListener(this,
+                                    "RetiredInsts",
+                                    &LocalInstTracker::retiredInstsHandler));
+            DPRINTF(InstTracker, "Listening to RetiredInsts\n");
+        }
+    }
+}
+
+void
+LocalInstTracker::stopListening()
+{
+    ifListening = false;
+    for (auto l = listeners.begin(); l != listeners.end(); ++l) {
+        delete (*l);
+    }
+    listeners.clear();
+    DPRINTF(InstTracker, "Stopped listening\n");
+}
+
+void
+LocalInstTracker::retiredInstsHandler(const uint64_t& inst)
+{
+    globalInstTracker->updateAndCheckInstCount(inst);
+}
+
+GlobalInstTracker::GlobalInstTracker(const GlobalInstTrackerParams &p)
+    : SimObject(p),
+      instCount(0),
+      instThreshold(p.inst_threshold)
+{}
+
+void
+GlobalInstTracker::updateAndCheckInstCount(const uint64_t& inst)
+{
+    instCount ++;
+    if (instCount >= instThreshold) {
+        DPRINTF(InstTracker, "Instruction count reached the threshold\n"
+                                "instCount = %lu\n"
+                                "instThreshold = %lu\n",
+                                instCount, instThreshold);
+
+        exitSimLoopNow("a thread reached the max instruction count");
+    }
+}
+
+} // namespace gem5
---
+++ src/cpu/probes/inst_tracker.hh
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2024 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer;
+ * redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution;
+ * neither the name of the copyright holders nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __CPU_PROBES_INST_TRACKER_HH__
+#define __CPU_PROBES_INST_TRACKER_HH__
+
+#include "debug/InstTracker.hh"
+#include "params/GlobalInstTracker.hh"
+#include "params/LocalInstTracker.hh"
+#include "sim/probe/probe.hh"
+#include "sim/sim_exit.hh"
+
+namespace gem5
+{
+
+class GlobalInstTracker : public SimObject
+{
+  public:
+    GlobalInstTracker(const GlobalInstTrackerParams &params);
+
+    /**
+     * this function is called by the LocalInstTracker object to update the
+      * instruction count and check if the instruction count has reached the
+      * threshold across all the cores.
+     */
+    void updateAndCheckInstCount(const uint64_t& inst);
+
+  private:
+    /** the number of instructions that have been executed across all the cores
+    */
+    uint64_t instCount;
+
+
+    /**
+      * the threshold for the number of instructions that should be executed
+      * before the simulation exits
+     */
+    uint64_t instThreshold;
+
+  public:
+    void changeThreshold(uint64_t new_threshold)
+    {
+      instThreshold = new_threshold;
+      DPRINTF(InstTracker, "Changing the instruction threshold\n"
+                          "instThreshold = %lu\n", instThreshold);
+    };
+
+    void resetCounter()
+    {
+      instCount = 0;
+      DPRINTF(InstTracker, "Resetting the instruction counter\n"
+                                              "instCount = %lu\n", instCount);
+    };
+
+    uint64_t getThreshold() const
+    {
+      return instThreshold;
+    };
+};
+
+class LocalInstTracker : public ProbeListenerObject
+{
+  public:
+    LocalInstTracker(const LocalInstTrackerParams &params);
+
+    /** setup the probelistener */
+    virtual void regProbeListeners();
+
+    /**
+     * this function is called when the ProbePoint "RetiredInsts" is notified
+     *
+     * @param inst the number of retired instructions. It is usually 1.
+     */
+    void retiredInstsHandler(const uint64_t& inst);
+
+  private:
+    typedef ProbeListenerArg<LocalInstTracker, uint64_t>
+                                                    LocalInstTrackerListener;
+
+    /** a boolean variable that determines if the LocalInstTracker is
+     * listening to the ProbePoints or not
+     */
+    bool ifListening;
+
+    /**
+     * the pointer to the GlobalInstTracker object. It is used to update the
+      * instruction count and check if the instruction count has reached the
+      * threshold across all the cores.
+     */
+    GlobalInstTracker *globalInstTracker;
+
+  public:
+    /** stop listening to the ProbePoints */
+    void stopListening();
+
+    /** start listening to the ProbePoints */
+    void startListening()
+    {
+      ifListening = true;
+      regProbeListeners();
+    }
+
+};
+
+}
+
+#endif // __CPU_PROBES_INST_TRACKER_HH__
