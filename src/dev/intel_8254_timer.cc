a7a74fc03fca5ac677d202290ca7395d8fc9739d
Harshil Patel <harshilp2107@gmail.com>
2024-08-12 23:26:27 -0700

    dev, arch-x86: added warning if softstrode is being used
    Change-Id: Ie5ff4853471aca09b9837eefb83afb1f64528019

--- src/dev/intel_8254_timer.cc
+++ src/dev/intel_8254_timer.cc
@@ -210,6 +210,8 @@ Intel8254Timer::Counter::setRW(int rw_val)
 void
 Intel8254Timer::Counter::setMode(int mode_val)
 {
+    if (mode_val == SoftwareStrobe)
+        warn_once("SoftwareStrobe mode is used and it is not well tested\n");
     if (mode_val != InitTc && mode_val != RateGen &&
        mode_val != SquareWave && mode_val != SoftwareStrobe)
         panic("PIT mode %#x is not implemented: \n", mode_val);
@@ -292,8 +294,6 @@ Intel8254Timer::Counter::CounterEvent::process()
         break;
       case RateGen:
       case SquareWave:
-        setTo(counter->period);
-        break;
       case SoftwareStrobe:
         setTo(counter->period);
         break;
