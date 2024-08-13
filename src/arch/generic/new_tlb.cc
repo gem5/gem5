0edc6b3b783046fef544407457d6601af5e4d558
saili.karkare@gmail.com <saili.karkare@gmail.com>
2024-08-12 17:47:12 -0700

    draft for new_tlb

--- src/arch/generic/new_tlb.cc
+++ src/arch/generic/new_tlb.cc
@@ -1,26 +1,20 @@
 
-// come back to this, after .hh is finalized
 
 #include "base/cache/associative_cache.hh"
 #include "generic/new_tlb.hh
 #include "params/NewTLB.hh"
 #include "sim/sim_object.hh"
 
+/*
+Questions:
+- How do I specify the return type?
+*/
+
 namespace gem5
 {
 
-// this will literally just be the actual functions
-// however, its important to take notes 
 
-// questions:
-/*
-- do i just return the TLB entry or do I have to return a type? not sure there
-
-*/
-
-// look at the Translator class, make sure that I can access the Associative cache
-// Associative Cache specific
-
+// Cache Management
 TLBEntry * lookup(----)
     // AssociativeCache::findEntry(const Addr addr)
 
@@ -36,32 +30,42 @@ void flushAll(---)
    
 void evictLRU(---)
     // AssociativeCachefindVictim(const Addr addr)
-    // plus a few more things!
+    // plus a few more functions
 
-void demapPage(---) // not entirely sure
+void demapPage(---)
 
-// all the Fault funcitons might need to be defined as virtual functions
+// Translation
 
+// virtual Fault translate()
+// check permissions
+virtual Fault TLB::translateAtomic(const RequestPtr &req, ThreadContext *tc, BaseMMU::Mode mode)
+virtual Fault TLB::translateFunctional(const RequestPtr &req, ThreadContext *tc, BaseMMU::Mode mode)
+virtual Fault TLB::translateTiming(const RequestPtr &req, ThreadContext *tc, BaseMMU::Mode mode)
+
+
+// Serialization
 void serialize(---)
-    // no idea where the serialize stuff happens
-    tlb[x].serializeSection?
-
 void unserialize(---)
 
 
-// Stats functions:
-
-// AC Functions that I am not sure how they would incorporate, but they would
-/*
-- accessEntry(Entry *entry) - this updates the replacement information for an entry
-- i think this reminds me of the lru sequence?
-- findVictime - this 
-
-
-
-
-*/
+// Statistics
+TLB::TlbStats::TlbStats(statistics::Group *parent)
+  : statistics::Group(parent),
+    ADD_STAT(readHits, statistics::units::Count::get(), "read hits"),
+    ADD_STAT(readMisses, statistics::units::Count::get(), "read misses"),
+    ADD_STAT(readAccesses, statistics::units::Count::get(), "read accesses"),
+    ADD_STAT(writeHits, statistics::units::Count::get(), "write hits"),
+    ADD_STAT(writeMisses, statistics::units::Count::get(), "write misses"),
+    ADD_STAT(writeAccesses, statistics::units::Count::get(), "write accesses"),
+    ADD_STAT(hits, statistics::units::Count::get(),
+             "Total TLB (read and write) hits", readHits + writeHits),
+    ADD_STAT(misses, statistics::units::Count::get(),
+             "Total TLB (read and write) misses", readMisses + writeMisses),
+    ADD_STAT(accesses, statistics::units::Count::get(),
+             "Total TLB (read and write) accesses",
+             readAccesses + writeAccesses)
 
 
 };
+
 
--- src/arch/generic/new_tlb.hh
+++ src/arch/generic/new_tlb.hh
@@ -5,135 +5,102 @@
 #include "base/cache/associative_cache.hh"
 #include "params/NewTLB.hh"
 #include "sim/sim_object.hh"
-
+#include "base/cache/cache_entry.hh"
 #include "arch/generic/new_tlb_entry.hh"
+
+/* General Notes:
+  Public vs. Private vs. protected
+- Private = only this class can access
+- Protected = this class and derived class can access (ex: ISA's that derive from this)
+- Public = any class can access
+
+Virtual Function:
+- a pure virtual function means that its not implemented in the base class
+
+*/
 
 namespace gem5
 {
 
-
-// notttttt really sure what goes in each of these
-class NewTLB : public Translator // inherits from Translator
-
-
-// every internal function is called from AssociativeCache functions
-// instead of AssociativeCache::, would do this->_cache.
-
-// public vs. private vs. protected
+class NewTLB : public Translator
 /*
-- private = something that only this class needs
-- protected = something that the future ISA's would need bc they inherit from them
-- public = something that it doesn't matter if its protected or not
-
-start with all the variables that are necessary
-then move down to all the ones that the ISA's need
-private is the actual last resort
+Types of Functions That Need to Be Added:
+- Constructor (including the Translator Paramenters)
+- Cache Management (called AC functions this->_cache)
+- Translation
+  - Transaltion
+  - Translation Helping Functions (Permissions, Miss Servicing, Etc.)
+  - Different Types of Translation
+- Serialization
+- Statistics
 
 */
 
-
 {
   public:
-    Associate
   protected:
-
-    /*
-    size_t size;
-
-    typedef std::list<TlbEntry *> EntryList;
-    EntryList freeList;         // free entries
-
-    // not sure if we still need this 
-    uint64_t lruSeq;
-
-    // this is riscv
-    Walker *walker; 
-
-    // this is x86
-    friend class Walker;
-    uint32_t configAddress;
-
-
-    struct TlbStats : public statistics::Group
-    {
-        TlbStats(statistics::Group *parent);
-
-        statistics::Scalar readHits;
-        statistics::Scalar readMisses;
-        statistics::Scalar readAccesses;
-        statistics::Scalar writeHits;
-        statistics::Scalar writeMisses;
-        statistics::Scalar writeAccesses;
-
-        statistics::Formula hits;
-        statistics::Formula misses;
-        statistics::Formula accesses;
-    } stats;
-    */ 
-
-
   private:
-    /*
-    uint64_t nextSeq() { return ++lruSeq; }
-
-    void evictLRU();
-
-    void remove(size_t idx);
-
-    Fault translate(const RequestPtr &req, ThreadContext *tc,
-                    BaseMMU::Translation *translation, BaseMMU::Mode mode,
-                    bool &delayed);
-    */
-
   public:
-
-
-    typedef NewTLBParams Params;
-
-    NewTLB(const Params &params) // this is identical to the params in x86
-      : AssociativeCache(name(), p.size, p.assoc,
-        p.replacement_policy, p.indexing_policy),
-      SimObject(p) // this is the constructor?
-    {
-
-    }
-
-    Walker * TLB::getWalker() {
-      return walker;
-    }
-
-    // Associate Cache specific - think about how to make these more compatible 
-    // with the whole translator class stuff
-    TLBEntry * lookup(Addr vpn, uint16_t asid, BaseMMU::Mode mode, bool hidden)
-        // AssociativeCache::findEntry(const Addr addr)
-
-    TLBEntry * insert(Addr vpn, const TlbEntry &entry);
-        // AssociativeCache::insertEntry(const Addr addr, Entry *entry)
-
-    TLB::remove(size_t idx);
-        // AssociativeCache::findEntry(const Addr addr)
-        // AssociativeCache::invalidate(Entry *entry)
-   
-    void flushAll(---);
-        // AssociateCache::clear()
-   
-    void evictLRU(---); // private in x86
-        // AssociativeCachefindVictim(const Addr addr)
-
-    
-
-    /*
-    Fault TLB::translateAtomic(const RequestPtr &req, ThreadContext *tc, BaseMMU::Mode mode)
-    Fault TLB::translateFunctional(const RequestPtr &req, ThreadContext *tc, BaseMMU::Mode mode)
-    Fault TLB::translateTiming(const RequestPtr &req, ThreadContext *tc, BaseMMU::Mode mode)
-    virtual translate 
-    void takeOverFrom(---)
-    */
 
 
 };
-//so basically, we have to use 
+// This is the NewTLB Entry
+
+// [0] necessary imports
+// using the struct TLB Entry to build this 
+class TLBEntry : public CacheEntry, public Serializable
+{ 
+
+  public:
+  // Variables found across the pagetable.hh files across ISA's
+  // [1] Variables
+    Addr vaddr;  // The virtual address corresponding to this entry
+    Addr paddr; // The physical address corresponding to this entry
+    bool uncacheable;  // Whether the page is cacheable or not.
+    bool writable;         // Read permission is always available, assuming it isn't blocked by other mechanisms.
+    unsigned logBytes;  // The size of the page this represents, in address bits.
+    uint64_t lruSeq;         // A sequence number to keep track of LRU.
+
+    /* example of some x86 specific variabeles*/
+    // bool user;         // Whether this page is accesible without being in supervisor mode, lets the caches handle the writeback policy.
+    // bool pwt;
+    // bool global;        // Whether or not to kick this page out on a write to CR3.
+    // bool patBit;         // A bit used to form an index into the PAT table.
+    // bool noExec;         // Whether or not memory on this page can be executed.
+
+
+  // [2] Constructors
+    // change this current implementation to include AC initialization
+    TlbEntry(Addr asn, Addr _vaddr, Addr _paddr, 
+            bool uncacheable, bool read_only); 
+    TlbEntry();
+
+  // [3] Internal Functions
+    void updateVaddr(Addr new_vaddr)
+    {
+        vaddr = new_vaddr;
+    }
+    Addr pageStart()
+    {
+        return paddr;
+    }
+    int size()     // Return the page size in bytes
+    {
+        return (1 << logBytes);
+    }
+
+
+// [4] Extended from Serializable
+    void serialize(CheckpointOut &cp) const override;
+    void unserialize(CheckpointIn &cp) override;
+
+
+// [5] Extra Functions Extended from Cache Entry
+
+};
+
 
 } // namespace gem5
 
 #endif
+
