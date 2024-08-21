cefdd65a7d14972c32eefd3185c1377e51bc3e02
Erin Le <ejle@ucdavis.edu>
2024-08-21 21:16:32 +0000

    configs: add example configuration scripts for RiscvDemoBoard
    Change-Id: I645e520b390654cae7fb7ca5aa0f28ad83cfbd4a

---
+++ configs/example/gem5_library/riscv-demo-board-ubuntu-run.py
@@ -0,0 +1,72 @@
+# Copyright (c) 2021 The Regents of the University of California
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+This script utilizes the RiscvDemoBoard to run a simple Ubunutu boot. The script
+will boot the the OS to login before exiting the simulation.
+
+A detailed terminal output can be found in `m5out/system.pc.com_1.device`.
+
+**Warning:** The RiscvDemoBoard uses the Timing CPU. The boot may take
+considerable time to complete execution.
+`configs/example/gem5_library/x86-ubuntu-run-with-kvm.py` can be referenced as
+an example of booting Ubuntu with a KVM CPU.
+
+Usage
+-----
+
+```
+scons build/RISCV/gem5.opt
+./build/RISCV/gem5.opt configs/example/gem5_library/riscv-demo-board-ubuntu-run.py --workload=riscv-ubuntu-20.04-boot --version=3.0.0
+```
+"""
+
+import argparse
+
+import m5
+
+from gem5.prebuilt.demo.riscv_demo_board import RiscvDemoBoard
+from gem5.resources.resource import obtain_resource
+from gem5.simulate.exit_event import ExitEvent
+from gem5.simulate.simulator import Simulator
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--workload")
+
+parser.add_argument("--version")
+
+args = parser.parse_args()
+
+
+board = RiscvDemoBoard(is_fs=True)
+
+board.set_workload(obtain_resource(args.workload, args.version))
+
+simulator = Simulator(board=board)
+
+
+simulator.run()
