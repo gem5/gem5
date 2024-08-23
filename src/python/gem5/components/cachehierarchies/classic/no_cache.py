48b2197b60995901eab4f07072221df7d9a516c8
Jason Lowe-Power <jason@lowepower.com>
2024-08-23 09:34:51 -0700

    stdlib: Use board get_mem_ports consistently
    Previously, whether the board object or the memory_system returned
    the memory ports was not consistent in the cache_hierarchies
    
    This commit makes it consistently use the board. Note: the board
    is a better place so it can customize the ports (e.g., add I/O
    components or other things.
    
    This commit also makes the arm board consistent with the other
    boards and removes the specialized `get_mem_ports` that was not
    used.
    
    Change-Id: Ibbd5291ae8e0fee10aefdbd8890a653b8bd737df
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- src/python/gem5/components/cachehierarchies/classic/no_cache.py
+++ src/python/gem5/components/cachehierarchies/classic/no_cache.py
@@ -124,7 +124,7 @@ class NoCache(AbstractClassicCacheHierarchy):
         # Set up the system port for functional access from the simulator.
         board.connect_system_port(self.membus.cpu_side_ports)
 
-        for _, port in board.get_memory().get_mem_ports():
+        for _, port in board.get_mem_ports():
             self.membus.mem_side_ports = port
 
     def _setup_coherent_io_bridge(self, board: AbstractBoard) -> None:
