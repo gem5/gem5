be49bf89c028c283b02d0cc2ce5653a0cbd98856
Tommaso Marinelli (imec) <Tommaso.Marinelli@imec.be>
2024-10-02 16:16:19 +0200

    arch-riscv: Enable clone3 syscall in riscv64
    The clone3 syscall, implemented in commit 87e774c, is currently only
    handled for x86-64 in gem5. Clone3 is employed by modern glibc versions
    instead of clone for processes/threads generation (e.g. issue #1204).
    This commit enables the clone3 syscall in riscv64 by adding the
    corresponding handler call, as well as its arguments struct.

--- src/arch/riscv/linux/se_workload.cc
+++ src/arch/riscv/linux/se_workload.cc
@@ -410,6 +410,7 @@ SyscallDescTable<SEWorkload::SyscallABI64> EmuLinux::syscallDescs64 = {
     { 285,  "copy_file_range" },
     { 286,  "preadv2" },
     { 287,  "pwritev2" },
+    { 435,  "clone3", clone3Func<RiscvLinux64> },
     { 1024, "open", openFunc<RiscvLinux64> },
     { 1025, "link", linkFunc },
     { 1026, "unlink", unlinkFunc },
