440999e447a19db88eeff9d4df4d8f7ba1ddf4e6
Giacomo Travaglini <giacomo.travaglini@arm.com>
2024-10-08 11:22:25 +0200

    cpu-o3: Add Crypto OpDesc to the O3 Default FU (#1639)
    There was a bug exposed by a recent PR [1] where until recently the O3
    CPU was executing an instruction even if it did not have the required
    functional unit in the FU pool.
    
    We are adding the crypto descriptors to the Default FU pool in the O3
    cpu so that no panic is encountered upon executing of a crypto
    instruction
    
    [1]: https://github.com/gem5/gem5/pull/1516
    
    Change-Id: Ifaf2f8e4780dfb8ba825a99a02dd587f011dbd23
    
    Reviewed-by: Richard Cooper <richard.cooper@arm.com>
    
    Signed-off-by: Giacomo Travaglini <giacomo.travaglini@arm.com>

--- src/cpu/o3/FuncUnitConfig.py
+++ src/cpu/o3/FuncUnitConfig.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010, 2017, 2020 ARM Limited
+# Copyright (c) 2010, 2017, 2020, 2024 Arm Limited
 # All rights reserved.
 #
 # The license below extends only to copyright in the software and shall
@@ -109,6 +109,14 @@ class SIMD_Unit(FUDesc):
         OpDesc(opClass="SimdExt"),
         OpDesc(opClass="SimdFloatExt"),
         OpDesc(opClass="SimdConfig"),
+        OpDesc(opClass="SimdAes"),
+        OpDesc(opClass="SimdAesMix"),
+        OpDesc(opClass="SimdSha1Hash"),
+        OpDesc(opClass="SimdSha1Hash2"),
+        OpDesc(opClass="SimdSha256Hash"),
+        OpDesc(opClass="SimdSha256Hash2"),
+        OpDesc(opClass="SimdShaSigma2"),
+        OpDesc(opClass="SimdShaSigma3"),
     ]
     count = 4
 
