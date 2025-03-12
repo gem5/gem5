04a5b6b9729ac9ba2a6153046b762af9eec14127
Erin Le <ejle@ucdavis.edu>
2025-03-11 19:52:24 -0700

    tests: Add tests for running longer workloads using multisim
    This commit adds a test that runs X86 22.04, 24.04 boots and
    X86 NPB size S workloads on atomic and timing cores to see if
    multisim can successfully run a number of longer workloads.

---
+++ tests/gem5/multisim/configs/longer-workloads.py
@@ -0,0 +1,146 @@
+# Copyright (c) 2025 The Regents of the University of California
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import argparse
+
+import gem5.utils.multisim as multisim
+from gem5.components.boards.x86_board import X86Board
+from gem5.components.cachehierarchies.classic.private_l1_private_l2_cache_hierarchy import (
+    PrivateL1PrivateL2CacheHierarchy,
+)
+from gem5.components.memory import SingleChannelDDR3_1600
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.isas import ISA
+from gem5.resources.resource import (
+    DiskImageResource,
+    KernelResource,
+)
+from gem5.simulate.simulator import Simulator
+
+multisim.set_num_processes(22)
+
+
+for ubuntu_ver in ["22-04", "24-04"]:
+    for core_type in [CPUTypes.ATOMIC, CPUTypes.TIMING]:
+        cache_hierarchy = PrivateL1PrivateL2CacheHierarchy(
+            l1d_size="16KiB",
+            l1i_size="16KiB",
+            l2_size="256KiB",
+        )
+        memory = SingleChannelDDR3_1600(size="3GiB")
+        processor = SimpleProcessor(
+            cpu_type=CPUTypes.ATOMIC, isa=ISA.X86, num_cores=1
+        )
+        board = X86Board(
+            clk_freq="1GHz",
+            processor=processor,
+            memory=memory,
+            cache_hierarchy=cache_hierarchy,
+        )
+
+        if (
+            ubuntu_ver == "22-04"
+        ):  # It doesn't look like the workloads json has a 22.04 workload
+            board.set_kernel_disk_workload(
+                kernel=KernelResource(
+                    "/home/bees/gem5-resources/src/ubuntu-generic-diskimages/x86-disk-image-22-04/vmlinux-x86-ubuntu"
+                ),
+                disk_image=DiskImageResource(
+                    "/home/bees/gem5-resources/src/ubuntu-generic-diskimages/x86-disk-image-22-04/x86-ubuntu"
+                ),
+                kernel_args=[
+                    "earlyprintk=ttyS0",
+                    "console=ttyS0",
+                    "lpj=7999923",
+                    "root=/dev/sda2",
+                ],
+            )
+        elif ubuntu_ver == "24-04":
+            board.set_kernel_disk_workload(
+                kernel=KernelResource(
+                    "/home/bees/gem5-resources/src/ubuntu-generic-diskimages/x86-disk-image-24-04/vmlinux-x86-ubuntu"
+                ),
+                disk_image=DiskImageResource(
+                    "/home/bees/gem5-resources/src/ubuntu-generic-diskimages/x86-disk-image-24-04/x86-ubuntu"
+                ),
+                kernel_args=[
+                    "earlyprintk=ttyS0",
+                    "console=ttyS0",
+                    "lpj=7999923",
+                    "root=/dev/sda2",
+                ],
+            )
+
+        multisim.add_simulator(
+            Simulator(
+                board=board,
+                id=f"process_x86-{core_type.name.lower()}-{ubuntu_ver}-boot",
+            )
+        )
+
+
+for npb_workload in ["bt", "cg", "ep", "ft", "is", "lu", "mg", "sp", "ua"]:
+    for core_type in [CPUTypes.ATOMIC, CPUTypes.TIMING]:
+
+        cache_hierarchy = PrivateL1PrivateL2CacheHierarchy(
+            l1d_size="16KiB",
+            l1i_size="16KiB",
+            l2_size="256KiB",
+        )
+        memory = SingleChannelDDR3_1600(size="3GiB")
+        processor = SimpleProcessor(
+            cpu_type=core_type, isa=ISA.X86, num_cores=1
+        )
+        board = X86Board(
+            clk_freq="1GHz",
+            processor=processor,
+            memory=memory,
+            cache_hierarchy=cache_hierarchy,
+        )
+
+        board.set_kernel_disk_workload(
+            kernel=KernelResource(
+                "/home/bees/gem5-resources/src/ubuntu-generic-diskimages/x86-disk-image-24-04/vmlinux-x86-ubuntu"
+            ),
+            disk_image=DiskImageResource(
+                "/home/bees/gem5-resources/src/npb-24.04-imgs/disk-image-x86-npb/x86-ubuntu-npb"
+            ),
+            kernel_args=[
+                "earlyprintk=ttyS0",
+                "console=ttyS0",
+                "lpj=7999923",
+                "root=/dev/sda2",
+            ],
+            readfile_contents=f"/home/gem5/NPB3.4-OMP/bin/{npb_workload}.S.x; sleep 5;",
+        )
+
+        multisim.add_simulator(
+            Simulator(
+                board=board,
+                id=f"process_x86-{core_type.name.lower()}-{npb_workload}-s",
+            )
+        )
