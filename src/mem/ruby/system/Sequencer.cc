c0bb6aaf510374e86973645eb84a4a13a9bccfbf
Saúl <33020671+saul44203@users.noreply.github.com>
2025-04-10 10:44:46 +0200

    mem-ruby: return early response for software prefetches

--- src/mem/ruby/system/Sequencer.cc
+++ src/mem/ruby/system/Sequencer.cc
@@ -367,6 +367,14 @@ Sequencer::insertRequest(PacketPtr pkt, RubyRequestType primary_type,
     Addr line_addr = makeLineAddress(pkt->getAddr());
     // Check if there is any outstanding request for the same cache line.
     auto &seq_req_list = m_RequestTable[line_addr];
+
+    // For software prefetches, if the same cache line is already requested we
+    // can return aliased and skip emplacing it on the request table, this way
+    // we won't need to handle any response at all
+    if (pkt->cmd.isSWPrefetch() && seq_req_list.size() > 1) {
+        return RequestStatus_Aliased;
+    }
+
     // Create a default entry
     seq_req_list.emplace_back(pkt, primary_type,
         secondary_type, curCycle());
@@ -737,6 +745,14 @@ Sequencer::hitCallback(SequencerRequest* srequest, DataBlock& data,
             (type == RubyRequestType_Locked_RMW_Read) ||
             (type == RubyRequestType_Load_Linked) ||
             (type == RubyRequestType_ATOMIC_RETURN)) {
+
+            // For software prefetches, since we've already sent an early
+            // response back to the core, we can just ignore this
+            if (pkt->cmd.isSWPrefetch()) {
+                delete pkt;
+                return;
+            }
+
             pkt->setData(
                 data.getData(getOffset(request_address), pkt->getSize()));
 
