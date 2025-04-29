59b8f461def73ed51554410231a972244d27d4f0
Erin Le <ejle@ucdavis.edu>
2025-04-29 16:45:50 -0700

    sim: minor change to convert cerr to DPRINTF in init_signals.cc

--- src/sim/init_signals.cc
+++ src/sim/init_signals.cc
@@ -249,14 +249,16 @@ processExternalSignal(void)
 
     int shm_fd = shm_open(shared_mem_name, O_RDWR, 0666); //0666 = rw-rw-rw-
     if (shm_fd == -1) {
-        std::cerr << "Error: Unable to open shared memory" << std::endl;
+        DPRINTF(ExternalSignal, "Error: Unable to open shared memory: %s\n",
+                std::strerror(errno));
         return;
     }
 
     void* shm_ptr = mmap(0, shared_mem_size, PROT_READ | PROT_WRITE,
         MAP_SHARED, shm_fd, 0);
     if (shm_ptr == MAP_FAILED) {
-        std::cerr << "Error: Unable to map shared memory" << std::endl;
+        DPRINTF(ExternalSignal, "Error: Unable to map shared memory: %s\n",
+                std::strerror(errno));
         close(shm_fd);
         return;
     }
