79423f5141d70e43ce5dd9538c31a883847086e5
Divyang Thakkar <divyangt@umich.edu>
2025-06-04 06:45:25 +0000

    arch-power: VSX Instructions
    Updated the mtvsrd and mfvsrd C++ functions to resolve the compilation errors.

--- src/arch/power/insts/vector_scalar.hh
+++ src/arch/power/insts/vector_scalar.hh
@@ -1,6 +1,9 @@
 #ifndef __ARCH_POWER_INSTS_VECTOR_SCALAR_HH__
 #define __ARCH_POWER_INSTS_VECTOR_SCALAR_HH__
 
+#include "arch/power/regs/float.hh"
+#include "arch/power/regs/int.hh"
+#include "arch/power/regs/vec.hh"
 #include "cpu/thread_context.hh"
 
 namespace gem5
@@ -8,8 +11,8 @@ namespace gem5
 namespace PowerISA
 {
 
-void mtvsrd(ThreadContext *tc, int xt, int ra);
-void mfvsrd(ThreadContext *tc, int rt, int xs);
+void mtvsrd_exec(ThreadContext *tc, int t_s, int ra_vsx, int tx_sx);
+void mfvsrd_exec(ThreadContext *tc, int t_s, int ra_vsx, int tx_sx);
 
 } // namespace PowerISA
 } // namespace gem5
