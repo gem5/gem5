9a85e2650263cdbabcd9ab7deaeecff490eff17b
Erin (Jianghua) Le <jianghuale@gmail.com>
2025-08-07 16:55:17 -0700

    cpu: spacing change in src/cpu/pred/gshare.hh

--- src/cpu/pred/gshare.hh
+++ src/cpu/pred/gshare.hh
@@ -67,8 +67,8 @@ class GshareBP : public BPredUnit
     GshareBP(const GshareBPParams &params);
     bool lookup(ThreadID tid, Addr pc, void * &bp_history) override;
     void updateHistories(ThreadID tid, Addr pc, bool uncond, bool taken,
-      Addr target, const StaticInstPtr &inst,
-      void * &bp_history) override;
+                         Addr target, const StaticInstPtr &inst,
+                         void * &bp_history) override;
     void squash(ThreadID tid, void * &bp_history) override;
     void update(ThreadID tid, Addr pc, bool taken,
                 void * &bp_history, bool squashed,
