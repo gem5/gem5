74efb6900aabb01ff93f8d244f4e7c016fa7ef5e
Erin Le <ejle@ucdavis.edu>
2025-08-13 10:37:19 -0700

    cpu: use standard gem5 copyright for gshare BP

--- src/cpu/pred/gshare.cc
+++ src/cpu/pred/gshare.cc
@@ -1,15 +1,6 @@
 /*
  * Copyright (c) 2024 REDS-HEIG-VD and ESL-EPFL
- * All rights reserved
- *
- * The license below extends only to copyright in the software and shall
- * not be construed as granting a license to any other intellectual
- * property including but not limited to intellectual property relating
- * to a hardware implementation of the functionality of the software
- * licensed hereunder.  You may use the software subject to the license
- * terms below provided that you ensure that this notice is replicated
- * unmodified and in its entirety in all distributions of the software,
- * modified or unmodified, in source code or in binary form.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
