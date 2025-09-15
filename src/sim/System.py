9462f644ece3aa05a269720ce7c06a88bf08ce9f
Jason Lowe-Power <jlowepower@google.com>
2025-09-15 09:59:42 -0700

    sim,cpu: Make SimObject imports consistent in SimObject files
    In SimObject description files, you often need to import other SimObjects
    to use as parameter values. Note that these are only used as parameters,
    so we don't need the C++ backend at this point.
    The problem is that when there is a SimObject description file with multiple
    SimObjects declared, it's hard to know what file you should import *from*.
    This change begins to make a consistent decision to import from
    m5.objects.<SimObjectName> instead of from the file that it was declared in.
    This works because all SimObjects end up in m5.objects.<SimObjectName> modules.
    
    Signed-off-by: Jason Lowe-Power <jlowepower@google.com>

--- src/sim/System.py
+++ src/sim/System.py
@@ -39,7 +39,7 @@
 
 from m5.objects.DVFSHandler import *
 from m5.objects.SimpleMemory import *
-from m5.objects.Workload import StubWorkload
+from m5.objects.StubWorkload import StubWorkload
 from m5.params import *
 from m5.proxy import *
 from m5.SimObject import *
