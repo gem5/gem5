bb0215c0c4c78025b01c8cf7713312979222d1f5
jdmorr95 <104585560+jdmorr95@users.noreply.github.com>
2025-11-19 21:22:49 -0600

    Add gem5 simObject methods to header.
    We will need to override these to both initialize the class
    and to support checkpointing.

--- src/gpu-compute/gpu_dvfs_policy.hh
+++ src/gpu-compute/gpu_dvfs_policy.hh
@@ -1,4 +1,26 @@
 #ifndef __GPU_DVFS_POLICY_HH__
 #define __GPU_DVFS_POLICY_HH__
 
+#include "sim/clocked_object.hh"
+#include "sim/serialize.hh"
+
+namespace gem5 {
+
+
+class GPUDVFSPolicy : public ClockedObject
+{
+
+public:
+    using Params = GPUDVFSPolicyParams;
+
+    void init() override;
+    void serialize(CheckpointOut &cp) const override;
+    void unserialize(CheckpointIn &cp) override;
+
+private:
+
+
+};
+
+}
 #endif // __GPU_DVFS_POLICY_HH__
