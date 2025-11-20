ea3caa6a782264a94018e2256acc14f29f85a4c9
jdmorr95 <104585560+jdmorr95@users.noreply.github.com>
2025-11-19 20:56:48 -0600

    Add basic DVFS setup to GPUFS config script.
    Most arguments are toy values for experimentation.

--- configs/example/gpufs/system/system.py
+++ configs/example/gpufs/system/system.py
@@ -103,6 +103,40 @@ def makeGpuFSSystem(args):
     shader = createGPU(system, args)
     connectGPU(system, args)
 
+    # --------------------- BEGIN DVFS SUPPORT ---------------------
+    if args.dvfs:
+
+        # define clock and volatge domains for DVFS.
+        system.dvfs_voltage_domain = VoltageDomain(volatge=[1.05, 0.95, 0.85]) # TODO toy ranges
+        system.dvfs_clk_domain = SrcClockDomain(
+            clock=['1.5Ghz', '1.0Ghz', '0.5Ghz'],
+            voltage_domain=system.dvfs_voltage_domain,
+            domain_id=67,
+            init_perf_level=1
+        )
+
+        # iterate over all GPU components and rewire their clock domains
+        # to a new DVFS clock domain
+
+        # shaders
+        for cu in shader.CUs:
+            cu.clk_domain = system.dvfs_clk_domain
+
+        # TODO other GPU components could be on some uncore domain.
+
+        # DVFS backend
+        system.dvfs_handler = DVFSHandler(
+            domains=[system.dvfs_clk_domain],
+            sys_clk_domain=system.clk_domain,
+            enable=True, # TODO this code could be put on main path and use args.dvfs
+            transition_latency='1us' # TODO toy value
+        )
+
+        # TODO add policy.
+
+
+    # --------------------- END DVFS SUPPORT ---------------------
+
     # The shader core will be whatever is after the CPU cores are accounted for
     shader_idx = args.num_cpus
     system.cpu.append(shader)
