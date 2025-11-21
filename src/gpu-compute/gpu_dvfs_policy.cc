5fa7df4b2da3f57a4f23a8db9ed9dc87c7e23024
jdmorr95 <104585560+jdmorr95@users.noreply.github.com>
2025-11-20 20:24:35 -0600

    gpu-compute: Add skeleton base gpu dvfs c file.

---
+++ src/gpu-compute/gpu_dvfs_policy.cc
@@ -0,0 +1,43 @@
+#include "gpu-compute/gpu_dvfs_policy.hh"
+
+#include "base/trace.hh"
+
+namespace gem5 {
+
+GPUDVFSPolicy::GPUDVFSPolicy(const Params &p)
+  : ClockedObject(p),
+    dvfsHandler(p.dvfs_handler),
+    gpuDomainId(p.gpu_domain_id),
+    sampleEvent(this), // TODO fix initialization?
+    samplingPeriod(p.sampling_period),
+    voltageOpps(p.voltage_opps.begin(), p.voltage_opps.end()),
+    frequencyOpps(p.frequency_opps.begin(), p.frequency_opps.end())
+
+{
+    DPRINTF(GPUDVFSPolicy, "%s: Initializing GPU DVFS Policy\n", name());
+    fatal_if(!dvfsHandler, "%s: DVFSHandler is required", name());
+    // fatal_if(!gpu,         "%s: GPU device pointer is required", name());
+}
+
+void
+GPUDVFSPolicy::init()
+{
+    ClockedObject::init();
+    scheduleNextSample();
+}
+
+
+// --- Checkpointing ---
+void
+GPUDVFSPolicy::serialize(CheckpointOut &cp) const
+{
+    // TODO implement.
+}
+
+void
+GPUDVFSPolicy::unserialize(CheckpointIn &cp)
+{
+    // TODO implement.
+}
+
+} // namespace gem5
