400befbcb16403b277de7d1c4a0f6ad8c0d71130
jdmorr95 <104585560+jdmorr95@users.noreply.github.com>
2025-11-21 22:19:39 -0600

    gpu-compute: call toy DVFS policy in GPUFS config.

--- configs/example/gpufs/system/system.py
+++ configs/example/gpufs/system/system.py
@@ -135,6 +135,13 @@ def makeGpuFSSystem(args):
         )
 
         # TODO add policy.
+        system.dvfs_policy = ToyDVFSPolicy(
+            dvfs_handler=system.dvfs_handler,
+            gpu_domain_id=67,
+            sampling_period="1000us",  # TODO toy value
+            voltage_opps=[1.05, 0.95, 0.85],
+            frequency_opps=["1.5GHz", "1.0GHz", "0.5GHz"],
+        )
 
     # --------------------- END DVFS SUPPORT ---------------------
 
