b72440c27a1270273509ae4ae17bf88a3748a8f1
xin <tsyw233@gmail.com>
2026-01-19 13:27:12 +0000

    mem-ruby: fix packet conversion to Atomic type with flags:ATOMIC

--- src/mem/ruby/system/Sequencer.cc
+++ src/mem/ruby/system/Sequencer.cc
@@ -45,6 +45,7 @@
 #include "base/compiler.hh"
 #include "base/logging.hh"
 #include "base/str.hh"
+#include "config/ruby_protocol_chi.hh"
 #include "cpu/testers/rubytest/RubyTester.hh"
 #include "debug/LLSC.hh"
 #include "debug/MemoryAccess.hh"
@@ -998,7 +999,7 @@ Sequencer::makeRequest(PacketPtr pkt)
     } else if (pkt->req->isTlbiCmd()) {
         primary_type = secondary_type = tlbiCmdToRubyRequestType(pkt);
         DPRINTF(RubySequencer, "Issuing TLBI\n");
-#if defined (PROTOCOL_CHI)
+#if defined (RUBY_PROTOCOL_CHI)
     } else if (pkt->isAtomicOp()) {
         if (pkt->req->isAtomicReturn()){
             DPRINTF(RubySequencer, "Issuing ATOMIC RETURN \n");
