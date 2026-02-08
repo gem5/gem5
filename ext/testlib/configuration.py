02d29adda927881ad92d51c45feee1f97a112a4b
Erin Le <ejle@ucdavis.edu>
2026-02-08 15:02:52 -0800

    ext-testlib: use .opt for TestLib's --gcov option
    The slowdown when using gem5.debug is too great, so use gem5.opt
    when running gcovr through TestLib instead.

--- ext/testlib/configuration.py
+++ ext/testlib/configuration.py
@@ -388,9 +388,6 @@ def define_post_processors(config):
             return isa
 
     def default_variant(variant):
-        # If running TestLib for gcov, build gem5.debug
-        if config._lookup_val("gcov")[0]:
-            return [[constants.debug_tag]]
         if not variant[0]:
             # Default variant is only opt. No need to run tests with multiple
             # different compilation targets
