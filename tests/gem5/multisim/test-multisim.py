932267682d2f7acdd494c09c7a314caa1eac6271
Erin Le <ejle@ucdavis.edu>
2026-03-20 11:25:14 -0700

    tests: remove unneeded imports from multisim test script

--- tests/gem5/multisim/test-multisim.py
+++ tests/gem5/multisim/test-multisim.py
@@ -24,10 +24,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
-import os
-import re
-
 from testlib import *
 from testlib.log import *
 
