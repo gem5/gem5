e61d55e86424d04f665447e6f409c89bdf3f56c6
Erin Le <ejle@ucdavis.edu>
2026-03-20 20:08:01 -0700

    tests: update copyright dates

--- tests/gem5/multisim/test-multisim.py
+++ tests/gem5/multisim/test-multisim.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 The Regents of the University of California
+# Copyright (c) 2025-2026 The Regents of the University of California
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
