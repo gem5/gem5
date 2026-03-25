54df5d9d068f8558c70a6dcbf673e31af4a9a3a5
Erin Le <ejle@ucdavis.edu>
2026-03-24 18:25:35 -0700

    tests: add multisim tests to run to avoid conflicts

--- tests/gem5/multisim/test-multisim.py
+++ tests/gem5/multisim/test-multisim.py
@@ -24,7 +24,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
 from testlib import *
 from testlib.log import *
 
@@ -48,6 +47,98 @@ gem5_verify_config(
     valid_isas=(constants.all_compiled_tag,),
     valid_hosts=constants.supported_hosts,
     length=constants.quick_tag,
+    uses_kvm=False,
+)
+
+gem5_verify_config(
+    name="test-multisim-hello-save-checkpoint",
+    fixtures=(),
+    verifiers=(),
+    gem5_args=[
+        "-m",
+        "gem5.utils.multisim",
+    ],
+    config=joinpath(
+        config.base_dir,
+        "tests",
+        "gem5",
+        "multisim",
+        "configs",
+        "hello-save-checkpoint-hypercall.py",
+    ),
+    config_args=[],
+    valid_isas=(constants.all_compiled_tag,),
+    valid_hosts=constants.supported_hosts,
+    length=constants.quick_tag,
+    uses_kvm=False,
+)
+
+gem5_verify_config(
+    name="test-multisim-riscv-hello-save-checkpoint",
+    fixtures=(),
+    verifiers=(),
+    gem5_args=[
+        "-m",
+        "gem5.utils.multisim",
+    ],
+    config=joinpath(
+        config.base_dir,
+        "tests",
+        "gem5",
+        "multisim",
+        "configs",
+        "riscv-hello-save-checkpoint-hypercall.py",
+    ),
+    config_args=[],
+    valid_isas=(constants.all_compiled_tag,),
+    valid_hosts=constants.supported_hosts,
+    length=constants.quick_tag,
+    uses_kvm=False,
+)
+
+gem5_verify_config(
+    name="test-multisim-varying-length-workloads",
+    fixtures=(),
+    verifiers=(),
+    gem5_args=[
+        "-m",
+        "gem5.utils.multisim",
+    ],
+    config=joinpath(
+        config.base_dir,
+        "tests",
+        "gem5",
+        "multisim",
+        "configs",
+        "varying-length-10-proc.py",
+    ),
+    config_args=[],
+    valid_isas=(constants.all_compiled_tag,),
+    valid_hosts=constants.supported_hosts,
+    length=constants.very_long_tag,
+    uses_kvm=False,
+)
+
+gem5_verify_config(
+    name="test-multisim-x86-processor-switch",
+    fixtures=(),
+    verifiers=(),
+    gem5_args=[
+        "-m",
+        "gem5.utils.multisim",
+    ],
+    config=joinpath(
+        config.base_dir,
+        "tests",
+        "gem5",
+        "multisim",
+        "configs",
+        "x86-processor-switch.py",
+    ),
+    config_args=[],
+    valid_isas=(constants.all_compiled_tag,),
+    valid_hosts=constants.supported_hosts,
+    length=constants.very_long_tag,
     uses_kvm=False,
 )
 
