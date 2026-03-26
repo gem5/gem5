d36921b8266996f608f35bcfa99d6eb07618eef2
Erin (Jianghua) Le <ejle@ucdavis.edu>
2026-03-26 11:10:41 -0700

    tests: Add tests for processor switch with MultiSim (#2083)
    This PR adds a test that checks if switching processors works when using
    MultiSim. It tests switching between combinations of Atomic, Timing, and
    O3 core types by running an X86 kernel boot on one core type, then
    switching to another core type and running for another 1 billion ticks.
    
    ---------
    
    Co-authored-by: Harshil Patel <hpppatel@ucdavis.edu>

--- tests/gem5/multisim/test-multisim.py
+++ tests/gem5/multisim/test-multisim.py
@@ -118,6 +118,30 @@ gem5_verify_config(
     length=constants.very_long_tag,
     uses_kvm=False,
 )
+
+gem5_verify_config(
+    name="test-multisim-x86-processor-switch",
+    fixtures=(),
+    verifiers=(),
+    gem5_args=[
+        "-m",
+        "gem5.utils.multisim",
+    ],
+    config=joinpath(
+        config.base_dir,
+        "tests",
+        "gem5",
+        "multisim",
+        "configs",
+        "x86-processor-switch.py",
+    ),
+    config_args=[],
+    valid_isas=(constants.all_compiled_tag,),
+    valid_hosts=constants.supported_hosts,
+    length=constants.very_long_tag,
+    uses_kvm=False,
+)
+
 # For now, skip this test because there are issues with obtaining one of the
 # checkpoints with obtain_resource
 # gem5_verify_config(
