gem5/gem5

commit d856e5ce7154202f050dafdb7a638e1146081507

arch,base: Restructure the object file loaders.
This change creates a distinction between object files which hold
executable code, and flat files which don't. The first type of files
have entry points, symbols, etc., while the others are just blobs which
can be shoved into memory. Rather than have those aspects but stub
them out, this change creates a new base class which simply doesn't
have them.

This change also restructures the ELF loader since it's main function
was quite long and doing multiple jobs.

It stops passing the architecture and operating system to the
ObjectFile constructor, since those might not be known at the very top
of the constructor. Instead, those default to Uknown*, and then are
filled in in the constructor body if appropriate. This removes a lot
of plumbing that was hard to actually use in practice.

It also introduces a mechanism to collect generic object file formats
so that they can be tried one by one by the general createObjectFile
function, rather than listing them all there one by one. It's unlikely
that new types of object files will need to be added in a modular way
without being able to modify the core loader code, but it's cleaner to
have that abstraction and modularization like is already there for
process loaders.

Finally, to make it possible to share the code which handles zipped
files for both true object files and also files which will be loaded
into memory but are just blobs, that mechanism is pulled out into a
new class called ImageFileData. It holds a collection of segments
which are set up by the object file and may refer to regions of the
original file, buffers maintained elsewhere, or even nothing to support
bss-es. shared_ptr is used to make it easier to keep track of that
information without having to do so explicitly or worry about deleting
a buffer before everyone was done using it.

Change-Id: I92890266f2ba0a703803cccad675a3ab41f2c4af
Reviewed-on: https://gem5-review.googlesource.com/c/public/gem5/+/21467

src/base/loader/elf_object.cc
@@ -61,309 +61,253 @@
 #include "sim/byteswap.hh"
 
 ObjectFile *
-ElfObject::tryFile(const std::string &fname, size_t len, uint8_t *data,
-                   bool skip_interp_check)
+ElfObjectFormat::load(ImageFileDataPtr ifd)
 {
     // check that header matches library version
     if (elf_version(EV_CURRENT) == EV_NONE)
         panic("wrong elf version number!");
 
+    ObjectFile *object = nullptr;
+
     // get a pointer to elf structure
     // Check that we actually have a elf file
-    Elf *elf = elf_memory((char*)data, len);
+    Elf *elf =
+        elf_memory((char *)const_cast<uint8_t *>(ifd->data()), ifd->len());
     assert(elf);
 
     GElf_Ehdr ehdr;
-    if (gelf_getehdr(elf, &ehdr) == 0) {
+    if (gelf_getehdr(elf, &ehdr) == 0)
         DPRINTFR(Loader, "Not ELF\n");
-        elf_end(elf);
-        return NULL;
-    }
-
-    // Detect the architecture
-    Arch arch = UnknownArch;
-    if (ehdr.e_machine == EM_SPARC64 ||
-        (ehdr.e_machine == EM_SPARC &&
-         ehdr.e_ident[EI_CLASS] == ELFCLASS64) ||
-        ehdr.e_machine == EM_SPARCV9) {
-        arch = SPARC64;
-    } else if (ehdr.e_machine == EM_SPARC32PLUS ||
-               (ehdr.e_machine == EM_SPARC &&
-                ehdr.e_ident[EI_CLASS] == ELFCLASS32)) {
-        arch = SPARC32;
-    } else if (ehdr.e_machine == EM_MIPS &&
-               ehdr.e_ident[EI_CLASS] == ELFCLASS32) {
-        arch = Mips;
-        if (ehdr.e_ident[EI_DATA] != ELFDATA2LSB) {
-            fatal("The binary you're trying to load is compiled for big "
-                  "endian MIPS. gem5\nonly supports little endian MIPS. "
-                  "Please recompile your binary.\n");
-        }
-    } else if (ehdr.e_machine == EM_X86_64 &&
-               ehdr.e_ident[EI_CLASS] == ELFCLASS64) {
-        arch = X86_64;
-    } else if (ehdr.e_machine == EM_386 &&
-               ehdr.e_ident[EI_CLASS] == ELFCLASS32) {
-        arch = I386;
-    } else if (ehdr.e_machine == EM_ARM &&
-               ehdr.e_ident[EI_CLASS] == ELFCLASS32) {
-        arch = bits(ehdr.e_entry, 0) ? Thumb : Arm;
-    } else if (ehdr.e_machine == EM_AARCH64 &&
-               ehdr.e_ident[EI_CLASS] == ELFCLASS64) {
-        arch = Arm64;
-    } else if (ehdr.e_machine == EM_RISCV) {
-        arch = (ehdr.e_ident[EI_CLASS] == ELFCLASS64) ? Riscv64 : Riscv32;
-    } else if (ehdr.e_machine == EM_PPC &&
-               ehdr.e_ident[EI_CLASS] == ELFCLASS32) {
-        arch = Power;
-        if (ehdr.e_ident[EI_DATA] != ELFDATA2MSB) {
-            fatal("The binary you're trying to load is compiled for "
-                  "little endian Power.\ngem5 only supports big "
-                  "endian Power. Please recompile your binary.\n");
-        }
-    } else if (ehdr.e_machine == EM_PPC64) {
-        fatal("The binary you're trying to load is compiled for 64-bit "
-              "Power. M5\n only supports 32-bit Power. Please "
-              "recompile your binary.\n");
-    } else if (ehdr.e_ident[EI_CLASS] == ELFCLASS64) {
-        // Since we don't know how to check for alpha right now, we'll
-        // just assume if it wasn't something else and it's 64 bit, that's
-        // what it must be.
-        arch = Alpha;
-    } else {
-        warn("Unknown architecture: %d\n", ehdr.e_machine);
-        arch = UnknownArch;
-    }
-
-    // Detect the operating system
-    OpSys op_sys;
-    switch (ehdr.e_ident[EI_OSABI]) {
-      case ELFOSABI_LINUX:
-        op_sys = Linux;
-        break;
-      case ELFOSABI_SOLARIS:
-        op_sys = Solaris;
-        break;
-      case ELFOSABI_TRU64:
-        op_sys = Tru64;
-        break;
-      case ELFOSABI_ARM:
-        op_sys = LinuxArmOABI;
-        break;
-      case ELFOSABI_FREEBSD:
-        op_sys = FreeBSD;
-        break;
-      default:
-        op_sys = UnknownOpSys;
-    }
-
-    // Take a look at the .note.ABI section.
-    // It can let us know what's what.
-    if (op_sys == UnknownOpSys) {
-        int sec_idx = 1;
-
-        // Get the first section
-        Elf_Scn *section = elf_getscn(elf, sec_idx);
-
-        // While there are no more sections
-        while (section && op_sys == UnknownOpSys) {
-            GElf_Shdr shdr;
-            gelf_getshdr(section, &shdr);
-
-            char *e_str = elf_strptr(elf, ehdr.e_shstrndx, shdr.sh_name);
-            if (shdr.sh_type == SHT_NOTE &&
-                !strcmp(".note.ABI-tag", e_str)) {
-                // we have found a ABI note section
-                // Check the 5th 32bit word for OS  0 == linux, 1 == hurd,
-                // 2 == solaris, 3 == freebsd
-                Elf_Data *raw_data = elf_rawdata(section, NULL);
-                assert(raw_data && raw_data->d_buf);
-
-                uint32_t raw_abi = ((uint32_t*)raw_data->d_buf)[4];
-                bool is_le = ehdr.e_ident[EI_DATA] == ELFDATA2LSB;
-                uint32_t os_abi = is_le ? htole(raw_abi) : htobe(raw_abi);
-
-                switch (os_abi) {
-                  case 0:
-                    op_sys = Linux;
-                    break;
-                  case 1:
-                    fatal("gem5 does not support the HURD ABI.\n");
-                  case 2:
-                    op_sys = Solaris;
-                    break;
-                  case 3:
-                    op_sys = FreeBSD;
-                    break;
-                }
-            } // if section found
-
-            if (!strcmp(".SUNW_version", e_str) ||
-                !strcmp(".stab.index", e_str))
-                op_sys = Solaris;
-
-            section = elf_getscn(elf, ++sec_idx);
-        } // while sections
-    }
-
-    ElfObject * result = new ElfObject(fname, len, data, arch, op_sys);
-
-    // The number of headers in the file
-    result->_programHeaderCount = ehdr.e_phnum;
-    // Record the size of each entry
-    result->_programHeaderSize = ehdr.e_phentsize;
-    result->_programHeaderTable = 0;
-    if (result->_programHeaderCount) { // If there is a program header table
-        // Figure out the virtual address of the header table in the
-        // final memory image. We use the program headers themselves
-        // to translate from a file offset to the address in the image.
-        GElf_Phdr phdr;
-        uint64_t e_phoff = ehdr.e_phoff;
-
-        for (int i = 0; i < result->_programHeaderCount; i++) {
-            gelf_getphdr(elf, i, &phdr);
-            // Check if we've found the segment with the headers in it
-            if (phdr.p_offset <= e_phoff &&
-                phdr.p_offset + phdr.p_filesz > e_phoff) {
-                result->_programHeaderTable =
-                    phdr.p_paddr + (e_phoff - phdr.p_offset);
-                break;
-            }
-        }
-    }
-
-    if (!skip_interp_check) {
-        for (int i = 0; i < ehdr.e_phnum; i++) {
-            GElf_Phdr phdr;
-            M5_VAR_USED void *check_p = gelf_getphdr(elf, i, &phdr);
-            assert(check_p != nullptr);
-
-           if (phdr.p_type != PT_INTERP)
-                continue;
-
-            char *interp_path = (char*)data + phdr.p_offset;
-            int fd = open(interp_path, O_RDONLY);
-            if (fd == -1)
-                fatal("Unable to open dynamic executable's interpreter.\n");
-
-            struct stat sb;
-            M5_VAR_USED int check_i = fstat(fd, &sb);
-            assert(check_i == 0);
-
-            void *mm = mmap(nullptr, sb.st_size, PROT_READ,
-                            MAP_PRIVATE, fd, 0);
-            assert(mm != MAP_FAILED);
-            close(fd);
-
-            uint8_t *interp_image = (uint8_t*)mm;
-            ObjectFile *obj = tryFile(interp_path, sb.st_size,
-                                      interp_image, true);
-            assert(obj != nullptr);
-            result->interpreter = dynamic_cast<ElfObject*>(obj);
-            assert(result->interpreter != nullptr);
-            break;
-        }
-    }
+    else
+        object = new ElfObject(ifd);
 
     elf_end(elf);
-    return result;
+
+    return object;
 }
 
-ElfObject::ElfObject(const std::string &_filename, size_t _len,
-                     uint8_t *_data, Arch _arch, OpSys _op_sys)
-    : ObjectFile(_filename, _len, _data, _arch, _op_sys),
-      _programHeaderTable(0), _programHeaderSize(0), _programHeaderCount(0),
-      interpreter(nullptr), ldBias(0), relocate(true),
-      ldMin(std::numeric_limits<Addr>::max()),
-      ldMax(std::numeric_limits<Addr>::min())
+namespace
 {
-    // check that header matches library version
-    if (elf_version(EV_CURRENT) == EV_NONE)
-        panic("wrong elf version number!");
 
+ElfObjectFormat elfObjectFormat;
+
+} // anonymous namespace
+
+ElfObject::ElfObject(ImageFileDataPtr ifd) : ObjectFile(ifd)
+{
     // get a pointer to elf structure
-    Elf *elf = elf_memory((char*)fileData,len);
+    elf = elf_memory((char *)const_cast<uint8_t *>(imageData->data()),
+                     imageData->len());
     assert(elf);
+    gelf_getehdr(elf, &ehdr);
 
-    // Check that we actually have a elf file
-    GElf_Ehdr ehdr;
-    if (gelf_getehdr(elf, &ehdr) ==0) {
-        panic("Not ELF, shouldn't be here");
-    }
+    determineArch();
+    determineOpSys();
 
     entry = ehdr.e_entry;
+    _programHeaderCount = ehdr.e_phnum;
+    _programHeaderSize = ehdr.e_phentsize;
 
-    int sec_idx = 1;
-
-    // Go through all the segments in the program, record them, and scrape
-    // out information about the text, data, and bss areas needed by other
-    // code.
+    // Go through all the segments in the program and record them.
     for (int i = 0; i < ehdr.e_phnum; ++i) {
         GElf_Phdr phdr;
         if (gelf_getphdr(elf, i, &phdr) == 0) {
             panic("gelf_getphdr failed for segment %d.", i);
         }
 
-        // for now we don't care about non-loadable segments
-        if (phdr.p_type != PT_LOAD)
-            continue;
-
-        ldMin = std::min(ldMin, phdr.p_vaddr);
-        ldMax = std::max(ldMax, phdr.p_vaddr + phdr.p_memsz);
-
-        std::string name;
-
-        // Get the first section
-        Elf_Scn *section = elf_getscn(elf, sec_idx);
-
-        // Name segments after the sections they contain.
-        while (section) {
-            GElf_Shdr shdr;
-            gelf_getshdr(section, &shdr);
-            char *sec_name = elf_strptr(elf, ehdr.e_shstrndx, shdr.sh_name);
-
-            if (!sec_name) {
-                Elf_Error errorNum = (Elf_Error)elf_errno();
-                if (errorNum != ELF_E_NONE) {
-                    const char *errorMessage = elf_errmsg(errorNum);
-                    fatal("Error from libelf: %s.\n", errorMessage);
-                }
-            }
-
-            if (shdr.sh_addr >= ldMin && shdr.sh_addr < ldMax) {
-                if (name != "")
-                    name += ",";
-                name += sec_name;
-            }
-
-            section = elf_getscn(elf, ++sec_idx);
-        }
-
-        image.addSegment(name, phdr.p_paddr, fileData + phdr.p_offset,
-                         phdr.p_filesz);
-        Addr uninitialized = phdr.p_memsz - phdr.p_filesz;
-        if (uninitialized) {
-            // There may be parts of a segment which aren't included in the
-            // file. In those cases, we need to create a new segment with no
-            // data to take up the extra space. This should be zeroed when
-            // loaded into memory.
-            image.addSegment(name + "(uninitialized)",
-                    phdr.p_paddr + phdr.p_filesz, nullptr, uninitialized);
+        if (phdr.p_type == PT_LOAD)
+            handleLoadableSegment(phdr);
+        if (phdr.p_type == PT_INTERP) {
+            // Make sure the interpreter is an valid ELF file.
+            char *interp_path = (char *)imageData->data() + phdr.p_offset;
+            ObjectFile *obj = createObjectFile(interp_path);
+            interpreter = dynamic_cast<ElfObject *>(obj);
+            assert(interpreter != nullptr);
         }
     }
 
     // should have found at least one loadable segment
     warn_if(image.segments().empty(),
-            "No loadable segments in '%s'. ELF file corrupted?\n", filename);
+            "No loadable segments in '%s'. ELF file corrupted?\n",
+            imageData->filename());
 
     for (auto &seg: image.segments())
         DPRINTFR(Loader, "%s\n", seg);
 
-    elf_end(elf);
-
     // We will actually read the sections when we need to load them
 }
 
+void
+ElfObject::determineArch()
+{
+    auto &emach = ehdr.e_machine;
+    auto &eclass = ehdr.e_ident[EI_CLASS];
+    auto &edata = ehdr.e_ident[EI_DATA];
+
+    // Detect the architecture
+    if (emach == EM_SPARC64 || (emach == EM_SPARC && eclass == ELFCLASS64) ||
+        emach == EM_SPARCV9) {
+        arch = SPARC64;
+    } else if (emach == EM_SPARC32PLUS ||
+               (emach == EM_SPARC && eclass == ELFCLASS32)) {
+        arch = SPARC32;
+    } else if (emach == EM_MIPS && eclass == ELFCLASS32) {
+        arch = Mips;
+        if (edata != ELFDATA2LSB) {
+            fatal("The binary you're trying to load is compiled for big "
+                  "endian MIPS. gem5\nonly supports little endian MIPS. "
+                  "Please recompile your binary.\n");
+        }
+    } else if (emach == EM_X86_64 && eclass == ELFCLASS64) {
+        arch = X86_64;
+    } else if (emach == EM_386 && eclass == ELFCLASS32) {
+        arch = I386;
+    } else if (emach == EM_ARM && eclass == ELFCLASS32) {
+        arch = bits(ehdr.e_entry, 0) ? Thumb : Arm;
+    } else if (emach == EM_AARCH64 && eclass == ELFCLASS64) {
+        arch = Arm64;
+    } else if (emach == EM_RISCV) {
+        arch = (eclass == ELFCLASS64) ? Riscv64 : Riscv32;
+    } else if (emach == EM_PPC && eclass == ELFCLASS32) {
+        arch = Power;
+        if (edata != ELFDATA2MSB) {
+            fatal("The binary you're trying to load is compiled for "
+                  "little endian Power.\ngem5 only supports big "
+                  "endian Power. Please recompile your binary.\n");
+        }
+    } else if (emach == EM_PPC64) {
+        fatal("The binary you're trying to load is compiled for 64-bit "
+              "Power. M5\n only supports 32-bit Power. Please "
+              "recompile your binary.\n");
+    } else if (eclass == ELFCLASS64) {
+        // Since we don't know how to check for alpha right now, we'll
+        // just assume if it wasn't something else and it's 64 bit, that's
+        // what it must be.
+        arch = Alpha;
+    } else {
+        warn("Unknown architecture: %d\n", emach);
+    }
+}
+
+void
+ElfObject::determineOpSys()
+{
+    // Detect the operating system
+    switch (ehdr.e_ident[EI_OSABI]) {
+      case ELFOSABI_LINUX:
+        opSys = Linux;
+        return;
+      case ELFOSABI_SOLARIS:
+        opSys = Solaris;
+        return;
+      case ELFOSABI_TRU64:
+        opSys = Tru64;
+        return;
+      case ELFOSABI_ARM:
+        opSys = LinuxArmOABI;
+        return;
+      case ELFOSABI_FREEBSD:
+        opSys = FreeBSD;
+        return;
+      default:
+        opSys = UnknownOpSys;
+    }
+
+    Elf_Scn *section = elf_getscn(elf, 1);
+    for (int sec_idx = 1; section; section = elf_getscn(elf, ++sec_idx)) {
+        GElf_Shdr shdr;
+        gelf_getshdr(section, &shdr);
+
+        char *e_str = elf_strptr(elf, ehdr.e_shstrndx, shdr.sh_name);
+        if (shdr.sh_type == SHT_NOTE && !strcmp(".note.ABI-tag", e_str)) {
+            // we have found a ABI note section
+            // Check the 5th 32bit word for OS  0 == linux, 1 == hurd,
+            // 2 == solaris, 3 == freebsd
+            Elf_Data *raw_data = elf_rawdata(section, nullptr);
+            assert(raw_data && raw_data->d_buf);
+
+            uint32_t raw_abi = ((uint32_t *)raw_data->d_buf)[4];
+            bool is_le = ehdr.e_ident[EI_DATA] == ELFDATA2LSB;
+            uint32_t os_abi = is_le ? htole(raw_abi) : htobe(raw_abi);
+
+            switch (os_abi) {
+              case 0:
+                opSys = Linux;
+                return;
+              case 1:
+                fatal("gem5 does not support the HURD ABI.\n");
+              case 2:
+                opSys = Solaris;
+                return;
+              case 3:
+                opSys = FreeBSD;
+                return;
+            }
+        }
+
+        if (!strcmp(".SUNW_version", e_str) || !strcmp(".stab.index", e_str)) {
+            opSys = Solaris;
+            return;
+        }
+    }
+}
+
+void
+ElfObject::handleLoadableSegment(GElf_Phdr phdr)
+{
+    Addr mem_start = phdr.p_vaddr;
+    Addr mem_end = mem_start + phdr.p_memsz;
+    Addr file_start = phdr.p_offset;
+    Addr file_end = file_start + phdr.p_filesz;
+
+    std::string name;
+
+    // Name segments after the sections they contain.
+    Elf_Scn *section = elf_getscn(elf, 1);
+    for (int sec_idx = 1; section; section = elf_getscn(elf, ++sec_idx)) {
+        GElf_Shdr shdr;
+        gelf_getshdr(section, &shdr);
+        char *sec_name = elf_strptr(elf, ehdr.e_shstrndx, shdr.sh_name);
+
+        if (!sec_name) {
+            Elf_Error errorNum = (Elf_Error)elf_errno();
+            if (errorNum != ELF_E_NONE) {
+                const char *errorMessage = elf_errmsg(errorNum);
+                fatal("Error from libelf: %s.\n", errorMessage);
+            }
+        }
+
+        if (shdr.sh_addr >= mem_start && shdr.sh_addr < mem_end) {
+            if (name != "")
+                name += ",";
+            name += sec_name;
+        }
+    }
+
+    image.addSegment({ name, phdr.p_paddr, imageData,
+                       phdr.p_offset, phdr.p_filesz });
+    Addr uninitialized = phdr.p_memsz - phdr.p_filesz;
+    if (uninitialized) {
+        // There may be parts of a segment which aren't included in the
+        // file. In those cases, we need to create a new segment with no
+        // data to take up the extra space. This should be zeroed when
+        // loaded into memory.
+        image.addSegment({ name + "(uninitialized)",
+                           phdr.p_paddr + phdr.p_filesz, uninitialized });
+    }
+
+    // If there is a program header table, figure out the virtual
+    // address of the header table in the final memory image. We use
+    // the program headers themselves to translate from a file offset
+    // to the address in the image.
+    if (file_start <= ehdr.e_phoff && file_end > ehdr.e_phoff)
+        _programHeaderTable = mem_start + (ehdr.e_phoff - file_start);
+}
+
+ElfObject::~ElfObject()
+{
+    elf_end(elf);
+}
 
 bool
 ElfObject::loadSomeSymbols(SymbolTable *symtab, int binding, Addr mask,
@@ -377,7 +321,8 @@ ElfObject::loadSomeSymbols(SymbolTable *symtab, int binding, Addr mask,
         panic("wrong elf version number!");
 
     // get a pointer to elf structure
-    Elf *elf = elf_memory((char*)fileData,len);
+    Elf *elf = elf_memory((char *)const_cast<uint8_t *>(
+                imageData->data()), imageData->len());
     assert(elf != NULL);
 
     // Get the first section
@@ -401,7 +346,8 @@ ElfObject::loadSomeSymbols(SymbolTable *symtab, int binding, Addr mask,
                 GElf_Sym sym;
                 gelf_getsym(data, i, &sym);
                 if (GELF_ST_BIND(sym.st_info) == binding) {
-                    char *sym_name = elf_strptr(elf, shdr.sh_link, sym.st_name);
+                    char *sym_name =
+                        elf_strptr(elf, shdr.sh_link, sym.st_name);
                     if (sym_name && sym_name[0] != '$') {
                         Addr value = sym.st_value - base + offset;
                         if (symtab->insert(value & mask, sym_name)) {
@@ -473,7 +419,9 @@ ElfObject::getSections()
         panic("wrong elf version number!");
 
     // get a pointer to elf structure
-    Elf *elf = elf_memory((char*)fileData,len);
+    Elf *elf =
+        elf_memory((char *)const_cast<uint8_t *>(imageData->data()),
+                imageData->len());
     assert(elf != NULL);
 
     // Check that we actually have a elf file

src/base/loader/elf_object.hh
@@ -40,48 +40,59 @@
  * Authors: Steve Reinhardt
  */
 
-#ifndef __ELF_OBJECT_HH__
-#define __ELF_OBJECT_HH__
+#ifndef __BASE_LOADER_ELF_OBJECT_HH__
+#define __BASE_LOADER_ELF_OBJECT_HH__
 
 #include <set>
 #include <vector>
 
 #include "base/loader/object_file.hh"
+#include "gelf.h"
+
+class ElfObjectFormat : public ObjectFileFormat
+{
+  public:
+    ObjectFile *load(ImageFileDataPtr data) override;
+};
 
 class ElfObject : public ObjectFile
 {
   protected:
+    Elf *elf;
+    GElf_Ehdr ehdr;
+
+    void determineArch();
+    void determineOpSys();
+    void handleLoadableSegment(GElf_Phdr phdr);
+
     // These values are provided to a linux process by the kernel, so we
     // need to keep them around.
-    Addr _programHeaderTable;
-    uint16_t _programHeaderSize;
-    uint16_t _programHeaderCount;
+    Addr _programHeaderTable = 0;
+    uint16_t _programHeaderSize = 0;
+    uint16_t _programHeaderCount = 0;
     std::set<std::string> sectionNames;
 
-    ElfObject *interpreter;
+    ElfObject *interpreter = nullptr;
 
     // An interpreter load bias is the location in the process address space
     // where the interpreter is chosen to reside. Typically, this is carved
     // out of the top of the mmap reserve section.
-    Addr ldBias;
+    Addr ldBias = 0;
 
     // The interpreter is typically a relocatable shared library and will
     // have a default value of zero which means that it does not care where
     // it is placed. However, the loader can be compiled and linked so that
     // it does care and needs a specific entry point.
-    bool relocate;
+    bool relocate = true;
 
     // The ldMin and ldMax fields are required to know how large of an
     // area is required to map the interpreter.
-    Addr ldMin;
-    Addr ldMax;
+    Addr ldMin = MaxAddr;
+    Addr ldMax = MaxAddr;
 
     /// Helper functions for loadGlobalSymbols() and loadLocalSymbols().
     bool loadSomeSymbols(SymbolTable *symtab, int binding, Addr mask,
                          Addr base, Addr offset);
-
-    ElfObject(const std::string &_filename, size_t _len, uint8_t *_data,
-              Arch _arch, OpSys _opSys);
 
     void getSections();
     bool sectionExists(std::string sec);
@@ -89,7 +100,8 @@ class ElfObject : public ObjectFile
     MemoryImage image;
 
   public:
-    virtual ~ElfObject() {}
+    ElfObject(ImageFileDataPtr ifd);
+    ~ElfObject();
 
     MemoryImage buildImage() const override { return image; }
 
@@ -111,12 +123,9 @@ class ElfObject : public ObjectFile
 
     bool hasTLS() override { return sectionExists(".tbss"); }
 
-    static ObjectFile *tryFile(const std::string &fname,
-                               size_t len, uint8_t *data,
-                               bool skip_interp_check = false);
     Addr programHeaderTable() {return _programHeaderTable;}
     uint16_t programHeaderSize() {return _programHeaderSize;}
     uint16_t programHeaderCount() {return _programHeaderCount;}
 };
 
-#endif // __ELF_OBJECT_HH__
+#endif // __BASE_LOADER_ELF_OBJECT_HH__

src/arch/arm/freebsd/system.cc
@@ -35,7 +35,7 @@
 #include "arch/arm/isa_traits.hh"
 #include "arch/arm/utility.hh"
 #include "arch/generic/freebsd/threadinfo.hh"
-#include "base/loader/dtb_object.hh"
+#include "base/loader/dtb_file.hh"
 #include "base/loader/object_file.hh"
 #include "base/loader/symtab.hh"
 #include "cpu/base.hh"
@@ -110,25 +110,15 @@ FreebsdArmSystem::initState()
     inform("Loading DTB file: %s at address %#x\n", params()->dtb_filename,
             params()->atags_addr + loadAddrOffset);
 
-    ObjectFile *dtb_file = createObjectFile(params()->dtb_filename, true);
-    if (!dtb_file) {
-        fatal("couldn't load DTB file: %s\n", params()->dtb_filename);
+    DtbFile *dtb_file = new DtbFile(params()->dtb_filename);
+
+    if (!dtb_file->addBootCmdLine(params()->boot_osflags.c_str(),
+                                  params()->boot_osflags.size())) {
+        warn("couldn't append bootargs to DTB file: %s\n",
+             params()->dtb_filename);
     }
 
-    DtbObject *_dtb_file = dynamic_cast<DtbObject*>(dtb_file);
-
-    if (_dtb_file) {
-        if (!_dtb_file->addBootCmdLine(params()->boot_osflags.c_str(),
-                                       params()->boot_osflags.size())) {
-            warn("couldn't append bootargs to DTB file: %s\n",
-                 params()->dtb_filename);
-        }
-    } else {
-        warn("dtb_file cast failed; couldn't append bootargs "
-             "to DTB file: %s\n", params()->dtb_filename);
-    }
-
-    Addr ra = _dtb_file->findReleaseAddr();
+    Addr ra = dtb_file->findReleaseAddr();
     if (ra)
         bootReleaseAddr = ra & ~ULL(0x7F);
 

commit 09c4c2b45a0e8b584165558acaa963b66814dc12

base: Fix ldMin/ldMax not updated in handleLoadableSegment
ElfObject::handleLoadableSegment() never updated ldMin and ldMax,
causing mapSize() to always return 0. This meant that in SE mode,
Process::updateBias() computed interp_mapsize=0 and mmap_end was
never advanced to reserve space for the dynamic linker, potentially
causing subsequent mmap allocations to overlap the interpreter.

The bug was introduced as a regression in d856e5ce71 ("arch,base:
Restructure the object file loaders"), which extracted the PT_LOAD
loop body into handleLoadableSegment() but accidentally dropped the
two lines that tracked ldMin/ldMax.

Also fix the initial value of ldMax from MaxAddr to 0, as it is
used for max-tracking (the original code used
std::numeric_limits<Addr>::min()).

Include <algorithm> explicitly for std::min/std::max, and guard
mapSize() against underflow when no non-empty PT_LOAD segment was
seen (ldMin/ldMax left at their sentinel values).

Fixes: https://github.com/gem5/gem5/issues/3217

src/base/loader/elf_object.cc
@@ -46,6 +46,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <algorithm>
 #include <cassert>
 #include <string>
 
@@ -391,6 +392,9 @@ ElfObject::handleLoadableSegment(GElf_Phdr phdr, int seg_num)
         image.addSegment({ name + "(uninitialized)",
                            phdr.p_paddr + phdr.p_filesz, uninitialized });
     }
+
+    ldMin = std::min(ldMin, phdr.p_vaddr);
+    ldMax = std::max(ldMax, phdr.p_vaddr + phdr.p_memsz);
 
     const Addr file_start = phdr.p_offset;
     const Addr file_end = file_start + phdr.p_filesz;

src/base/loader/elf_object.hh
@@ -94,7 +94,7 @@ class ElfObject : public ObjectFile
     // The ldMin and ldMax fields are required to know how large of an
     // area is required to map the interpreter.
     Addr ldMin = MaxAddr;
-    Addr ldMax = MaxAddr;
+    Addr ldMax = 0;
 
     /// Helper functions for loadGlobalSymbols() and loadLocalSymbols().
     bool loadSomeSymbols(SymbolTable *symtab, int binding, Addr mask,
@@ -116,7 +116,14 @@ class ElfObject : public ObjectFile
 
     Addr bias() const override { return ldBias; }
     bool relocatable() const override { return relocate; }
-    Addr mapSize() const override { return ldMax - ldMin; }
+    Addr
+    mapSize() const override
+    {
+        // ldMin/ldMax stay at their sentinel values if no non-empty
+        // PT_LOAD segment was seen. Returning ldMax - ldMin in that case
+        // would underflow, so report an empty mapping instead.
+        return (ldMin <= ldMax) ? (ldMax - ldMin) : 0;
+    }
     void updateBias(Addr bias_addr) override;
 
     bool hasTLS() override { return sectionExists(".tbss"); }