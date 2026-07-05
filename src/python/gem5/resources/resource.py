7157926580167b073454fff3e6be2e4e2817ab61
Bobby R. Bruce <bbruce@ucdavis.edu>
2026-07-04 20:07:08 -0700

    python: Fix syntax error in parameter list

--- src/python/gem5/resources/resource.py
+++ src/python/gem5/resources/resource.py
@@ -855,7 +855,8 @@ class WorkloadResource(AbstractResource):
         description: str | None = None,
         source: str | None = None,
         local_path: str | None = None,
-        parameters: dict | None = None**kwargs,
+        parameters: dict | None = None,
+        **kwargs,
     ):
         """
         :param function: The function to call on the board.
